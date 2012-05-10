eb6fdb1ef035f8fc3e0d296b92de3ef842495275
Steffen Daode Nurpmeso <sdaoden@gmail.com>
2012-05-10 14:47:03 +0200

    Fix implicit pointer->int conversion type

--- junk.c
+++ junk.c
@@ -1037,7 +1037,8 @@ clsf(struct message *m)
 	float	a = 1, b = 1, r;
 
 	if (verbose)
-		fprintf(stderr, "Examining message %d\n", m - &message[0] + 1);
+		fprintf(stderr, "Examining message %d\n",
+				(int)(m - &message[0] + 1));
 	for (i = 0; i < BEST; i++) {
 		best[i].dist = 0;
 		best[i].prob = -1;
@@ -1065,7 +1066,7 @@ clsf(struct message *m)
 	r = a+b > 0 ? a / (a+b) : 0;
 	if (verbose)
 		fprintf(stderr, "Junk probability of message %d: %g\n",
-				m - &message[0] + 1, r);
+				(int)(m - &message[0] + 1), r);
 	if (r > THR)
 		m->m_flag |= MJUNK;
 	else
--- maildir.c
+++ maildir.c
@@ -336,7 +336,8 @@ readin(const char *name, struct message *m)
 
 	if ((fp = Fopen(m->m_maildir_file, "r")) == NULL) {
 		fprintf(stderr, "Cannot read \"%s/%s\" for message %d\n",
-				name, m->m_maildir_file, m - &message[0] + 1);
+				name, m->m_maildir_file,
+				(int)(m - &message[0] + 1));
 		m->m_flag |= MHIDDEN;
 		return;
 	}
@@ -443,7 +444,7 @@ maildir_update(void)
 				fprintf(stderr, "Cannot delete file \"%s/%s\" "
 						"for message %d.\n",
 						mailname, m->m_maildir_file,
-						m - &message[0] + 1);
+						(int)(m - &message[0] + 1));
 			else
 				gotcha++;
 		} else {
@@ -492,7 +493,7 @@ move(struct message *m)
 				"message %d not touched.\n",
 				mailname, m->m_maildir_file,
 				mailname, new,
-				m - &message[0] + 1);
+				(int)(m - &message[0] + 1));
 		return;
 	}
 	if (unlink(m->m_maildir_file) < 0)
