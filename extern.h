8fd918fc7a787fa8f81ddb185bd9ffe55eff98ca
Steffen "Daode" Nurpmeso <sdaoden@users.sf.net>
2012-10-08 15:22:45 +0200

    extern.h: style nits

--- extern.h
+++ extern.h
@@ -91,12 +91,14 @@ int putuc(int u, int c, FILE *fp);
 int asccasecmp(const char *s1, const char *s2);
 int ascncasecmp(const char *s1, const char *s2, size_t sz);
 char *asccasestr(const char *haystack, const char *xneedle);
+
 /* base64.c */
 char *strtob64(const char *p);
 char *memtob64(const void *vp, size_t isz);
 size_t mime_write_tob64(struct str *in, FILE *fo, int is_header);
 void mime_fromb64(struct str *in, struct str *out, int is_text);
 void mime_fromb64_b(struct str *in, struct str *out, int is_text, FILE *f);
+
 /* cache.c */
 enum okay getcache1(struct mailbox *mp, struct message *m,
 		enum needspec need, int setflags);
@@ -112,6 +114,7 @@ enum okay cache_rename(const char *old, const char *new);
 unsigned long cached_uidvalidity(struct mailbox *mp);
 FILE *cache_queue(struct mailbox *mp);
 enum okay cache_dequeue(struct mailbox *mp);
+
 /* cmd1.c */
 char *get_pager(void);
 int headers(void *v);
@@ -134,6 +137,7 @@ int top(void *v);
 int stouch(void *v);
 int mboxit(void *v);
 int folders(void *v);
+
 /* cmd2.c */
 int next(void *v);
 int save(void *v);
@@ -160,6 +164,7 @@ int unsaveignore(void *v);
 int unsaveretain(void *v);
 int unfwdignore(void *v);
 int unfwdretain(void *v);
+
 /* cmd3.c */
 int shell(void *v);
 int dosh(void *v);
@@ -210,25 +215,30 @@ int cscore(void *v);
 int cnoop(void *v);
 int cremove(void *v);
 int crename(void *v);
+
 /* cmdtab.c */
+
 /* collect.c */
 struct attachment *edit_attachments(struct attachment *attach);
 struct attachment *add_attachment(struct attachment *attach, char *file,
-					int expand_file);
+		int expand_file);
 FILE *collect(struct header *hp, int printheaders, struct message *mp,
 		char *quotefile, int doprefix, int tflag);
 void savedeadletter(FILE *fp);
+
 /* dotlock.c */
 int fcntl_lock(int fd, int type);
 int dot_lock(const char *fname, int fd, int pollinterval, FILE *fp,
 		const char *msg);
 void dot_unlock(const char *fname);
+
 /* edit.c */
 int editor(void *v);
 int visual(void *v);
 FILE *run_editor(FILE *fp, off_t size, int type, int readonly,
 		struct header *hp, struct message *mp, enum sendaction action,
 		sighandler_type oldint);
+
 /* fio.c */
 void setptr(FILE *ibuf, off_t offset);
 int putline(FILE *obuf, char *linebuf, size_t count);
@@ -253,13 +263,16 @@ enum okay swrite1(struct sock *sp, const char *data, int sz, int use_buffer);
 int sgetline(char **line, size_t *linesize, size_t *linelen, struct sock *sp);
 enum okay sopen(const char *xserver, struct sock *sp, int use_ssl,
 		const char *uhp, const char *portstr, int verbose);
+
 /* getname.c */
 char *getname(int uid);
 int getuserid(char *name);
+
 /* getopt.c */
 #ifdef USE_BUILTIN_GETOPT
 int getopt(int argc, char *const argv[], const char *optstring);
 #endif
+
 /* head.c */
 int is_head(char *linebuf, size_t linelen);
 void parse(char *line, size_t linelen, struct headline *hl, char *pbuf);
@@ -287,6 +300,7 @@ time_t combinetime(int year, int month, int day,
 void substdate(struct message *m);
 int check_from_and_sender(struct name *fromfield, struct name *senderfield);
 char *getsender(struct message *m);
+
 /* imap.c */
 enum okay imap_noop(void);
 enum okay imap_select(struct mailbox *mp, off_t *size, int *count,
@@ -316,7 +330,9 @@ time_t imap_read_date(const char *cp);
 const char *imap_make_date_time(time_t t);
 char *imap_quotestr(const char *s);
 char *imap_unquotestr(const char *s);
+
 /* imap_gssapi.c */
+
 /* imap_search.c */
 enum okay imap_search(const char *spec, int f);
 /* junk.c */
@@ -326,6 +342,7 @@ int cungood(void *v);
 int cunjunk(void *v);
 int cclassify(void *v);
 int cprobability(void *v);
+
 /* lex.c */
 int setfile(char *name, int newmail);
 int newmailinfo(int omsgCount);
@@ -339,17 +356,20 @@ int getmdot(int newmail);
 int pversion(void *v);
 void load(char *name);
 void initbox(const char *name);
+
 /* list.c */
 int getmsglist(char *buf, int *vector, int flags);
 int getrawlist(const char *line, size_t linesize,
 		char **argv, int argc, int echolist);
 int first(int f, int m);
 void mark(int mesg, int f);
+
 /* lzw.c */
 int zwrite(void *cookie, const char *wbp, int num);
 int zfree(void *cookie);
 int zread(void *cookie, char *rbp, int num);
 void *zalloc(FILE *fp);
+
 /* macro.c */
 int cdefine(void *v);
 int define1(const char *name, int account);
@@ -360,23 +380,26 @@ int callhook(const char *name, int newmail);
 int listaccounts(FILE *fp);
 int cdefines(void *v);
 void delaccount(const char *name);
+
 /* maildir.c */
 int maildir_setfile(const char *name, int newmail, int isedit);
 void maildir_quit(void);
 enum okay maildir_append(const char *name, FILE *fp);
 enum okay maildir_remove(const char *name);
+
 /* main.c */
 int main(int argc, char *argv[]);
+
 /* mime.c */
 int mime_name_invalid(struct name *np, int putmsg);
 struct name *checkaddrs(struct name *np);
 char *gettcharset(void);
 char *need_hdrconv(struct header *hp, enum gfield w);
-#ifdef	HAVE_ICONV
+#ifdef HAVE_ICONV
 iconv_t iconv_open_ft(const char *tocode, const char *fromcode);
 size_t iconv_ft(iconv_t cd, char **inb, size_t *inbleft,
 		char **outb, size_t *outbleft, int tolerant);
-#endif	/* HAVE_ICONV */
+#endif
 enum mimeenc mime_getenc(char *h);
 int mime_getcontent(char *h);
 char *mime_getparam(char *param, char *h);
@@ -390,8 +413,8 @@ size_t prefixwrite(void *ptr, size_t size, size_t nmemb, FILE *f,
 		char *prefix, size_t prefixlen);
 size_t mime_write(void *ptr, size_t size, FILE *f,
 		enum conversion convert, enum tdflags dflags,
-		char *prefix, size_t prefixlen,
-		char **rest, size_t *restsize);
+		char *prefix, size_t prefixlen, char **rest, size_t *restsize);
+
 /* names.c */
 struct name *nalloc(char *str, enum gfield ntype);
 struct name *ndup(struct name *np, enum gfield ntype);
@@ -408,28 +431,32 @@ struct name *elide(struct name *names);
 int count(struct name *np);
 struct name *delete_alternates(struct name *np);
 int is_myname(char *name);
+
 /* nss.c */
-#ifdef	USE_NSS
+#ifdef USE_NSS
 enum okay ssl_open(const char *server, struct sock *sp, const char *uhp);
 void nss_gen_err(const char *fmt, ...);
-#endif	/* USE_NSS */
+#endif
+
 /* openssl.c */
-#ifdef	USE_OPENSSL
+#ifdef USE_OPENSSL
 enum okay ssl_open(const char *server, struct sock *sp, const char *uhp);
 void ssl_gen_err(const char *fmt, ...);
-#endif	/* USE_OPENSSL */
+#endif
 int cverify(void *vp);
 FILE *smime_sign(FILE *ip, struct header *);
 FILE *smime_encrypt(FILE *ip, const char *certfile, const char *to);
 struct message *smime_decrypt(struct message *m, const char *to,
 		const char *cc, int signcall);
 enum okay smime_certsave(struct message *m, int n, FILE *op);
+
 /* pop3.c */
 enum okay pop3_noop(void);
 int pop3_setfile(const char *server, int newmail, int isedit);
 enum okay pop3_header(struct message *m);
 enum okay pop3_body(struct message *m);
 void pop3_quit(void);
+
 /* popen.c */
 sighandler_type safe_signal(int signum, sighandler_type handler);
 FILE *safe_fopen(const char *file, const char *mode, int *omode);
@@ -448,6 +475,7 @@ void prepare_child(sigset_t *nset, int infd, int outfd);
 void sigchild(int signo);
 void free_child(int pid);
 int wait_child(int pid);
+
 /* quit.c */
 int quitcmd(void *v);
 void quit(void);
@@ -455,13 +483,15 @@ int holdbits(void);
 enum okay makembox(void);
 int savequitflags(void);
 void restorequitflags(int);
+
 /* send.c */
 char *foldergets(char **s, size_t *size, size_t *count, size_t *llen,
 		FILE *stream);
-#undef	send
-#define	send(a, b, c, d, e, f)	xsend(a, b, c, d, e, f)
+#undef send
+#define send(a, b, c, d, e, f)  xsend(a, b, c, d, e, f)
 int send(struct message *mp, FILE *obuf, struct ignoretab *doign,
 		char *prefix, enum sendaction action, off_t *stats);
+
 /* sendout.c */
 char *makeboundary(void);
 int mail(struct name *to, struct name *cc, struct name *bcc,
@@ -477,6 +507,7 @@ int puthead(struct header *hp, FILE *fo, enum gfield w,
 		enum sendaction action, enum conversion convert,
 		char *contenttype, char *charset);
 enum okay resend_msg(struct message *mp, struct name *to, int add_resent);
+
 /* smtp.c */
 char *nodename(int mayoverride);
 char *myaddrs(struct header *hp);
@@ -484,6 +515,7 @@ char *myorigin(struct header *hp);
 char *smtp_auth_var(const char *type, const char *addr);
 int smtp_mta(char *server, struct name *to, FILE *fi, struct header *hp,
 		const char *user, const char *password, const char *skinned);
+
 /* ssl.c */
 void ssl_set_vrfy_level(const char *uhp);
 enum okay ssl_vrfy_decide(void);
@@ -494,6 +526,7 @@ FILE *smime_encrypt_assemble(FILE *hp, FILE *yp);
 struct message *smime_decrypt_assemble(struct message *m, FILE *hp, FILE *bp);
 int ccertsave(void *v);
 enum okay rfc2595_hostname_match(const char *host, const char *pattern);
+
 /* strings.c */
 void *salloc(size_t size);
 void *csalloc(size_t nmemb, size_t size);
@@ -503,10 +536,12 @@ char *savestr(const char *str);
 char *savestrbuf(const char *sbuf, size_t sbuf_len);
 char *save2str(const char *str, const char *old);
 char *savecat(const char *s1, const char *s2);
+
 /* temp.c */
 FILE *Ftemp(char **fn, char *prefix, char *mode, int bits, int register_file);
 void Ftfree(char **fn);
 void tinit(void);
+
 /* thread.c */
 int thread(void *vp);
 int unthread(void *vp);
@@ -517,14 +552,17 @@ int sort(void *vp);
 int ccollapse(void *v);
 int cuncollapse(void *v);
 void uncollapse1(struct message *m, int always);
+
 /* tty.c */
 int grabh(struct header *hp, enum gfield gflags, int subjfirst);
 char *readtty(char *prefix, char *string);
 int yorn(char *msg);
+
 /* v7.local.c */
 void findmail(char *user, int force, char *buf, int size);
 void demail(void);
 char *username(void);
+
 /* vars.c */
 void assign(const char *name, const char *value);
 char *vcopy(const char *str);
