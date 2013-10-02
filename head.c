sdaoden/s-mailx

commit 9385b90ec17cee614be09e7cd1193d92cff1b5ec

Move myaddrs(),myorigin() from smtp.c to head.c..
Also change their return type from char* to char const*.

head.c
@@ -355,6 +355,49 @@ jleave:
 	return ((agp->ag_n_flags & NAME_ADDRSPEC_INVALID) != 0);
 }
 
+char const *
+myaddrs(struct header *hp)
+{
+	static char *addr;
+	struct name *np;
+	char const *ret = NULL;
+
+	if (hp != NULL && (np = hp->h_from) != NULL) {
+		if ((ret = np->n_fullname) != NULL)
+			goto jleave;
+		if ((ret = np->n_name) != NULL)
+			goto jleave;
+	}
+	if ((ret = value("from")) != NULL)
+		goto jleave;
+	/*
+	 * When invoking sendmail directly, it's its task
+	 * to generate an otherwise undeterminable From: address.
+	 */
+	if (value("smtp") == NULL)
+		goto jleave;
+	if (addr == NULL) {
+		char *hn = nodename(1);
+		size_t sz = strlen(myname) + strlen(hn) + 2;
+		ret = addr = smalloc(sz);
+		snprintf(addr, sz, "%s@%s", myname, hn);
+	}
+jleave:
+	return (ret);
+}
+
+char const *
+myorigin(struct header *hp)
+{
+	char const *ret = NULL, *ccp;
+	struct name *np;
+
+	if ((ccp = myaddrs(hp)) != NULL &&
+			(np = lextract(ccp, GEXTRA|GFULL)) != NULL)
+		ret = np->n_flink != NULL ? value("sender") : ccp;
+	return (ret);
+}
+
 /*
  * See if the passed line buffer is a mail header according to RFC 4155.
  * Return true if yes.

extern.h
@@ -349,6 +349,12 @@ int getopt(int argc, char *const argv[], const char *optstring);
 #endif
 
 /* head.c */
+
+/* Return the user's From: address(es) */
+char const *	myaddrs(struct header *hp);
+/* Boil the user's From: addresses down to a single one, or use *sender* */
+char const *	myorigin(struct header *hp);
+
 int	is_head(char const *linebuf, size_t linelen);
 int	extract_date_from_from_(char const *line, size_t linelen,
 		char datebuf[FROM_DATEBUF]);
@@ -607,8 +613,6 @@ enum okay resend_msg(struct message *mp, struct name *to, int add_resent);
 
 /* smtp.c */
 char *nodename(int mayoverride);
-char *myaddrs(struct header *hp);
-char *myorigin(struct header *hp);
 char *smtp_auth_var(const char *type, const char *addr);
 #ifdef USE_SMTP
 int	smtp_mta(char *server, struct name *to, FILE *fi, struct header *hp,

smtp.c
@@ -117,51 +117,6 @@ nodename(int mayoverride)
 	return hostname;
 }
 
-/*
- * Return the user's From: address(es).
- */
-char *
-myaddrs(struct header *hp)
-{
-	char *cp, *hn;
-	static char *addr;
-	size_t sz;
-
-	if (hp != NULL && hp->h_from != NULL) {
-		if (hp->h_from->n_fullname)
-			return savestr(hp->h_from->n_fullname);
-		if (hp->h_from->n_name)
-			return savestr(hp->h_from->n_name);
-	}
-	if ((cp = value("from")) != NULL)
-		return cp;
-	/*
-	 * When invoking sendmail directly, it's its task
-	 * to generate a From: address.
-	 */
-	if (value("smtp") == NULL)
-		return NULL;
-	if (addr == NULL) {
-		hn = nodename(1);
-		sz = strlen(myname) + strlen(hn) + 2;
-		addr = smalloc(sz);
-		snprintf(addr, sz, "%s@%s", myname, hn);
-	}
-	return addr;
-}
-
-char *
-myorigin(struct header *hp)
-{
-	char	*cp;
-	struct name	*np;
-
-	if ((cp = myaddrs(hp)) == NULL ||
-			(np = lextract(cp, GEXTRA|GFULL)) == NULL)
-		return NULL;
-	return np->n_flink != NULL ? value("sender") : cp;
-}
-
 #ifdef USE_SMTP
 
 static int read_smtp(struct sock *sp, int value, int ign_eof);

commit cfd604796a644760a25d9d668ba804167bc92604

myaddrs(): assign result to return value..
Karol Błażewicz reported a problem he had with an Arch Linux
S-nail test package [1].

  [1] <https://bugs.archlinux.org/task/37161>

  (gdb) run -s test karol
   Starting program: /home/karol/apps/s-nail/src/s-nail-14.4.2/./s-nail -s test karol
   warning: Could not load shared library symbols for linux-gate.so.1.
   Do you need "set solib-search-path" or "set sysroot"?
   test
   ^DEOT

   Program received signal SIGSEGV, Segmentation fault.
   0xb7c3e016 in __strlen_sse2_bsf () from /usr/lib/libc.so.6
   (gdb) bt
   #0 0xb7c3e016 in __strlen_sse2_bsf () from /usr/lib/libc.so.6
   #1 0x0808978c in smtp_auth_var (atype=atype@entry=0x809c5b7 "-user", addr=addr@entry=0x0) at smtp.c:316
   #2 0x08086293 in start_mta (to=to@entry=0x80a84d0 <_builtin_buf+16>, input=input@entry=0x80ca678, hp=hp@entry=0xbffff500) at sendout.c:894
   #3 0x08086936 in transfer (to=to@entry=0x80a84d0 <_builtin_buf+16>, input=input@entry=0x80ca678, hp=hp@entry=0xbffff500) at sendout.c:848
   #4 0x08088d2f in mail1 (hp=hp@entry=0xbffff500, printheaders=printheaders@entry=0, quote=quote@entry=0x0, quotefile=quotefile@entry=0x0,
   recipient_record=recipient_record@entry=0, doprefix=doprefix@entry=0) at sendout.c:1186
   #5 0x08088e82 in mail (to=0x80a84d0 <_builtin_buf+16>, cc=0x0, bcc=0x0, subject=0xbffff898 "test", attach=0x0, quotefile=quotefile@entry=0x0,
   recipient_record=0) at sendout.c:766
   #6 0x0804c429 in main (argc=4, argv=0xbffff684) at main.c:712

This bug i've introduced in [9385b90e] (Move myaddrs(),myorigin()
from smtp.c to head.c.., 2012-12-18).
Unfortunately it seems i was in a hurry and nonetheless not only
moved the functions but also slightly rewrote them.
(Pain.)

head.c
@@ -385,7 +385,7 @@ myaddrs(struct header *hp)
 	 */
 	if (value("smtp") == NULL)
 		goto jleave;
-	if (addr == NULL) {
+	if ((ret = addr) == NULL) {
 		char *hn = nodename(1);
 		size_t sz = strlen(myname) + strlen(hn) + 2;
 		ret = addr = smalloc(sz);