ecfa149cf615755881b12e7bcbc1be750b7ba612
Steffen "Daode" Nurpmeso <sdaoden@users.sf.net>
2013-10-19 13:59:03 +0200

    send.c:sendpart(): tweak (actually: fix) previous

--- send.c
+++ send.c
@@ -819,30 +819,34 @@ skip:
 	case MIME_ALTERNATIVE:
 		if ((action == SEND_TODISP || action == SEND_QUOTE) &&
 				value("print-alternatives") == NULL) {
-			size_t done = FAL0;
-			for (np = ip->m_multipart; np; np = np->m_nextpart) {
+			bool_t doact = FAL0;
+			for (np = ip->m_multipart; np; np = np->m_nextpart)
 				if (np->m_mimecontent == MIME_TEXT_PLAIN)
-					++done;
-				if (done > 0 && np->m_ct_type_plain != NULL &&
-						action != SEND_QUOTE) {
-					_print_part_info(&rest, np, doign,
-						level);
-					_out(rest.s, rest.l, obuf,
-						CONV_NONE, SEND_MBOX, qf,
-						stats, NULL);
+					doact = TRU1;
+			if (doact) {
+				for (np = ip->m_multipart; np;
+						np = np->m_nextpart) {
+					if (np->m_ct_type_plain != NULL &&
+							action != SEND_QUOTE) {
+						_print_part_info(&rest, np,
+							doign, level);
+						_out(rest.s, rest.l, obuf,
+							CONV_NONE, SEND_MBOX,
+							qf, stats, NULL);
+					}
+					if (doact && np->m_mimecontent ==
+							MIME_TEXT_PLAIN) {
+						doact = FAL0;
+						rt = sendpart(zmp, np, obuf,
+							doign, qf, action,
+							stats, level + 1);
+						quoteflt_reset(qf, origobuf);
+						if (rt < 0)
+							break;
+					}
 				}
-				if (done == 1 && np->m_mimecontent ==
-						MIME_TEXT_PLAIN) {
-					rt = sendpart(zmp, np, obuf, doign,
-							qf, action, stats,
-							level + 1);
-					quoteflt_reset(qf, origobuf);
-					if (rt < 0)
-						break;
-				}
-			}
-			if (done > 0)
 				return rt;
+			}
 		}
 		/*FALLTHRU*/
 	case MIME_MULTI:
