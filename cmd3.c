sdaoden/s-mailx

commit 3adf33ee1952782cfc554f2f505c0202b267c58d

schdir(): realpath() local files before leaving CWD..
S-nail (and all BSD Mails afaik) has the problem that the
program effectively locks up if the currently open folder cannot
be accessed no more.

Implement a short workaround for the "chdir" command that will
first try to create an absolute path for the folder, and refuse
to perform the chdir if that fails.
This workaround requires realpath(3).

It is not a solution for the actual problem, since that arises
in a lot of different conditions (like concurrent removal etc.
of the mailbox), but the real solution will need some more years
of development, and this simply commit gives a hand *now*.

catd/en_US
@@ -86,7 +86,7 @@ $set 1
 83 "%s": Expansion buffer overflow.\n
 84 "%s": Ambiguous.\n
 85 --- DELETED ---
-86 --- DELETED ---
+86 Won't "chdir": "%s" would become inaccessible
 87 temporary mail message file
 88 No mail for %s\n
 89 Use "quit" to quit.\n

cmd3.c
@@ -316,19 +316,40 @@ jleave:
 int 
 schdir(void *v)
 {
-	char **arglist = v;
-	char *cp;
+	char **arglist = v, *cp;
 
+#ifdef HAVE_REALPATH
+	/* TODO Avoid locked-up situation with relative paths when chdir(2)ing
+	 * TODO away; intermediate in that the current box's temporary
+	 * TODO representation should be saved to TMPDIR and the current BOX
+	 * TODO should be set to a-to-be-invented "void" box?  Or the user
+	 * TODO should be prompted if interactive?  It SHOULD be encapsulated
+	 * TODO in the box type driver!!  Thus - maybe add an is_absolute()
+	 * TODO flag to the (not yet existent) drivers, and forbid changing
+	 * TODO the path if that returns false (for whatever reason) */
+	if (mb.mb_type == MB_FILE || mb.mb_type == MB_MAILDIR) {
+		cp = realpath(mailname, NULL);
+		if (cp != NULL) {
+			sstpcpy(mailname, cp);
+			(free)(cp);
+		} else {
+			fprintf(stderr, tr(86, "Won't \"chdir\": "
+				"\"%s\" would become inaccessible"),
+				mailname);
+			goto jleave;
+		}
+	}
+#endif
 	if (*arglist == NULL)
 		cp = homedir;
-	else
-		if ((cp = file_expand(*arglist)) == NULL)
-			return(1);
+	else if ((cp = file_expand(*arglist)) == NULL)
+		goto jleave;
 	if (chdir(cp) < 0) {
 		perror(cp);
-		return(1);
+		cp = NULL;
 	}
-	return 0;
+jleave:
+	return (cp != NULL);
 }
 
 static void 

commit eff4397c0ef4da832b8cd55e0e455a87d2e8844e

Add `cwd' command; fix (flip) `chdir' return value..
I broke the latter in [3adf33ee] (schdir(): realpath() local
files before leaving CWD.., 2013-01-08).

catd/en_US
@@ -488,6 +488,7 @@ $ It follows the "help command" one line help block, alpha-sorted (see nail.1)
 425 Define a <ghost> of <command>, or list all ghosts
 426 Delete <ghost-list>
 427 Inside `define' / `account': insulate modifications: <0> / <1>
+428 Print current working directory (CWD)
 
 
 500 $ done.\n

cmd3.c
@@ -229,11 +229,23 @@ jleave:
 	return ret;
 }
 
-/*
- * Change user's working directory.
- */
+int
+c_cwd(void *v)
+{
+	char buf[MAXPATHLEN]; /* TODO getcwd(3) may return a larger value */
+
+	if (getcwd(buf, sizeof buf) != NULL) {
+		puts(buf);
+		v = (void*)0x1;
+	} else {
+		perror("getcwd");
+		v = NULL;
+	}
+	return (v == NULL);
+}
+
 int 
-schdir(void *v)
+c_chdir(void *v)
 {
 	char **arglist = v;
 	char const *cp;
@@ -247,7 +259,7 @@ schdir(void *v)
 		cp = NULL;
 	}
 jleave:
-	return (cp != NULL);
+	return (cp == NULL);
 }
 
 static void 

cmd_tab.h
@@ -101,9 +101,9 @@
      DS(314, "Copy <message-list>, but don't mark them for deletion") },
    { "Copy", Copycmd, (A | M | STRLIST), 0, 0
      DS(315, "Like \"copy\", but derive filename from first sender") },
-   { "chdir", schdir, (M | RAWLIST), 0, 1
+   { "chdir", c_chdir, (M | RAWLIST), 0, 1
      DS(309, "Change CWD to the specified/the login directory") },
-   { "cd", schdir, (M | RAWLIST), 0, 1
+   { "cd", c_chdir, (M | RAWLIST), 0, 1
      DS(309, "Change CWD to the specified/the login directory") },
    { "save", save, (A | STRLIST), 0, 0
      DS(371, "Append <message-list> to <file>") },
@@ -345,6 +345,10 @@
      DS(426, "Delete <ghost-list>") },
    { "localopts", &c_localopts, (M | RAWLIST), 1, 1
      DS(427, "Inside `define' / `account': insulate modifications? <0> / <1>")},
+   { "cwd", &c_cwd, (M | NOLIST), 0, 0
+     DS(428, "Print current working directory (CWD)") },
+   { "pwd", &c_cwd, (M | NOLIST), 0, 0
+     DS(428, "Print current working directory (CWD)") },
 #ifdef HAVE_ASSERTS
    { "core", core, (M | NOLIST), 0, 0
      DS(414, "Produce a core dump (ouch!)") },

extern.h
@@ -296,7 +296,13 @@ int unfwdretain(void *v);
 int shell(void *v);
 int dosh(void *v);
 int help(void *v);
-int schdir(void *v);
+
+/* Print user's working directory */
+int	c_cwd(void *v);
+
+/* Change user's working directory */
+int	c_chdir(void *v);
+
 int respond(void *v);
 int respondall(void *v);
 int respondsender(void *v);
@@ -338,8 +344,6 @@ int cundraft(void *v);
 int cnoop(void *v);
 int cremove(void *v);
 int crename(void *v);
-
-/* cmdtab.c */
 
 /* collect.c */
 

nail.1
@@ -1045,6 +1045,8 @@ command.
 .Ic copy ,
 but saves the messages in a file named after the local part of the
 sender address of the first message.
+.It Ic cwd
+Print the current working directory.
 .It Ic decrypt
 \*(OP (dec) For unencrypted messages,
 this command is identical to