46e121317f1522e00c3c8f75020bd435d8198b02
Steffen "Daode" Nurpmeso <sdaoden@users.sf.net>
2013-12-20 22:24:25 +0100

    cc-test.sh: tweak

--- cc-test.sh
+++ cc-test.sh
@@ -2,6 +2,19 @@
 #@ Usage: ./cc-test.sh [--check-only]
 #@ XXX Add tests
 
+NAIL=./s-nail
+CONF=./conf.rc
+BODY=./.cc-body.txt
+MBOX=./.cc-test.mbox
+
+awk=`command -pv awk`
+cat=`command -pv cat`
+cksum=`command -pv cksum`
+MAKE="${MAKE:-`command -pv make`}"
+rm=`command -pv rm`
+
+##  --  >8  --  8<  --  ##
+
 # NOTE!  UnixWare 7.1.4 gives ISO-10646-Minimum-European-Subset for
 # nl_langinfo(CODESET), then, so also overwrite ttycharset.
 # (In addition this setup allows us to succeed on TinyCore 4.4 that has no
@@ -11,12 +24,6 @@ LC_ALL=${LC} LANG=${LC}
 ttycharset=UTF-8
 export LC_ALL LANG ttycharset
 
-MAKE=make
-NAIL=./s-nail
-CONF=./conf.rc
-
-BODY=./.cc-body.txt
-MBOX=./.cc-test.mbox
 ESTAT=0
 
 usage() {
@@ -37,7 +44,7 @@ rm -f "${BODY}" "${MBOX}"
 
 # Test all configs TODO doesn't cover all *combinations*, stupid!
 cc_all_configs() {
-   < ${CONF} awk '
+   < ${CONF} ${awk} '
    BEGIN {i = 0}
    /^[[:space:]]*WANT_/ {
       sub(/^[[:space:]]*/, "")
@@ -85,34 +92,34 @@ test_mail() {
 
    # Three tests for MIME-CTE and (a bit) content classification.
    # At the same time testing -q FILE, < FILE and -t FILE
-   rm -f "${MBOX}"
+   ${rm} -f "${MBOX}"
    < "${BODY}" MAILRC=/dev/null \
    "${NAIL}" -n -Sstealthmua -a "${BODY}" -s "${SUB}" "${MBOX}"
    cksum_test 1 "${MBOX}" '2606934084 5649'
 
-   rm -f "${MBOX}"
+   ${rm} -f "${MBOX}"
    < /dev/null MAILRC=/dev/null \
    "${NAIL}" -n -Sstealthmua -a "${BODY}" -s "${SUB}" \
       -q "${BODY}" "${MBOX}"
    cksum_test 2 "${MBOX}" '2606934084 5649'
 
-   rm -f "${MBOX}"
+   ${rm} -f "${MBOX}"
    (  echo "To: ${MBOX}" && echo "Subject: ${SUB}" && echo &&
-      cat "${BODY}"
+      ${cat} "${BODY}"
    ) | MAILRC=/dev/null "${NAIL}" -n -Sstealthmua -a "${BODY}" -t
    cksum_test 3 "${MBOX}" '799758423 5648'
 
    # Test for [260e19d].  Juergen Daubert.
-   rm -f "${MBOX}"
+   ${rm} -f "${MBOX}"
    echo body | MAILRC=/dev/null "${NAIL}" -n -Sstealthmua "${MBOX}"
    cksum_test 4 "${MBOX}" '506144051 104'
 
    # Sending of multiple mails in a single invocation
-   rm -f "${MBOX}"
+   ${rm} -f "${MBOX}"
    (  printf "m ${MBOX}\n~s subject1\nE-Mail Körper 1\n.\n" &&
       printf "m ${MBOX}\n~s subject2\nEmail body 2\n.\n" &&
       echo x
-   ) | MAILRC=/dev/null "${NAIL}" -N -n -# -Sstealthmua
+   ) | MAILRC=/dev/null "${NAIL}" -n -# -Sstealthmua
    cksum_test 5 "${MBOX}" '2028749685 277'
 }
 
@@ -187,7 +194,7 @@ test_mail
 
 [ ${ESTAT} -eq 0 ] && echo Ok || echo >&2 'Errors occurred'
 [ -n "${CHECK_ONLY}" ] || "${MAKE}" distclean
-rm -f "${BODY}" "${MBOX}"
+${rm} -f "${BODY}" "${MBOX}"
 
 exit ${ESTAT}
 # vim:set fenc=utf8:s-it-mode
