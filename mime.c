202506ecc49428be1ced8d00fad08470cf0f4947
Steffen "Daode" Nurpmeso <sdaoden@users.sf.net>
2013-12-31 14:08:11 +0100

    charset_iter_reset(): FIX: need to count sep. commas!

--- mime.c
+++ mime.c
@@ -364,7 +364,7 @@ charset_iter_reset(char const *a_charset_to_try_first)
 		sarrl[0] = 0;
 #endif
 
-	_cs_iter_base = cp = salloc(len + 1);
+	_cs_iter_base = cp = salloc(len + 1 + 1 +1);
 
 #ifdef HAVE_ICONV
 	if ((len = sarrl[0]) != 0) {
