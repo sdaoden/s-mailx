sdaoden/s-mailx

commit b7b673d2b0bd8e161bb3e4bd75cfeda9dfc860f5

Furtherly extend mime.types TYPEMARKER extension..
On the long run we will gain primitive builtin support for HTML-
and XML-tagsoup-to-text conversion.  (I'm still dreaming of RSS
and Atom feed support.)

Since we're at it, extend the mechanism today for the necessary
support (at least in respect to HTML tagsoup).

nail.1
@@ -6779,6 +6779,18 @@ The following type markers are supported:
 .Bl -tag -compact -offset indent -width ".It Ar _n_u"
 .It Ar @
 Treat message parts with this content as plain text.
+.It Ar @t@
+The same as plain
+.Ar @ .
+.It Ar @h@
+Treat message parts with this content as HTML tagsoup.
+If the (primitive) HTML-tagsoup-to-text converter is not available treat
+the content as plain text instead.
+.It Ar @H@
+Likewise
+.Ar @h@
+but instead of falling back to plain text require an explicit content
+handler to be defined.
 .El
 .
 .Pp

send.c
@@ -524,42 +524,39 @@ _print_part_info(struct str *out, struct mimepart *mip,
 static enum pipeflags
 _pipecmd(char const **result, struct mimepart const *mpp)
 {
-   enum pipeflags ret;
+   enum pipeflags rv;
    char const *cp;
-   NYD_ENTER;
+   NYD2_ENTER;
 
    *result = NULL;
 
    /* Do we have any handler for this part? */
-   if ((cp = mime_type_mimepart_handler(mpp)) == NULL)
-      ret = PIPE_NULL;
-   /* User specified a command, inspect for special cases */
-   else if (cp[0] != '@') {
-      /* Normal command line */
-      ret = PIPE_COMM;
+   if ((cp = mime_type_mimepart_handler(mpp)) == NULL ||
+         cp == MIME_TYPE_HANDLER_HTML)
+      rv = PIPE_NULL;
+   else if (cp == MIME_TYPE_HANDLER_TEXT)
+      rv = PIPE_TEXT;
+   else if (*cp != '@') {
       *result = cp;
-   }
-   else if (*++cp == '\0')
-      /* Treat as plain text */
-      ret = PIPE_TEXT;
-   else if (!(pstate & PS_MSGLIST_DIRECT)) {
+      rv = PIPE_COMM;
+   } else if (!(pstate & PS_MSGLIST_DIRECT)) {
       /* Viewing multiple messages in one go, don't block system */
-      ret = PIPE_MSG;
       *result = _("[Directly address message only to display this]\n");
+      rv = PIPE_MSG;
    } else {
       /* Viewing a single message only */
       /* TODO send/MIME layer rewrite: when we have a single-pass parser
        * TODO then the parsing phase and the send phase will be separated;
        * TODO that allows us to ask a user *before* we start the send, i.e.,
        * TODO *before* a pager pipe is setup */
-      if (cp[0] == '&')
+      if (*++cp == '&')
          /* Asynchronous command, normal command line */
-         ret = PIPE_ASYNC, *result = ++cp;
+         *result = ++cp, rv = PIPE_ASYNC;
       else
-         ret = PIPE_COMM, *result = cp;
+         *result = cp, rv = PIPE_COMM;
    }
-   NYD_LEAVE;
-   return ret;
+   NYD2_LEAVE;
+   return rv;
 }
 
 static FILE *

mime.types
@@ -12,11 +12,19 @@
 # interpreted according to the scheme
 #    [TYPEMARKER ]MIMETYPE whitespace EXTENSION(S, whitespace separated)
 # MIMETYPE may occur several times, values are joined.
+#
 # Syntax extensions:
 # - Follow lines may be used: instead of repeating MIMETYPE, start the next
 #   line with whitespace.  (Intervening comment lines may be used.)
 # - TYPEMARKER: an at sign (@) will tag the MIME type as plain text (unless
 #   the user installs a specific pipe handler for the type in question).
+#   . "@t@" is identical to plain "@".
+#   . "@h@" will tag the MIME type as HTML tagsoup.
+#   . "@H@" is identical to "@h@", but won't treat the MIME type as plain
+#      text if the builtin HTML tagsoup parser isn't available, i.e., it
+#      requires an explicit pipe-MIMETYPE (or pipe-EXTENSION) handler.
+#   [Maybe later: . "@x@" and "@X@" are like h/H, but for XML tagsoup.]
+#
 # Syntax restrictions:
 # - Comments must be lines of their own.
 # - Only MIME types for which we have mime.c:enum mime_type constants are
@@ -33,9 +41,9 @@
 @ text/troff                  t tr roff man me ms
                               1 2 3 4 5 6 7 8 9
 application/pdf               pdf
-@ application/xhtml+xml       xhtml xht
-@ text/html                   html htm
-@ application/xml             xml xsl xsd
+@h@ application/xhtml+xml     xhtml xht
+@h@ text/html                 html htm
+application/xml               xml xsl xsd
 
 @ application/x-sh            sh bash
 @ text/x-awk                  awk

mime_types.c
@@ -33,10 +33,14 @@ enum mime_type {
    __MT_TMAX   = _MT_OTHER,
    __MT_TMASK  = 0x07,
 
-   _MT_LOADED  = 1<< 4,       /* Not struct mtbltin */
-   _MT_USR     = 1<< 5,       /* MIME_TYPES_USR */
-   _MT_SYS     = 1<< 6,       /* MIME_TYPES_SYS */
-   _MT_PLAIN   = 1<< 7        /* Without pipe handler display as text */
+   _MT_LOADED  = 1<< 8,       /* Not struct mtbltin */
+   _MT_USR     = 1<< 9,       /* MIME_TYPES_USR */
+   _MT_SYS     = 1<<10,       /* MIME_TYPES_SYS */
+
+   _MT_PLAIN   = 1<<16,       /* Without pipe handler display as text */
+   _MT_SOUP_h  = 2<<16,       /* Ditto, but HTML tagsoup parser if possible */
+   _MT_SOUP_H  = 3<<16,       /* HTML tagsoup parser, else NOT plain text */
+   __MT_MARKMASK = _MT_SOUP_H
 };
 
 struct mtbltin {
@@ -251,12 +255,28 @@ _mt_create(bool_t cmdcalled, ui32_t orflags, char const *line, size_t len)
 
    /* (But wait - is there a type marker?) */
    if (!(orflags & (_MT_USR | _MT_SYS)) && *typ == '@') {
-      if (len < 2 || typ[1] != ' ')
+      if (len < 2)
          goto jeinval;
-      orflags |= _MT_PLAIN;
-      typ += 2;
-      len -= 2;
-      line += 2;
+      if (typ[1] == ' ') {
+         orflags |= _MT_PLAIN;
+         typ += 2;
+         len -= 2;
+         line += 2;
+      } else if (len > 4 && typ[2] == '@' && typ[3] == ' ') {
+         switch (typ[1]) {
+         case 't':   orflags |= _MT_PLAIN;   goto jexttypmar;
+         case 'h':   orflags |= _MT_SOUP_h;  goto jexttypmar;
+         case 'H':   orflags |= _MT_SOUP_H;
+jexttypmar:
+            typ += 4;
+            len -= 4;
+            line += 4;
+            break;
+         default:
+            goto jeinval;
+         }
+      } else
+         goto jeinval;
    }
 
    while (len > 0 && !blankchar(*line))
@@ -460,15 +480,22 @@ c_mimetype(void *v)
       }
 
       for (l = 0, mtnp = _mt_list; mtnp != NULL; ++l, mtnp = mtnp->mt_next) {
-         char const *typ = ((mtnp->mt_flags & __MT_TMASK) == _MT_OTHER)
+         char const *tmark, *typ;
+
+         switch (mtnp->mt_flags & __MT_MARKMASK) {
+         case _MT_PLAIN:   tmark = "/t"; break;
+         case _MT_SOUP_h:  tmark = "/h"; break;
+         case _MT_SOUP_H:  tmark = "/H"; break;
+         default:          tmark = "  "; break;
+         }
+         typ = ((mtnp->mt_flags & __MT_TMASK) == _MT_OTHER)
                ? "" : _mt_typnames[mtnp->mt_flags & __MT_TMASK];
 
-         fprintf(fp, "%c%c %s%.*s <%s>\n",
+         fprintf(fp, "%c%s %s%.*s <%s>\n",
             (mtnp->mt_flags & _MT_USR ? 'U'
                : (mtnp->mt_flags & _MT_SYS ? 'S'
                : (mtnp->mt_flags & _MT_LOADED ? 'F' : 'B'))),
-            (mtnp->mt_flags & _MT_PLAIN ? '@' : ' '),
-            typ, (int)mtnp->mt_mtlen, mtnp->mt_line,
+            tmark, typ, (int)mtnp->mt_mtlen, mtnp->mt_line,
             mtnp->mt_line + mtnp->mt_mtlen);
       }
 
@@ -844,11 +871,17 @@ mime_type_mimepart_handler(struct mimepart const *mpp)
       if ((rv = vok_vlook(buf)) != NULL)
          goto jok;
 
-      if (_mt_by_mtname(&mtl, cs) != NULL &&
-            (mtl.mtl_node->mt_flags & _MT_PLAIN)) {
-         rv = "@";
-         goto jok;
-      }
+      if (_mt_by_mtname(&mtl, cs) != NULL)
+         switch (mtl.mtl_node->mt_flags & __MT_MARKMASK) {
+         case _MT_SOUP_H:
+            /* FALLTHRU */
+         default:
+            break;
+         case _MT_SOUP_h:
+         case _MT_PLAIN:
+            rv = MIME_TYPE_HANDLER_TEXT;
+            goto jok;
+         }
    }
 
    rv = NULL;

mk-mk.in
@@ -60,13 +60,13 @@ mime_types.h: mime.types
 		add(ltype, $$0);\
 		next\
 	}\
-	/^(@ )?[[:alpha:]]/ {\
-		if ($$1 ~ /^@$$/) {\
-			pa = 1;\
+	/^(@([thH]@)? )?[[:alpha:]]/ {\
+		if ($$1 ~ /^@([thH]@)?$$/) {\
+			pa = $$1;\
 			$$1 = $$2;\
 			$$2 = ""\
 		} else\
-			pa = 0;\
+			pa = "";\
 		if ($$1 !~ /^([[:alnum:]]+)\/([[:alnum:]_+-]+)$$/)\
 			print "FAULTY MIME TYPE: <" $$1 ">" >> "/dev/stderr";\
 		ltype = $$1; $$1 = "";\
@@ -83,7 +83,17 @@ mime_types.h: mime.types
 			mt = toupper(substr(t, 1, j - 1));\
 			j = substr(t, j + 1);\
 			l = length(j);\
-			mt = p_a[t] ? "_MT_" mt " | _MT_PLAIN" : "_MT_" mt;\
+			if (!p_a[t])\
+				mt = "_MT_" mt;\
+			else {\
+				tm = p_a[t];\
+				if (tm == "@" || tm == "@t@")\
+					mt = "_MT_" mt " | _MT_PLAIN";\
+				else if (tm == "@h@")\
+					mt = "_MT_" mt " | _MT_SOUP_h";\
+				else if (tm == "@H@")\
+					mt = "_MT_" mt " | _MT_SOUP_H";\
+			}\
 			print "   {" mt ", " l ", \"" j e_a[t] "\"},"\
 		}\
 	}\

nail.h
@@ -749,6 +749,14 @@ enum mimecontent {
    MIME_DISCARD      /* content is discarded */
 };
 
+enum mime_counter_evidence {
+   MIMECE_NONE,
+   MIMECE_SET        = 1<<0,  /* *mime-counter-evidence* was set */
+   MIMECE_BIN_OVWR   = 1<<1,  /* appli../octet-stream: check, ovw if possible */
+   MIMECE_ALL_OVWR   = 1<<2,  /* all: check, ovw if possible */
+   MIMECE_BIN_PARSE  = 1<<3   /* appli../octet-stream: classify contents last */
+};
+
 /* Content-Transfer-Encodings as defined in RFC 2045:
  * - Quoted-Printable, section 6.7
  * - Base64, section 6.8 */
@@ -756,6 +764,15 @@ enum mimecontent {
 
 #define B64_LINESIZE    (4 * 19)       /* Max. compliant Base64 linesize */
 #define B64_ENCODE_INPUT_PER_LINE 57   /* Max. input for Base64 encode/line */
+
+enum mime_enc {
+   MIMEE_NONE,       /* message is not in MIME format */
+   MIMEE_BIN,        /* message is in binary encoding */
+   MIMEE_8B,         /* message is in 8bit encoding */
+   MIMEE_7B,         /* message is in 7bit encoding */
+   MIMEE_QP,         /* message is quoted-printable */
+   MIMEE_B64         /* message is in base64 encoding */
+};
 
 /* xxx QP came later, maybe rewrite all to use mime_enc_flags directly? */
 enum mime_enc_flags {
@@ -799,22 +816,9 @@ enum b64flags {
    B64_ISENCWORD  = MIMEEF_ISENCWORD
 };
 
-enum mime_enc {
-   MIMEE_NONE,       /* message is not in MIME format */
-   MIMEE_BIN,        /* message is in binary encoding */
-   MIMEE_8B,         /* message is in 8bit encoding */
-   MIMEE_7B,         /* message is in 7bit encoding */
-   MIMEE_QP,         /* message is quoted-printable */
-   MIMEE_B64         /* message is in base64 encoding */
-};
-
-enum mime_counter_evidence {
-   MIMECE_NONE,
-   MIMECE_SET        = 1<<0,  /* *mime-counter-evidence* was set */
-   MIMECE_BIN_OVWR   = 1<<1,  /* appli../octet-stream: check, ovw if possible */
-   MIMECE_ALL_OVWR   = 1<<2,  /* all: check, ovw if possible */
-   MIMECE_BIN_PARSE  = 1<<3   /* appli../octet-stream: classify contents last */
-};
+/* Special handler return values for mime_type_mimepart_handler() */
+#define MIME_TYPE_HANDLER_TEXT   (char*)-1
+#define MIME_TYPE_HANDLER_HTML   (char*)-2
 
 enum mlist_state {
    MLIST_OTHER       = 0,     /* Normal address */

nailfuns.h
@@ -1324,7 +1324,8 @@ FL enum conversion mime_type_file_classify(FILE *fp, char const **contenttype,
  * but otherwise mpp is const */
 FL enum mimecontent mime_type_mimepart_content(struct mimepart *mpp);
 
-/* Get the (pipe) handler for a part, or NULL if there is none known */
+/* Get the (pipe) handler for a part (may be MIME_TYPE_HANDLER_*),
+ * or NULL if there is none known */
 FL char const * mime_type_mimepart_handler(struct mimepart const *mpp);
 
 /*

commit ea655de57ed0afd8dae6d50d2ad958cbb8e21f4d

Fix "pipe-TYPE/SUBTYPE=@"..
It has been broken in [b7b673d] (Furtherly extend mime.types
TYPEMARKER extension.., 2015-03-28), which mysteriously removed
the entire condition check.  A real Freudian error.
So oops, that killed a cat: document that the PIPE=@ text-
enforcing case can also be achieved with a special type-marker and
the `mimetype' command, in a (possibly) more useful context.

nail.1
@@ -6199,6 +6199,9 @@ The special value
 can be used to force interpretation of the message part as plain text, e.g.,
 .Ql set pipe-application/pgp-signature=@
 will henceforth treat signatures as plain text and display them "as is".
+(The same can also be achieved, in a more useful context, by using the
+.Ic mimetype
+command in conjunction with a type marker.)
 .Pp
 Also, if a shell command is prefixed with
 .Ql @ ,

send.c
@@ -534,6 +534,9 @@ _pipecmd(char const **result, struct mimepart const *mpp)
          *cp != '@') {
       *result = cp;
       rv = PIPE_COMM;
+   } else if (*++cp == '\0') {
+      /* Treat as plain text */
+      rv = PIPE_TEXT;
    } else if (!(pstate & PS_MSGLIST_DIRECT)) {
       /* Viewing multiple messages in one go, don't block system */
       *result = _("[Directly address message only to display this]\n");
@@ -544,7 +547,7 @@ _pipecmd(char const **result, struct mimepart const *mpp)
        * TODO then the parsing phase and the send phase will be separated;
        * TODO that allows us to ask a user *before* we start the send, i.e.,
        * TODO *before* a pager pipe is setup */
-      if (*++cp == '&')
+      if (*cp == '&')
          /* Asynchronous command, normal command line */
          *result = ++cp, rv = PIPE_ASYNC;
       else