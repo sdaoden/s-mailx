3bcbbfc3e0271f9248b8002ae2ba98809a59f85b
Steffen (Daode) Nurpmeso <sdaoden@users.sf.net>
2015-07-22 16:16:51 +0200

    FIX!  NAIL_FILENAME_GENERATED would generate ENAMETOOLONG!

--- send.c
+++ send.c
@@ -592,18 +592,20 @@ _pipefile(char const *pipecomm, struct mimepart const *mpp, FILE **qbuf,
       cp = "";
    env_addon[0] = str_concat_csvl(&s, NAILENV_FILENAME, "=", cp, NULL)->s;
 
-   /* NAIL_FILENAME_GENERATED */
-   s.s = getrandstring(NAME_MAX);
+   /* NAIL_FILENAME_GENERATED TODO pathconf NAME_MAX, adjust */
+   s.s = getrandstring(NAME_MAX / 4);
    if (mpp == NULL)
       cp = s.s;
    else if (*cp == '\0') {
       if (  (((cp = mpp->m_ct_type_usr_ovwr) == NULL || *cp == '\0') &&
              ((cp = mpp->m_ct_type_plain) == NULL || *cp == '\0')) ||
-            ((sh = strrchr(cp, '/')) == NULL || *++sh == '\0'))
+            ((sh = strrchr(cp, '/')) == NULL || *++sh == '\0') ||
+            strlen(sh) > NAME_MAX / 4)
          cp = s.s;
       else {
-         LCTA(NAME_MAX >= 8);
-         s.s[7] = '.';
+         LCTA(NAME_MAX / 4 >= 13);
+         s.s[NAME_MAX / 4  -1] = '.';
+         s.s[NAME_MAX / 4] = '\0';
          cp = savecat(s.s, sh);
       }
    }
