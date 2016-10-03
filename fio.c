sdaoden/s-mailx

commit edb2dcda6dbb80733232477e484d64edcceb8bbf

Drop expand_shell() (unused), change IMAP % expands..
in that `%' really means *folder* unless the IMAP URL therein
doesn't have a folder part, in which case `/INBOX' is appended;
before `%' always appended `/INBOX' for IMAP, though i'd already
documented the new behaviour.

While here, change expansion in var_folder_updated() from
EXP_SHELL to EXP_FULL.  This is more flexible.
And ensure that a we do not enter endless recursion if *MBOX* is
set to `&', which could happen due to this.
And treat an expanded `%' as final again, don't reenter expansion.

  In general all of this should be done in a different way; maybe
  once we have an URI object that parses anything for us, we could
  then decide what should be valid and what not.  I don't really
  know; i.e., i'm completely frustrated by the `@' special
  expansion, which, according to Gunnar Ritter, expands to the
  root of an IMAP *folder*, but **ONLY** if we are connected to
  that one, not otherwise!  Shouldn't this work relative to
  *folder* instead?  I'm afraid it should -- but, for this to
  work, the entire MUA needs to be rewritten, so that we can
  automatically connect SAFELY, which of course doesn't work right
  now, not even approximately...  This?  Maybe never.

Tweak documentation of "folder", *folder*, *MAIL* and *MBOX*.

fio.c
@@ -87,6 +87,10 @@ enum expmode {
  */
 static char *	_expand(char const *name, enum expmode expmode);
 
+/* Locate the user's mailbox file (where new, unread mail is queued) */
+static void	_findmail(char *buf, size_t bufsize, char const *user,
+			bool_t force);
+
 /* Perform shell meta character expansion */
 static char *	_globname(char const *name);
 
@@ -108,7 +112,7 @@ _expand(char const *name, enum expmode expmode)
 	char cbuf[MAXPATHLEN], *res;
 	struct str s;
 	struct shortcut *sh;
-	int dyn;
+	bool_t dyn;
 
 	/*
 	 * The order of evaluation is "%" and "#" expand into constants.
@@ -121,25 +125,26 @@ _expand(char const *name, enum expmode expmode)
 		res = sh->sh_long;
 
 	if (expmode & EXP_SHELL) {
-		dyn = 0;
+		dyn = FAL0;
 		goto jshell;
 	}
-
-jnext:	dyn = 0;
+jnext:
+	dyn = FAL0;
 	switch (*res) {
 	case '%':
-		if (res[1] == ':' && res[2]) {
+		if (res[1] == ':' && res[2] != '\0') {
 			res = &res[2];
 			goto jnext;
 		}
-		findmail((res[1] ? res + 1 : myname),
-			(res[1] != '\0' || option_u_arg), cbuf, sizeof cbuf);
+		_findmail(cbuf, sizeof cbuf,
+			(res[1] != '\0') ? res + 1 : myname,
+			(res[1] != '\0' || option_u_arg != NULL));
 		res = cbuf;
-		goto jnext;/*jislocal; XXX */
+		goto jislocal;
 	case '#':
-		if (res[1] != 0)
+		if (res[1] != '\0')
 			break;
-		if (prevfile[0] == 0) {
+		if (prevfile[0] == '\0') {
 			fprintf(stderr, tr(80, "No previous file\n"));
 			res = NULL;
 			goto jleave;
@@ -147,40 +152,46 @@ jnext:	dyn = 0;
 		res = prevfile;
 		goto jislocal;
 	case '&':
-		if (res[1] == 0 && (res = value("MBOX")) == NULL)
-			res = UNCONST("~/mbox");
+		if (res[1] == '\0') {
+			if ((res = value("MBOX")) == NULL)
+				res = UNCONST("~/mbox");
+			else if (res[0] != '&' || res[1] != '\0')
+				goto jnext;
+		}
 		break;
 	}
 
 	if (res[0] == '@' && which_protocol(mailname) == PROTO_IMAP) {
 		res = str_concat_csvl(&s,
 			protbase(mailname), "/", res + 1, NULL)->s;
-		dyn = 1;
+		dyn = TRU1;
 	}
 
 	if (res[0] == '+' && getfold(cbuf, sizeof cbuf)) {
-		char const *cp = "/";
+		size_t i = strlen(cbuf);
 
-		if (which_protocol(cbuf) == PROTO_IMAP &&
-				strcmp(cbuf, protbase(cbuf)))
-			cp = "";
-		res = str_concat_csvl(&s, cbuf, cp, res + 1, NULL)->s;
-		dyn = 1;
-		if (cbuf[0] == '%' && cbuf[1] == ':')
+		res = str_concat_csvl(&s, cbuf,
+			(i > 0 && cbuf[i - 1] == '/') ? "" : "/",
+			res + 1, NULL)->s;
+		dyn = TRU1;
+
+		if (res[0] == '%' && res[1] == ':') {
+			res += 2;
 			goto jnext;
+		}
 	}
 
 	/* Catch the most common shell meta character */
 jshell:
 	if (res[0] == '~' && (res[1] == '/' || res[1] == '\0')) {
 		res = str_concat_csvl(&s, homedir, res + 1, NULL)->s;
-		dyn = 1;
+		dyn = TRU1;
 	}
 
 	if (anyof(res, "|&;<>~{}()[]*?$`'\"\\") &&
 			which_protocol(res) == PROTO_FILE) {
 		res = _globname(res);
-		dyn = 1;
+		dyn = TRU1;
 		goto jleave;
 	}
 
@@ -202,6 +213,34 @@ jleave:
 	if (res && ! dyn)
 		res = savestr(res);
 	return res;
+}
+
+static void
+_findmail(char *buf, size_t bufsize, char const *user, bool_t force)
+{
+	char *cp;
+
+	if (strcmp(user, myname) == 0 && ! force &&
+			(cp = value("folder")) != NULL) {
+		switch (which_protocol(cp)) {
+		case PROTO_IMAP:
+			if (strcmp(cp, protbase(cp)) != 0)
+				goto jcopy;
+			snprintf(buf, bufsize, "%s/INBOX", cp);
+			goto jleave;
+		default:
+			break;
+		}
+	}
+
+	if (force || (cp = value("MAIL")) == NULL)
+		snprintf(buf, bufsize, "%s/%s", MAILSPOOL, user);
+	else {
+jcopy:
+		n_strlcpy(buf, cp, bufsize);
+	}
+jleave:
+	;
 }
 
 static char *
@@ -747,40 +786,6 @@ file_expand(char const *name)
 	return _expand(name, EXP_LOCAL);
 }
 
-char *
-shell_expand(char const *name)
-{
-	return _expand(name, EXP_SHELL);
-}
-
-void
-findmail(char const *user, int force, char *buf, int size)
-{
-	char *mbox, *cp;
-
-	if (strcmp(user, myname) == 0 && ! force &&
-			(cp = value("folder")) != NULL) {
-		mbox = UNCONST("");
-		switch (which_protocol(cp)) {
-		case PROTO_IMAP:
-			mbox = UNCONST("/INBOX");
-			/* FALLTRHU */
-		case PROTO_POP3:
-			snprintf(buf, size, "%s%s", protbase(cp), mbox);
-			goto jleave;
-		default:
-			break;
-		}
-	}
-	if (force || (mbox = value("MAIL")) == NULL)
-		snprintf(buf, size, "%s/%s", MAILSPOOL, user);
-	else {
-		strncpy(buf, mbox, size);
-		buf[size-1]='\0';
-	}
-jleave:	;
-}
-
 void
 demail(void)
 {
@@ -807,7 +812,7 @@ var_folder_updated(char **name)
 	 * XXX update environment variables via the "set" mechanism */
 	if (folder[0] == '%' && folder[1] == ':')
 		folder += 2;
-	if ((folder = _expand(folder, EXP_SHELL)) == NULL)
+	if ((folder = _expand(folder, EXP_FULL)) == NULL)
 		goto jleave;
 
 	switch (which_protocol(folder)) {

extern.h
@@ -363,22 +363,18 @@ off_t fsize(FILE *iob);
  *	+file	file in folder directory
  *	any shell meta character
  * file_expand() requires the expansion to be a local file/directory, and
- * shell_expand() doesn't interpret the meta characters except for shell ones.
  * Return the file name as a dynamic string */
 char *	expand(char const *name);
 char *	file_expand(char const *name);
-char *	shell_expand(char const *name);
 
-/* Locate the user's mailbox file (where new, unread mail is queued) */
-void	findmail(char const *user, int force, char *buf, int size);
 /* Get rid of queued mail */
 void	demail(void);
 
 /* vars.c hook: *folder* variable has been updated */
 bool_t	var_folder_updated(char **name);
 
-/* Determine the current *folder* name */
-bool_t	getfold(char *name, int size);
+/* Determine the current *folder* name, store it in *name* */
+bool_t	getfold(char *name, size_t size);
 
 char const *getdeadletter(void);
 

nail.1
@@ -34,7 +34,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd Jun 01, 2013
+.Dd Jun 05, 2013
 .\"--MKMAN-START--
 .ds UU \\%S-NAIL
 .ds UA \\%S-nail
@@ -1099,12 +1099,10 @@ If `user' contains special characters, in particular `/' or `%',
 they must be escaped in URL notation, as `%2F' or `%25'.
 The optional `file' part applies to IMAP only;
 if it is omitted, the default `INBOX' is used.
+.Pp
 If \*(UA is connected to an IMAP server,
-a name of the form `@mailbox' refers to the `mailbox' on that server.
-If the
-.Va folder
-variable refers to an IMAP account,
-the special name `%' selects the `INBOX' on that account.
+a name of the form `@mailbox' refers to the `mailbox' on that server,
+but otherwise a `@' prefix has no special meaning.
 .It Ic Followup
 (F) Similar to
 .Ic Respond ,
@@ -2607,29 +2605,19 @@ gives the character to use in place of `~' to denote tilde escapes.
 .It Va folder
 The name of the directory to use for storing folders of messages.
 All folder names that begin with `+' refer to files below it.
-Shell word expansions are applied to
-.Va folder
-first; if the resulting name begins with a `/' then \*(UA considers it
-to be an absolute pathname;
-otherwise the folder directory is found relative to the user's home
-directory.
-.Pp
-The directory name may also refer to an IMAP account;
-all names that begin with `+' refer to IMAP mailboxes on that account,
-then.
-An IMAP folder is normally given in the form
-.Pp
-.Dl imaps://mylogin@imap.myisp.example
-.Pp
-In this case the `+' and `@' prefixes for folder names have the same
-effect (see the
+The same special conventions as documented for the
 .Ic folder
-command).
+command may be used when specifying a new value for
+.Va folder ,
+but be aware that the expansion is fully performed immediately.
+E.g., if the expanded name refers to an IMAP account, all names that
+begin with `+' refer to IMAP mailboxes below the
+.Va folder
+target box.
 .Pp
-Some IMAP servers do not accept the creation of mailboxes in the
-hierarchy base,
-but require that they are created as subfolders of `INBOX'.
-With such servers a folder name of the form
+Note: some IMAP servers do not accept the creation of mailboxes in
+the hierarchy base, but require that they are created as subfolders of
+`INBOX' \(en with such servers a folder name of the form
 .Pp
 .Dl imaps://mylogin@imap.myisp.example/INBOX.
 .Pp
@@ -2806,13 +2794,21 @@ Default is
 .It Va MAIL
 Is used as the user's mailbox, if set.
 Otherwise, a system-dependent default is used.
-Can be a `protocol://' string (see the
+Supports a logical subset of the special conventions that are documented
+for the
 .Ic folder
-command for more information).
+command and the
+.Va folder
+option.
 .It Va MBOX
 The name of the mbox file.
-It can be the name of a folder.
-The default is `mbox' in the user's home directory.
+Supports a logical subset of the special conventions that are documented
+for the
+.Ic folder
+command and the
+.Va folder
+option.
+The fallback default is `mbox' in the user's home directory.
 .It Va mimetypes-load-control
 This option can be used to control which of the
 .Xr mime.types 5

commit e5eb5fe8e67e78a0c46943dc2c09c09de4f6b0c0

Fix + expansion for IMAP, broken since [edb2dcd], 2013-06!!

fio.c
@@ -145,26 +145,8 @@ _findmail(char *buf, size_t bufsize, char const *user, bool_t force)
    char *cp;
    NYD_ENTER;
 
-   if (!strcmp(user, myname) && !force && (cp = ok_vlook(folder)) != NULL) {
-      switch (which_protocol(cp)) {
-      case PROTO_IMAP:{
-         char const *sep;
-         size_t i;
-
-         i = strlen(cp);
-         if(i > 0 && cp[i - 1] != '/'){
-            if(strcmp(cp, protbase(cp)))
-               goto jcopy;
-            sep = "/";
-         }else
-            sep = "";
-         snprintf(buf, bufsize, "%s%sINBOX", cp, sep);
-         goto jleave;
-      }
-      default:
-         break;
-      }
-   }
+   if (!strcmp(user, myname) && !force && (cp = ok_vlook(folder)) != NULL)
+      goto jcopy;
 
    if (force || (cp = ok_vlook(MAIL)) == NULL)
       snprintf(buf, bufsize, "%s/%s", MAILSPOOL, user);
@@ -1344,10 +1326,14 @@ jnext:
    }
 
    if (res[0] == '+' && getfold(cbuf, sizeof cbuf)) {
-      size_t i = strlen(cbuf);
+      size_t i;
 
-      res = str_concat_csvl(&s, cbuf,
-            ((i > 0 && cbuf[i - 1] == '/') ? "" : "/"), res + 1, NULL)->s;
+      i = strlen(cbuf);
+      if(which_protocol(cbuf) != PROTO_IMAP)
+         res = str_concat_csvl(&s, cbuf,
+               ((i > 0 && cbuf[i - 1] == '/') ? "" : "/"), res + 1, NULL)->s;
+      else
+         res = savestrbuf(cbuf, i);
       dyn = TRU1;
 
       if (res[0] == '%' && res[1] == ':') {