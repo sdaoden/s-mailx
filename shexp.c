sdaoden/s-mailx

commit 57744f933671df2f9809ac3bf08397c211bb3c63

Redefine *folder* / getfold->folder_query() / *HOME* (Justin Ellingwood)..
Accidentally i've (blush) searched the internet for "S-nail" and
found a tutorial written by Justin Ellingwood (JustinEllingwood at
gmail dot com) back in May (on Ubuntu/Postfix).  In there he writes

  You should get the following response:

    Output
    Can't canonicalize "/home/sammy/Maildir"

  This is normal and will only show during this first message.

Oh yes, unfortunately it is since we made *folder* a VIP and try
to resolve it (also via realpath(3)) whenever it is changed.  We
did so because of the problem of the BSD Mail / Heirloom mailx
codebase which cannot escape from a file/folder that is opened on
a relative path and that no longer resolved once we need to access
it again, resulting in a completely stuck codebase.  (The real
healing will be Void boxes again, but not before v15.)  Ah ja, and
then also because i don't want to see full path names in the
*prompt* and the plain `file'/`folder' when they reside under
*folder*: they should simply indicate that condition with the
leading "+" that is the syntax for this purpose.  These two
conditions wouldn't come together otherwise.

But now that we have our much improved variable flag handling we
can do a little bit better:

  - keep *folder* a VIP, but don't resolve it immediately once the
    variable is set, instead delay that until it is accessed the
    first time.

  - add a "private" (nodelete,readonly,undocumented) variable
    *-folder-resolved* that will keep track of that state.

  - since *folder* correlates to *HOME* handle that in the VIP
    handling.

  - drop the global "homedir" variable, go for vlook(HOME)
    instead (while here).

  - remove the global fio.c:var_folder_updated() function, merge
    that into the folder_query() that replaces the old getfold().

  - try to use realpath(, NULL) if possible: extend the mk-conf.sh
    test accordingly.

shexp.c
@@ -57,8 +57,7 @@ struct shvar_stack {
 };
 
 /* Locate the user's mailbox file (where new, unread mail is queued) */
-static void       _findmail(char *buf, size_t bufsize, char const *user,
-                     bool_t force);
+static char * _findmail(char const *user, bool_t force);
 
 /* Perform shell meta character expansion TODO obsolete (INSECURE!) */
 static char *     _globname(char const *name, enum fexp_mode fexpm);
@@ -66,24 +65,24 @@ static char *     _globname(char const *name, enum fexp_mode fexpm);
 /* Perform shell variable expansion */
 static char *  _sh_exp_var(struct shvar_stack *shsp);
 
-static void
-_findmail(char *buf, size_t bufsize, char const *user, bool_t force)
+static char *
+_findmail(char const *user, bool_t force)
 {
+   char *rv;
    char const *cp;
    NYD_ENTER;
 
-   if (!force && !strcmp(user, myname) && (cp = ok_vlook(folder)) != NULL) {
-      ;
-   }
+   if (force || (cp = ok_vlook(MAIL)) == NULL) {
+      size_t ul = strlen(user), i = sizeof(MAILSPOOL) -1 + 1 + ul +1;
 
-   if (force || (cp = ok_vlook(MAIL)) == NULL)
-      snprintf(buf, bufsize, "%s/%s", MAILSPOOL, user); /* TODO */
-   else {
-      char const *exp = fexpand(cp, FEXP_NSHELL);
-
-      n_strscpy(buf, (exp != NULL ? exp : cp), bufsize);
-   }
+      rv = salloc(i);
+      memcpy(rv, MAILSPOOL, i = sizeof(MAILSPOOL));
+      rv[i] = '/';
+      memcpy(&rv[++i], user, ul +1);
+   } else if ((rv = fexpand(cp, FEXP_NSHELL)) == NULL)
+      rv = savestr(cp);
    NYD_LEAVE;
+   return rv;
 }
 
 static char *
@@ -339,9 +338,8 @@ jrecurse:
 FL char *
 fexpand(char const *name, enum fexp_mode fexpm)
 {
-   char cbuf[PATH_MAX +1];
-   char const *res;
    struct str s;
+   char const *cp, *res;
    bool_t dyn;
    NYD_ENTER;
 
@@ -364,9 +362,7 @@ jnext:
          res = &res[2];
          goto jnext;
       }
-      _findmail(cbuf, sizeof cbuf, (res[1] != '\0' ? res + 1 : myname),
-         (res[1] != '\0'));
-      res = cbuf;
+      res = _findmail((res[1] != '\0' ? res + 1 : myname), (res[1] != '\0'));
       goto jislocal;
    case '#':
       if (res[1] != '\0')
@@ -384,13 +380,15 @@ jnext:
       break;
    }
 
-   if (res[0] == '+' && getfold(cbuf, sizeof cbuf)) {
-      size_t i = strlen(cbuf);
+   /* POSIX: if *folder* unset or null, "+" shall be retained */
+   if (*res == '+' && *(cp = folder_query()) != '\0') {
+      size_t i = strlen(cp);
 
-      res = str_concat_csvl(&s, cbuf,
-            ((i > 0 && cbuf[i - 1] == '/') ? "" : "/"), res + 1, NULL)->s;
+      res = str_concat_csvl(&s, cp,
+            ((i == 0 || cp[i -1] == '/') ? "" : "/"), res + 1, NULL)->s;
       dyn = TRU1;
 
+      /* TODO *folder* can't start with %[:], can it!?! */
       if (res[0] == '%' && res[1] == ':') {
          res += 2;
          goto jnext;
@@ -474,7 +472,7 @@ n_shell_expand_tilde(char const *s, bool_t *err_or_null)
       goto jasis;
 
    if (*(rp = s + 1) == '/' || *rp == '\0')
-      np = homedir;
+      np = ok_vlook(HOME);
    else {
       if ((rp = strchr(s + 1, '/')) == NULL)
          rp = (np = UNCONST(s)) + 1;

accmacvar.c
@@ -688,34 +688,27 @@ a_amv_var_free(char *cp){
 
 static bool_t
 a_amv_var_check_vips(enum okeys okey, bool_t enable, char **val){
-   char *cp;
    int flag;
    bool_t ok;
    NYD2_ENTER;
 
    ok = TRU1;
    flag = 0;
-   cp = NULL;
 
    switch(okey){
    case ok_b_debug:
       flag = OPT_DEBUG;
       break;
-   case ok_v_folder:
-      ok = (val != NULL && var_folder_updated(*val, &cp));
-      if(ok && cp != NULL){
-         a_amv_var_free(*val);
-         /* Ensure we don't leak later on */
-         if(*cp == '\0'){
-            *val = UNCONST("");
-            free(cp);
-         }else
-            *val = cp;
-      }
-      break;
    case ok_v_HOME:
-      assert(enable);
-      homedir = *val; /* XXX replace users with ok_vlook(HOME) */
+      /* Invalidate any resolved folder then, too
+       * FALLTHRU */
+   case ok_v_folder:
+      ok = !(pstate & PS_ROOT);
+      pstate |= PS_ROOT;
+      ok_vclear(_folder_resolved);
+      if(ok)
+         pstate &= ~PS_ROOT;
+      ok = TRU1;
       break;
    case ok_b_header:
       flag = OPT_N_FLAG;

cmd1.c
@@ -1454,21 +1454,20 @@ jleave:
 FL int
 c_folders(void *v)
 {
-   char dirname[PATH_MAX], *name, **argv = v;
-   int rv = 1;
+   char const *cp;
+   char **argv;
+   int rv;
    NYD_ENTER;
 
-   if (*argv) {
-      name = expand(*argv);
-      if (name == NULL)
-         goto jleave;
-   } else if (!getfold(dirname, sizeof dirname)) {
-      n_err(_("No value set for \"folder\"\n"));
-      goto jleave;
-   } else
-      name = dirname;
+   rv = 1;
 
-   run_command(ok_vlook(LISTER), 0, COMMAND_FD_PASS, COMMAND_FD_PASS, name,
+   if(*(argv = v) != NULL){
+      if((cp = fexpand(*argv, FEXP_NSHELL | FEXP_LOCAL)) == NULL)
+         goto jleave;
+   }else
+      cp = folder_query();
+
+   run_command(ok_vlook(LISTER), 0, COMMAND_FD_PASS, COMMAND_FD_PASS, cp,
       NULL, NULL, NULL);
 jleave:
    NYD_LEAVE;

cmd3.c
@@ -805,7 +805,7 @@ c_chdir(void *v)
    NYD_ENTER;
 
    if (*arglist == NULL)
-      cp = homedir;
+      cp = ok_vlook(HOME);
    else if ((cp = file_expand(*arglist)) == NULL)
       goto jleave;
    if (chdir(cp) == -1) {

fio.c
@@ -367,69 +367,6 @@ fsize(FILE *iob)
    return rv;
 }
 
-FL bool_t
-var_folder_updated(char const *name, char **store)
-{
-   char rv = TRU1;
-   char *folder, *unres = NULL, *res = NULL;
-   NYD_ENTER;
-
-   if ((folder = UNCONST(name)) == NULL)
-      goto jleave;
-
-   /* Expand the *folder*; skip %: prefix for simplicity of use */
-   /* XXX This *only* works because we do NOT
-    * XXX update environment variables via the "set" mechanism */
-   if (folder[0] == '%' && folder[1] == ':')
-      folder += 2;
-   if ((folder = fexpand(folder, FEXP_FULL)) == NULL) /* XXX error? */
-      goto jleave;
-
-   switch (which_protocol(folder)) {
-   case PROTO_POP3:
-      n_err(_("*folder* cannot be set to a flat, readonly POP3 account\n"));
-      rv = FAL0;
-      goto jleave;
-   default:
-      /* Further expansion desired */
-      break;
-   }
-
-   /* All non-absolute paths are relative to our home directory */
-   if (*folder != '/') {
-      size_t l1 = strlen(homedir), l2 = strlen(folder);
-      unres = ac_alloc(l1 + l2 + 1 +1);
-      memcpy(unres, homedir, l1);
-      unres[l1] = '/';
-      memcpy(unres + l1 + 1, folder, l2);
-      unres[l1 + 1 + l2] = '\0';
-      folder = unres;
-   }
-
-   /* Since lex.c:_update_mailname() uses realpath(3) if available to
-    * avoid that we loose track of our currently open folder in case we
-    * chdir away, but still checks the leading path portion against
-    * getfold() to be able to abbreviate to the +FOLDER syntax if
-    * possible, we need to realpath(3) the folder, too */
-#ifdef HAVE_REALPATH
-   res = ac_alloc(PATH_MAX +1);
-   if (realpath(folder, res) == NULL)
-      n_err(_("Can't canonicalize \"%s\"\n"), folder);
-   else
-      folder = res;
-#endif
-
-   *store = sstrdup(folder);
-
-   if (res != NULL)
-      ac_free(res);
-   if (unres != NULL)
-      ac_free(unres);
-jleave:
-   NYD_LEAVE;
-   return rv;
-}
-
 FL char const *
 getdeadletter(void) /* XXX should that be in auxlily.c? */
 {

folder.c
@@ -81,7 +81,7 @@ __narrow_suffix(char const *cp, size_t cpl, size_t maxl)
 static bool_t
 _update_mailname(char const *name)
 {
-   char tbuf[PATH_MAX], *mailp, *dispp;
+   char *mailp, *dispp;
    size_t i, j;
    bool_t rv = TRU1;
    NYD_ENTER;
@@ -90,8 +90,9 @@ _update_mailname(char const *name)
    if (name != NULL) {
 #ifdef HAVE_REALPATH
       enum protocol p = which_protocol(name);
+
       if (p == PROTO_FILE || p == PROTO_MAILDIR) {
-         if (realpath(name, mailname) == NULL) {
+         if (realpath(name, mailname) == NULL && errno != ENOENT) {
             n_err(_("Can't canonicalize \"%s\"\n"), name);
             rv = FAL0;
             goto jdocopy;
@@ -106,13 +107,17 @@ jdocopy:
    dispp = displayname;
 
    /* Don't display an absolute path but "+FOLDER" if under *folder* */
-   if (getfold(tbuf, sizeof tbuf)) {
-      i = strlen(tbuf);
-      if (i < sizeof(tbuf) -1)
-         tbuf[i++] = '/';
-      if (!strncmp(tbuf, mailp, i)) {
-         mailp += i;
-         *dispp++ = '+';
+   /* C99 */{
+      char const *folderp;
+
+      if(*(folderp = folder_query()) != '\0'){
+         i = strlen(folderp);
+         if(!strncmp(folderp, mailp, i)){
+            if(folderp[i -1] != '/' && mailp[i] == '/') /* XXX DIRSEP magic */
+               ++i;
+            mailp += i;
+            *dispp++ = '+';
+         }
       }
    }
 
@@ -156,13 +161,27 @@ setfile(char const *name, enum fedit_mode fm) /* TODO oh my god */
 
    pstate &= ~PS_SETFILE_OPENED;
 
-   if (name[0] == '%' || ((who = shortcut_expand(name)) != NULL && *who == '%'))
-      fm |= FEDIT_SYSBOX; /* TODO fexpand() needs to tell is-valid-user! */
+   /* C99 */{
+      enum fexp_mode fexpm;
 
-   who = (name[1] != '\0') ? name + 1 : myname;
+      if((who = shortcut_expand(name)) != NULL){
+         fexpm = FEXP_NSHORTCUT | FEXP_NSHELL;
+         name = who;
+      }else
+         fexpm = FEXP_NSHELL;
 
-   if ((name = expand(name)) == NULL)
-      goto jem1;
+      if(name[0] == '%'){
+         fm |= FEDIT_SYSBOX; /* TODO fexpand() needs to tell is-valid-user! */
+         if(*(who = &name[1]) == ':')
+            ++who;
+         if(*who == '\0')
+            who = myname;
+      }else
+         who = myname;
+
+      if ((name = fexpand(name, fexpm)) == NULL)
+         goto jem1;
+   }
 
    switch (which_protocol(name)) {
    case PROTO_FILE:
@@ -171,9 +190,19 @@ setfile(char const *name, enum fedit_mode fm) /* TODO oh my god */
       isdevnull = ((options & OPT_BATCH_FLAG) && !strcmp(name, "/dev/null"));
 #ifdef HAVE_REALPATH
       do { /* TODO we need objects, goes away then */
-         char ebuf[PATH_MAX];
-         if (realpath(name, ebuf) != NULL)
-            name = savestr(ebuf);
+# ifdef HAVE_REALPATH_NULL
+         char *cp;
+
+         if ((cp = realpath(name, NULL)) != NULL) {
+            name = savestr(cp);
+            (free)(cp);
+         }
+# else
+         char cbuf[PATH_MAX];
+
+         if (realpath(name, cbuf) != NULL)
+            name = savestr(cbuf);
+# endif
       } while (0);
 #endif
       rv = 1;
@@ -664,16 +693,111 @@ initbox(char const *name)
    NYD_LEAVE;
 }
 
-FL bool_t
-getfold(char *name, size_t size)
-{
-   char const *folder;
+FL char const *
+folder_query(void){
+   struct n_string s, *sp = &s;
+   char *cp;
+   char const *rv;
+   bool_t err;
    NYD_ENTER;
 
-   if ((folder = ok_vlook(folder)) != NULL)
-      n_strscpy(name, folder, size);
+   /* *folder* is linked with *_folder_resolved*: we only use the latter */
+   for(err = FAL0;;){
+      if((rv = ok_vlook(_folder_resolved)) != NULL)
+         break;
+
+      /* POSIX says:
+       *    If directory does not start with a <slash> ('/'), the contents
+       *    of HOME shall be prefixed to it.
+       * And:
+       *    If folder is unset or set to null, [.] filenames beginning with
+       *    '+' shall refer to files in the current directory.
+       * We may have the result already */
+      rv = "";
+      err = FAL0;
+
+      if((cp = ok_vlook(folder)) == NULL)
+         goto jset;
+
+      /* Expand the *folder*; skip %: prefix for simplicity of use */
+      if(cp[0] == '%' && cp[1] == ':')
+         cp += 2;
+      if((err = (cp = fexpand(cp, FEXP_NSHELL)) == NULL) || *cp == '\0')
+         goto jset;
+
+      switch(which_protocol(cp)){
+      case PROTO_POP3:
+         n_err(_("*folder* can't be set to a flat, readonly POP3 account\n"));
+         err = TRU1;
+         goto jset;
+      default:
+         /* Further expansion desired */
+         break;
+      }
+
+      /* Prefix HOME as necessary */
+      if(*cp != '/'){
+         char const *home = ok_vlook(HOME);
+         size_t l1 = strlen(home), l2 = strlen(cp);
+
+         sp = n_string_creat_auto(sp);
+         sp = n_string_reserve(sp, l1 + 1 + l2 +1);
+         sp = n_string_push_buf(sp, home, l1);
+         sp = n_string_push_c(sp, '/');
+         sp = n_string_push_buf(sp, cp, l2);
+         cp = n_string_cp(sp);
+      }
+
+      rv = cp;
+
+      /* TODO Since our visual mailname is resolved via realpath(3) if available
+       * TODO to avoid that we loose track of our currently open folder in case
+       * TODO we chdir away, but still checks the leading path portion against
+       * TODO fold_query() to be able to abbreviate to the +FOLDER syntax if
+       * TODO possible, we need to realpath(3) the folder, too */
+#ifdef HAVE_REALPATH
+      /* C99 */{
+# ifndef HAVE_REALPATH_NULL
+         if(cp == sp->s_dat)
+            sp = n_string_drop_ownership(sp);
+         sp = n_string_reserve(sp, PATH_MAX +1);
+# else
+         sp->s_dat = NULL;
+# endif
+
+         if((sp->s_dat = realpath(cp, sp->s_dat)) != NULL){
+# ifdef HAVE_REALPATH_NULL
+            sp->s_dat = savestr(cp = sp->s_dat);
+            (free)(cp);
+# endif
+            rv = sp->s_dat;
+         }else if(errno == ENOENT)
+            rv = cp;
+         else{
+            n_err(_("Can't canonicalize *folder*: \"%s\"\n"), cp);
+            err = TRU1;
+            rv = "";
+         }
+      }
+#endif /* HAVE_REALPATH */
+
+jset:
+      /* C99 */{
+         bool_t reset = !(pstate & PS_ROOT);
+
+         pstate |= PS_ROOT;
+         ok_vset(_folder_resolved, rv);
+         if(reset)
+            pstate &= ~PS_ROOT;
+      }
+   }
+
+   if(err){
+      n_err(_("*folder* is not resolvable, using CWD\n"));
+      assert(rv != NULL && *rv == '\0');
+   }
    NYD_LEAVE;
-   return (folder != NULL);
+   return rv;
 }
 
 /* s-it-mode */

mk-conf.sh
@@ -1418,21 +1418,30 @@ int main(void){
 !
 fi # have_setlocale
 
-run_check realpath 'realpath(3)' '#define HAVE_REALPATH' << \!
+if run_check realpath 'realpath(3)' '#define HAVE_REALPATH' << \!
 #include <stdlib.h>
 int main(void){
-#if 1 /* TODO for now we use realpath(3) without NULL as 2nd arg! */
-   /* (And note that on Linux tcc(1) otherwise didn't detect once tested! */
    char x_buf[4096], *x = realpath(".", x_buf);
 
    return (x != NULL) ? 0 : 1;
-#else
-   char *x = realpath(".", NULL), *y = realpath("/", NULL);
-
-   return (x != NULL && y != NULL) ? 0 : 1;
-#endif
 }
 !
+then
+   if run_check realpath_malloc 'realpath(3) takes NULL' \
+         '#define HAVE_REALPATH_NULL' << \!
+#include <stdlib.h>
+int main(void){
+   char *x = realpath(".", NULL);
+
+   if(x != NULL)
+      free(x);
+   return (x != NULL) ? 0 : 1;
+}
+!
+   then
+      :
+   fi
+fi
 
 link_check wordexp 'wordexp(3)' '#define HAVE_WORDEXP' << \!
 #include <stdio.h> /* For C89 NULL */

nail.1
@@ -5823,15 +5823,20 @@ set file-hook-load-xy='echo >&2 XY-LOAD; gzip -cd' \e
 .
 .Mx
 .It Va folder
-The name of the directory to use for storing folders of messages.
-All folder names that begin with
+The default path under which mailboxes are to be saved:
+file names that begin with the plus-sign
 .Ql +
-refer to files below it.
-The same special conventions as documented for the
+will be expanded by prefixing them with the value of this variable.
+The same special syntax conventions as documented for the
 .Ic file
-command may be used when specifying a new value for
-.Va \&\&folder ,
-but be aware that the expansion is fully performed immediately.
+command may be used; if the non-empty value doesn't start with a slash
+.Ql / ,
+then the value of
+.Ev HOME
+will be prefixed automatically.
+If unset or the empty string any
+.Ql +
+prefixing file names will remain unexpanded.
 .
 .Mx
 .It Va folder-hook

commit dfc5dacf0174f5f3ca459692b984e570f3f6202c

Fix off-by-one introduced in [57744f9] (Redefine *folder* ..)..
This bug introduced on 2016-06-02 wasn't even recognized after
doing [053e19f] (Add *inbox* ..) on 2016-10-06.  All the testing
on [stable/v14.8] it seems.  We would need much better tests,
and it seems i have never tried 's-nail -Ru root' for a while...

shexp.c
@@ -163,7 +163,7 @@ a_shexp_findmail(char const *user, bool_t force){
       i = sizeof(VAL_MAIL) -1 + 1 + ul;
 
       rv = salloc(i);
-      memcpy(rv, VAL_MAIL, i = sizeof(VAL_MAIL));
+      memcpy(rv, VAL_MAIL, (i = sizeof(VAL_MAIL) -1));
       rv[i] = '/';
       memcpy(&rv[++i], user, ul);
    }