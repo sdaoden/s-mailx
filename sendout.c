7cd202d71a2ae6713ce5609146d89e00fae56e7f
Steffen (Daode) Nurpmeso <steffen@sdaoden.eu>
2017-01-19 16:39:05 +0100

    Fix constant warning, real code will not make it to v14.9.0

--- sendout.c
+++ sendout.c
@@ -1131,9 +1131,9 @@ _transfer(struct sendbundle *sbp)
    NYD_ENTER;
 
    for (cnt = 0, np = sbp->sb_to; np != NULL;) {
-      char const k[] = "smime-encrypt-";
+      char const k[] = "smime-encrypt-", *cp;
       size_t nl = strlen(np->n_name);
-      char *cp, *vs = ac_alloc(sizeof(k)-1 + nl +1);
+      char *vs = ac_alloc(sizeof(k)-1 + nl +1);
       memcpy(vs, k, sizeof(k) -1);
       memcpy(vs + sizeof(k) -1, np->n_name, nl +1);
 
