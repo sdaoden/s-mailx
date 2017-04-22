d2bd74736da26966c9c5a1a57ff184856c7d73bc
Steffen (Daode) Nurpmeso <steffen@sdaoden.eu>
2017-04-23 00:51:37 +0200

    sendout.c:_sendbundle_setup_creds(): fix !HAVE_SMTP (CONFIG=NULL tests)

--- sendout.c
+++ sendout.c
@@ -428,7 +428,9 @@ _sendbundle_setup_creds(struct sendbundle *sbp, bool_t signing_caps)
          sbp->sb_signer.l = strlen(sbp->sb_signer.s = from);
    }
 
-#ifdef HAVE_SMTP
+#ifndef HAVE_SMTP
+   rv = TRU1;
+#else
    if ((smtp = ok_vlook(smtp)) == NULL) {
       rv = TRU1;
       goto jleave;
