sdaoden/s-mailx

commit 096e6021d5368319db23f4f9d5d4ad0f23a4a1a6

Allow Base64 and QP en- and decoding to fail

smtp.c
@@ -140,7 +140,7 @@ do {\
 } while (0)
 
 static bool_t
-_smtp_talk(struct sock *sp, struct sendbundle *sbp)
+_smtp_talk(struct sock *sp, struct sendbundle *sbp) /* TODO n_string etc. */
 {
    char o[LINESIZE], *hostname;
    struct smtp_line _sl, *slp = &_sl;
@@ -193,39 +193,62 @@ _smtp_talk(struct sock *sp, struct sendbundle *sbp)
    default:
       /* FALLTHRU (doesn't happen) */
    case AUTHTYPE_PLAIN:
+      cnt = sbp->sb_ccred.cc_user.l;
+      if(sbp->sb_ccred.cc_pass.l >= UIZ_MAX - 2 ||
+            cnt >= UIZ_MAX - 2 - sbp->sb_ccred.cc_pass.l){
+jerr_cred:
+         n_err(_("Credentials overflow buffer sizes\n"));
+         goto jleave;
+      }
+      cnt += sbp->sb_ccred.cc_pass.l;
+
+      if(cnt >= sizeof(o) - 2)
+         goto jerr_cred;
+      cnt += 2;
+      if(b64_encode_calc_size(cnt) == UIZ_MAX)
+         goto jerr_cred;
+
       _OUT(NETLINE("AUTH PLAIN"));
       _ANSWER(3, FAL0, FAL0);
 
       snprintf(o, sizeof o, "%c%s%c%s",
          '\0', sbp->sb_ccred.cc_user.s, '\0', sbp->sb_ccred.cc_pass.s);
-      b64_encode_buf(&b64, o,
-         sbp->sb_ccred.cc_user.l + sbp->sb_ccred.cc_pass.l + 2,
-         B64_SALLOC | B64_CRLF);
+      b64_encode_buf(&b64, o, cnt, B64_SALLOC | B64_CRLF);
       _OUT(b64.s);
       _ANSWER(2, FAL0, FAL0);
       break;
    case AUTHTYPE_LOGIN:
+      if(b64_encode_calc_size(sbp->sb_ccred.cc_user.l) == UIZ_MAX ||
+            b64_encode_calc_size(sbp->sb_ccred.cc_pass.l) == UIZ_MAX)
+         goto jerr_cred;
+
       _OUT(NETLINE("AUTH LOGIN"));
       _ANSWER(3, FAL0, FAL0);
 
-      b64_encode_cp(&b64, sbp->sb_ccred.cc_user.s, B64_SALLOC | B64_CRLF);
+      b64_encode_buf(&b64, sbp->sb_ccred.cc_user.s, sbp->sb_ccred.cc_user.l,
+         B64_SALLOC | B64_CRLF);
       _OUT(b64.s);
       _ANSWER(3, FAL0, FAL0);
 
-      b64_encode_cp(&b64, sbp->sb_ccred.cc_pass.s, B64_SALLOC | B64_CRLF);
+      b64_encode_buf(&b64, sbp->sb_ccred.cc_pass.s, sbp->sb_ccred.cc_pass.l,
+         B64_SALLOC | B64_CRLF);
       _OUT(b64.s);
       _ANSWER(2, FAL0, FAL0);
       break;
 #ifdef HAVE_MD5
-   case AUTHTYPE_CRAM_MD5:
+   case AUTHTYPE_CRAM_MD5:{
+      char *cp;
+
+      if((cp = cram_md5_string(&sbp->sb_ccred.cc_user, &sbp->sb_ccred.cc_pass,
+            slp->dat)) == NULL)
+         goto jerr_cred;
+
       _OUT(NETLINE("AUTH CRAM-MD5"));
       _ANSWER(3, FAL0, TRU1);
-      {  char *cp = cram_md5_string(&sbp->sb_ccred.cc_user,
-               &sbp->sb_ccred.cc_pass, slp->dat);
-         _OUT(cp);
-      }
+
+      _OUT(cp);
       _ANSWER(2, FAL0, FAL0);
-      break;
+   }  break;
 #endif
 #ifdef HAVE_GSSAPI
    case AUTHTYPE_GSSAPI:

auxlily.c
@@ -652,6 +652,7 @@ getrandstring(size_t length)
    }
 #endif
 
+   assert(length + 3 < UIZ_MAX / 4);
    b64_encode_buf(&b64, data, length + 3,
       B64_SALLOC | B64_RFC4648URL | B64_NOPAD);
    ac_free(data);

mime.c
@@ -417,10 +417,19 @@ jenc_retry:
          cin.s = n_UNCONST(wbot);
          cin.l = PTR2SIZE(wend - wbot);
 
-         if (flags & _ENC_B64)
-            j = b64_encode(&cout, &cin, B64_ISHEAD | B64_ISENCWORD)->l;
-         else
-            j = qp_encode(&cout, &cin, QP_ISHEAD | QP_ISENCWORD)->l;
+         /* C99 */{
+            struct str *xout;
+
+            if(flags & _ENC_B64)
+               xout = b64_encode(&cout, &cin, B64_ISHEAD | B64_ISENCWORD);
+            else
+               xout = qp_encode(&cout, &cin, QP_ISHEAD | QP_ISENCWORD);
+            if(xout == NULL){
+               sz = -1;
+               break;
+            }
+            j = xout->l;
+         }
          /* (Avoid trigraphs in the RFC 2047 placeholder..) */
          i = j + (flags & _8BIT ? cset8_len : cset7_len) + sizeof("=!!B!!=") -1;
          if (*wcur != '\0')
@@ -912,13 +921,10 @@ mime_fromhdr(struct str const *in, struct str *out, enum tdflags flags)
          cout.s = NULL;
          cout.l = 0;
          if (convert == CONV_FROMB64) {
-            /* XXX Take care for, and strip LF from
-             * XXX [Invalid Base64 encoding ignored] */
-            if (b64_decode(&cout, &cin, NULL) == STOP &&
-                  cout.s[cout.l - 1] == '\n')
-               --cout.l;
-         } else
-            qp_decode(&cout, &cin, NULL);
+            if(!b64_decode(&cout, &cin, NULL))
+               n_str_assign_cp(&cout, _("[Invalid Base64 encoding]"));
+         }else if(!qp_decode(&cout, &cin, NULL))
+            n_str_assign_cp(&cout, _("[Invalid Quoted-Printable encoding]"));
 
          out->l = lastenc;
 #ifdef HAVE_ICONV
@@ -1108,10 +1114,19 @@ jconvert:
 
    switch (convert) {
    case CONV_FROMQP:
-      state = qp_decode(&out, &in, rest);
+      if(!qp_decode(&out, &in, rest)){
+         n_err(_("Invalid Quoted-Printable encoding ignored\n"));
+         sz = 0;
+         state = STOP; /* TODO sz = -1 stops outer levels! */
+         break;
+      }
+      state = OKAY;
       goto jqpb64_dec;
    case CONV_TOQP:
-      qp_encode(&out, &in, QP_NONE);
+      if(qp_encode(&out, &in, QP_NONE) == NULL){
+         sz = -1;
+         break;
+      }
       goto jqpb64_enc;
    case CONV_8BIT:
       sz = quoteflt_push(qf, in.s, in.l);
@@ -1120,7 +1135,13 @@ jconvert:
       rest = NULL;
       /* FALLTHRU */
    case CONV_FROMB64_T:
-      state = b64_decode(&out, &in, rest);
+      if(!b64_decode(&out, &in, rest)){
+         n_err(_("Invalid Base64 encoding ignored\n"));
+         sz = 0;
+         state = STOP; /* TODO sz = -1 stops outer levels! */
+         break;
+      }
+      state = OKAY;
 jqpb64_dec:
       if ((sz = out.l) != 0) {
          ui32_t opl = qf->qf_pfix_len;
@@ -1133,7 +1154,10 @@ jqpb64_dec:
          sz = -1;
       break;
    case CONV_TOB64:
-      b64_encode(&out, &in, B64_LF | B64_MULTILINE);
+      if(b64_encode(&out, &in, B64_LF | B64_MULTILINE) == NULL){
+         sz = -1;
+         break;
+      }
 jqpb64_enc:
       sz = fwrite(out.s, sizeof *out.s, out.l, f);
       if (sz != (ssize_t)out.l)

mime_enc.c
@@ -389,18 +389,32 @@ qp_encode_calc_size(size_t len)
     *    }' |
     *    MAILRC=/dev/null LC_ALL=en_US.UTF-8 s-nail -nvvd \
     *       -Ssendcharsets=utf8 -s testsub ./LETTER */
+
+   if(len >= UIZ_MAX / 3){
+      len = UIZ_MAX;
+      goto jleave;
+   }
    bytes = len * 3;
    lines = bytes / QP_LINESIZE;
    len += lines;
 
-   bytes = len * 3;
    /* Trailing hard NL may be missing, so there may be two lines.
     * Thus add soft + hard NL per line and a trailing NUL */
+   if(len >= UIZ_MAX / 3){
+      len = UIZ_MAX;
+      goto jleave;
+   }
+   bytes = len * 3;
    lines = (bytes / QP_LINESIZE) + 1;
    lines <<= 1;
+   ++bytes;
+   /*if(UIZ_MAX - bytes >= lines){
+      len = UIZ_MAX;
+      goto jleave;
+   }*/
    bytes += lines;
-   len = ++bytes;
-
+   len = bytes;
+jleave:
    NYD_LEAVE;
    return len;
 }
@@ -438,13 +452,16 @@ FL struct str *
 qp_encode(struct str *out, struct str const *in, enum qpflags flags)
 {
    bool_t sol = (flags & QP_ISHEAD ? FAL0 : TRU1), seenx;
-   ssize_t lnlen;
+   size_t lnlen;
    char *qp;
    char const *is, *ie;
    NYD_ENTER;
 
-   if (!(flags & QP_BUF)) {
-      lnlen = qp_encode_calc_size(in->l);
+   if(!(flags & QP_BUF)){
+      if((lnlen = qp_encode_calc_size(in->l)) == UIZ_MAX){
+         out = NULL;
+         goto jerr;
+      }
       out->s = (flags & QP_SALLOC) ? salloc(lnlen) : srealloc(out->s, lnlen);
    }
    qp = out->s;
@@ -530,23 +547,31 @@ jsoftnl:
 jleave:
    out->l = PTR2SIZE(qp - out->s);
    out->s[out->l] = '\0';
+jerr:
    NYD_LEAVE;
    return out;
 }
 
-FL int
+FL bool_t
 qp_decode(struct str *out, struct str const *in, struct str *rest)
 {
-   int rv = STOP;
    char *os, *oc;
    char const *is, *ie;
+   bool_t rv;
    NYD_ENTER;
+
+   rv = FAL0;
 
    if (rest != NULL && rest->l != 0) {
       os = out->s;
       *out = *rest;
       rest->s = os;
       rest->l = 0;
+   }
+
+   if(UIZ_MAX - 3 - out->l <= in->l){
+      out->l = 0;
+      goto jerr;
    }
 
    oc = os =
@@ -651,7 +676,8 @@ jsoftnl:
    /* XXX RFC: QP decode should check no trailing WS on line */
 jleave:
    out->l = PTR2SIZE(oc - os);
-   rv = OKAY;
+   rv = TRU1;
+jerr:
    NYD_LEAVE;
    return rv;
 }
@@ -660,9 +686,13 @@ FL size_t
 b64_encode_calc_size(size_t len)
 {
    NYD_ENTER;
-   len = (len * 4) / 3;
-   len += (((len / B64_ENCODE_INPUT_PER_LINE) + 1) * 3);
-   len += 2 + 1; /* CRLF, \0 */
+   if(len >= UIZ_MAX / 4)
+      len = UIZ_MAX;
+   else{
+      len = (len * 4) / 3;
+      len += (((len / B64_ENCODE_INPUT_PER_LINE) + 1) * 3);
+      len += 2 + 1; /* CRLF, \0 */
+   }
    NYD_LEAVE;
    return len;
 }
@@ -671,7 +701,7 @@ FL struct str *
 b64_encode(struct str *out, struct str const *in, enum b64flags flags)
 {
    ui8_t const *p;
-   ssize_t i, lnlen;
+   size_t i, lnlen;
    char *b64;
    NYD_ENTER;
 
@@ -680,8 +710,12 @@ b64_encode(struct str *out, struct str const *in, enum b64flags flags)
 
    p = (ui8_t const*)in->s;
 
-   if (!(flags & B64_BUF)) {
-      i = b64_encode_calc_size(in->l);
+   if(!(flags & B64_BUF)){
+      if((i = b64_encode_calc_size(in->l)) == UIZ_MAX){
+         out->l = 0; /* XXX legacy unless n_string + callers error adjusted */
+         out = NULL;
+         goto jleave;
+      }
       out->s = (flags & B64_SALLOC) ? salloc(i) : srealloc(out->s, i);
    }
    b64 = out->s;
@@ -689,7 +723,7 @@ b64_encode(struct str *out, struct str const *in, enum b64flags flags)
    if (!(flags & (B64_CRLF | B64_LF)))
       flags &= ~B64_MULTILINE;
 
-   for (lnlen = 0, i = (ssize_t)in->l; i > 0; p += 3, i -= 3) {
+   for (lnlen = 0, i = in->l; (ssize_t)i > 0; p += 3, i -= 3) {
       ui32_t a = p[0], b, c;
 
       b64[0] = _b64_enctbl[a >> 2];
@@ -754,6 +788,7 @@ b64_encode(struct str *out, struct str const *in, enum b64flags flags)
          else if (c == '/')
                *b64 = '_';
    }
+jleave:
    NYD_LEAVE;
    return out;
 }
@@ -787,13 +822,13 @@ b64_encode_cp(struct str *out, char const *cp, enum b64flags flags)
 }
 #endif
 
-FL int
+FL bool_t
 b64_decode(struct str *out, struct str const *in, struct str *rest)
 {
    struct str work;
    char *x;
    size_t len;
-   int rv; /* XXX -> bool_t */
+   bool_t rv;
    NYD_ENTER;
 
    len = _b64_decode_prepare(&work, in);
@@ -865,35 +900,29 @@ b64_decode(struct str *out, struct str const *in, struct str *rest)
             }
          }
       }
-      rv = OKAY;
+      out->s[out->l] = '\0';
+      rv = TRU1;
       goto jleave;
    }
 
    /* Ignore an empty input, as may happen for an empty final line */
-   if (work.l == 0) {
+   if (work.l == 0)
       out->s = srealloc(out->s, 1);
-      rv = OKAY;
-   } else if (work.l >= 4 && !(work.l & 3)) {
+   else if (work.l >= 4 && !(work.l & 3)){
       out->s = srealloc(out->s, len +1);
       if ((ssize_t)(len = _b64_decode(out, &work)) < 0)
          goto jerr;
-      rv = OKAY;
-   } else
+   }else
       goto jerr;
 
-jleave:
    out->s[out->l] = '\0';
+   rv = TRU1;
+jleave:
    NYD_LEAVE;
    return rv;
-
-jerr: {
-   char const *err = _("[Invalid Base64 encoding]\n");
-   out->l = len = strlen(err);
-   out->s = srealloc(out->s, len +1);
-   memcpy(out->s, err, len);
-   rv = STOP;
+jerr:
+   rv = FAL0;
    goto jleave;
-   }
 }
 
 /* s-it-mode */

mime_types.c
@@ -685,14 +685,14 @@ jos_leave:
 jdobuf:
       switch (mpp->m_mime_enc) {
       case MIMEE_B64:
-         if (b64_decode(&dec, &in, &rest) == STOP) {
+         if (!b64_decode(&dec, &in, &rest)) {
             mtca.mtca_mtc = _MT_C_HASNUL;
             goto jstopit; /* break;break; */
          }
          break;
       case MIMEE_QP:
          /* Drin */
-         if (qp_decode(&dec, &in, &rest) == STOP) {
+         if (!qp_decode(&dec, &in, &rest)) {
             mtca.mtca_mtc = _MT_C_HASNUL;
             goto jstopit; /* break;break; */
          }

nailfuns.h
@@ -1250,13 +1250,13 @@ FL size_t      mime_enc_mustquote(char const *ln, size_t lnlen,
                   enum mime_enc_flags flags);
 
 /* How much space is necessary to encode len bytes in QP, worst case.
- * Includes room for terminator */
+ * Includes room for terminator, UIZ_MAX on overflow */
 FL size_t      qp_encode_calc_size(size_t len);
 
 /* If flags includes QP_ISHEAD these assume "word" input and use special
  * quoting rules in addition; soft line breaks are not generated.
  * Otherwise complete input lines are assumed and soft line breaks are
- * generated as necessary */
+ * generated as necessary.  Return NULL on error (overflow) */
 FL struct str * qp_encode(struct str *out, struct str const *in,
                   enum qpflags flags);
 #ifdef notyet
@@ -1272,19 +1272,19 @@ FL struct str * qp_encode_buf(struct str *out, void const *vp, size_t vp_len,
  * to decode a header strings and (1) uses special decoding rules and (b)
  * directly produces output.
  * The buffers of out and possibly rest will be managed via srealloc().
- * Returns OKAY. XXX or STOP on error (in which case out is set to an error
- * XXX message); caller is responsible to free buffers */
-FL int         qp_decode(struct str *out, struct str const *in,
+ * Returns FAL0 on error; caller is responsible to free buffers */
+FL bool_t      qp_decode(struct str *out, struct str const *in,
                   struct str *rest);
 
 /* How much space is necessary to encode len bytes in Base64, worst case.
- * Includes room for (CR/LF/CRLF and) terminator */
+ * Includes room for (CR/LF/CRLF and) terminator, UIZ_MAX on overflow */
 FL size_t      b64_encode_calc_size(size_t len);
 
 /* Note these simply convert all the input (if possible), including the
  * insertion of NL sequences if B64_CRLF or B64_LF is set (and multiple thereof
  * if B64_MULTILINE is set).
- * Thus, in the B64_BUF case, better call b64_encode_calc_size() first */
+ * Thus, in the B64_BUF case, better call b64_encode_calc_size() first.
+ * Return NULL on error (overflow; cannot happen for B64_BUF) */
 FL struct str * b64_encode(struct str *out, struct str const *in,
                   enum b64flags flags);
 FL struct str * b64_encode_buf(struct str *out, void const *vp, size_t vp_len,
@@ -1296,11 +1296,10 @@ FL struct str * b64_encode_cp(struct str *out, char const *cp,
 
 /* If rest is set then decoding will assume text input.
  * The buffers of out and possibly rest will be managed via srealloc().
- * Returns OKAY or STOP on error (in which case out is set to an error
- * message); caller is responsible to free buffers.
- * XXX STOP is effectively not returned for text input (rest!=NULL), instead
+ * Returns FAL0 on error; caller is responsible to free buffers.
+ * XXX FAL0 is effectively not returned for text input (rest!=NULL), instead
  * XXX replacement characters are produced for invalid data */
-FL int         b64_decode(struct str *out, struct str const *in,
+FL bool_t      b64_decode(struct str *out, struct str const *in,
                   struct str *rest);
 
 /*
@@ -2437,7 +2436,7 @@ FL int         c_netrc(void *v);
 # define MD5TOHEX_SIZE           32
 FL char *      md5tohex(char hex[MD5TOHEX_SIZE], void const *vp);
 
-/* CRAM-MD5 encode the *user* / *pass* / *b64* combo */
+/* CRAM-MD5 encode the *user* / *pass* / *b64* combo; NULL on overflow error */
 FL char *      cram_md5_string(struct str const *user, struct str const *pass,
                   char const *b64);
 

smtp_gssapi.h
@@ -126,8 +126,13 @@ _smtp_gssapi(struct sock *sp, struct sendbundle *sbp, struct smtp_line *slp)
    gss_ctx_id_t gss_context;
    OM_uint32 maj_stat, min_stat, ret_flags;
    int conf_state;
-   bool_t ok = FAL0;
+   bool_t ok;
    NYD_ENTER;
+
+   ok = FAL0;
+
+   if(INT_MAX - 1 - 4 <= sbp->sb_ccred.cc_user.l)
+      goto j_leave;
 
    send_tok.value = salloc(send_tok.length = sbp->sb_url.url_host.l + 5 +1);
    memcpy(send_tok.value, "smtp@", 5);
@@ -138,7 +143,7 @@ _smtp_gssapi(struct sock *sp, struct sendbundle *sbp, struct smtp_line *slp)
          &target_name);
    if (maj_stat != GSS_S_COMPLETE) {
       _smtp_gssapi_error(send_tok.value, maj_stat, min_stat);
-      goto jleave;
+      goto j_leave;
    }
 
    token_ptr = GSS_C_NO_BUFFER;
@@ -159,8 +164,10 @@ _smtp_gssapi(struct sock *sp, struct sendbundle *sbp, struct smtp_line *slp)
    if (maj_stat != GSS_S_COMPLETE && maj_stat != GSS_S_CONTINUE_NEEDED) {
       _smtp_gssapi_error("initializing GSS context", maj_stat, min_stat);
       gss_release_name(&min_stat, &target_name);
-      goto jleave;
+      goto j_leave;
    }
+   recv_tok.value = out.s = NULL;
+   recv_tok.length = out.l = 0;
 
    _OUT(NETLINE("AUTH GSSAPI"));
    _ANSWER(3, FAL0, FAL0);
@@ -175,7 +182,8 @@ _smtp_gssapi(struct sock *sp, struct sendbundle *sbp, struct smtp_line *slp)
       out.s = NULL;
       in.s = slp->dat;
       in.l = slp->datlen;
-      b64_decode(&out, &in, NULL);
+      if(!b64_decode(&out, &in, NULL))
+         goto jebase64;
       recv_tok.value = out.s;
       recv_tok.length = out.l;
       token_ptr = &recv_tok;
@@ -210,7 +218,13 @@ _smtp_gssapi(struct sock *sp, struct sendbundle *sbp, struct smtp_line *slp)
    out.s = NULL;
    in.s = slp->dat;
    in.l = slp->datlen;
-   b64_decode(&out, &in, NULL);
+   if(!b64_decode(&out, &in, NULL)){
+jebase64:
+      if(out.s != NULL)
+         free(out.s);
+      n_err(_("Invalid base64 encoding from GSSAPI server\n"));
+      goto jleave;
+   }
    recv_tok.value = out.s;
    recv_tok.length = out.l;
    maj_stat = gss_unwrap(&min_stat, gss_context, &recv_tok, &send_tok,
@@ -241,11 +255,12 @@ _smtp_gssapi(struct sock *sp, struct sendbundle *sbp, struct smtp_line *slp)
    b64_encode_buf(&out, recv_tok.value, recv_tok.length, B64_SALLOC | B64_CRLF);
    _OUT(out.s);
    _ANSWER(2, FAL0, FAL0);
-   gss_delete_sec_context(&min_stat, &gss_context, &recv_tok);
-   gss_release_buffer(&min_stat, &recv_tok);
 
    ok = TRU1;
 jleave:
+   gss_delete_sec_context(&min_stat, &gss_context, &recv_tok);
+   gss_release_buffer(&min_stat, &recv_tok);
+j_leave:
    NYD_LEAVE;
    return ok;
 }

urlcrecry.c
@@ -1501,10 +1501,20 @@ cram_md5_string(struct str const *user, struct str const *pass,
    NYD_ENTER;
 
    out.s = NULL;
+   if(user->l >= UIZ_MAX - 1 - MD5TOHEX_SIZE - 1)
+      goto jleave;
+   if(pass->l >= INT_MAX)
+      goto jleave;
+
    in.s = n_UNCONST(b64);
    in.l = strlen(in.s);
-   b64_decode(&out, &in, NULL);
-   assert(out.s != NULL);
+   if(!b64_decode(&out, &in, NULL))
+      goto jleave;
+   if(out.l >= INT_MAX){
+      free(out.s);
+      out.s = NULL;
+      goto jleave;
+   }
 
    hmac_md5((uc_i*)out.s, out.l, (uc_i*)pass->s, pass->l, digest);
    free(out.s);
@@ -1514,9 +1524,11 @@ cram_md5_string(struct str const *user, struct str const *pass,
    in.s = ac_alloc(user->l + 1 + MD5TOHEX_SIZE +1);
    memcpy(in.s, user->s, user->l);
    in.s[user->l] = ' ';
-   memcpy(in.s + user->l + 1, cp, MD5TOHEX_SIZE);
-   b64_encode(&out, &in, B64_SALLOC | B64_CRLF);
+   memcpy(&in.s[user->l + 1], cp, MD5TOHEX_SIZE);
+   if(b64_encode(&out, &in, B64_SALLOC | B64_CRLF) == NULL)
+      out.s = NULL;
    ac_free(in.s);
+jleave:
    NYD_LEAVE;
    return out.s;
 }

commit 4ff74f9589822979c323110978dfdb72167c7646

smtp.c:_smtp_talk(): FIX CRAM-MD5..
it has been broken by [096e6021d] (Allow Base64 and QP en- and
decoding to fail) in November 2016.

smtp.c
@@ -242,13 +242,12 @@ jerr_cred:
    case AUTHTYPE_CRAM_MD5:{
       char *cp;
 
-      if((cp = cram_md5_string(&sbp->sb_ccred.cc_user, &sbp->sb_ccred.cc_pass,
-            slp->dat)) == NULL)
-         goto jerr_cred;
-
       _OUT(NETLINE("AUTH CRAM-MD5"));
       _ANSWER(3, FAL0, TRU1);
 
+      if((cp = cram_md5_string(&sbp->sb_ccred.cc_user, &sbp->sb_ccred.cc_pass,
+            slp->dat)) == NULL)
+         goto jerr_cred;
       _OUT(cp);
       _ANSWER(2, FAL0, FAL0);
    }  break;