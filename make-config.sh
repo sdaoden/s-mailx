sdaoden/s-mailx

commit 7039d7f4c08a6eeb103911105549c030b4edf520

Outsource some constants to new config.h; use mk-* for mk stuff

make-config.sh
@@ -188,15 +188,15 @@ option_update() {
 }
 
 rc=./make.rc
-lst=./config.lst
-ev=./config.ev
-h=./config.h h_name=config.h
-mk=./mk.mk
+lst=./mk-config.lst
+ev=./mk-config.ev
+h=./mk-config.h h_name=mk-config.h
+mk=./mk-config.mk
 
-newlst=./config.lst-new
-newmk=./config.mk-new
-newev=./config.ev-new
-newh=./config.h-new
+newlst=./mk-nconfig.lst
+newmk=./mk-nconfig.mk
+newev=./mk-nconfig.ev
+newh=./mk-nconfig.h
 tmp0=___tmp
 tmp=./${tmp0}1$$
 tmp2=./${tmp0}2$$
@@ -1169,7 +1169,7 @@ if [ -f ${lst} ] && ${cmp} ${newlst} ${lst} >/dev/null 2>&1; then
    exit 0
 elif [ -f ${lst} ]; then
    echo 'Configuration has been updated..'
-   ( eval "${MAKE} -f ./mk.mk clean" )
+   ( eval "${MAKE} -f ./mk-config.mk clean" )
    echo
 else
    echo 'Shiny configuration..'
@@ -1199,16 +1199,16 @@ fi
 ## Compile and link checking
 
 tmp3=./${tmp0}3$$
-log=./config.log
-lib=./config.lib
-inc=./config.inc
-makefile=./config.mk
+log=./mk-config.log
+lib=./mk-config.lib
+inc=./mk-config.inc
+makefile=./${tmp0}.mk
 
 # (No function since some shells loose non-exported variables in traps)
 trap "trap \"\" HUP INT TERM;\
    ${rm} -f ${lst} ${h} ${mk} ${lib} ${inc}; exit 1" HUP INT TERM
 trap "trap \"\" HUP INT TERM EXIT;\
-   ${rm} -rf ${tmp0}.* ${tmp0}* ${makefile}" EXIT
+   ${rm} -rf ${tmp0}.* ${tmp0}*" EXIT
 
 # Time to redefine helper 2
 msg() {
@@ -1250,7 +1250,7 @@ echo '#define VAL_BUILD_OSENV "'"${OSENV}"'"' >> ${h}
 # Generate n_err_number OS mappings
 (
    feat_yes DEVEL && NV= || NV=noverbose
-   TARGET="${h}" awk="${awk}" ./mk-errors.sh ${NV} config
+   TARGET="${h}" awk="${awk}" ./make-errors.sh ${NV} config
 ) |
    xrun_check oserrno 'OS error mapping table generated' || config_exit 1
 
@@ -2606,14 +2606,14 @@ ${mv} ${tmp} ${inc}
 squeeze_em ${lib} ${tmp}
 ${mv} ${tmp} ${lib}
 
-# config.h
+# mk-config.h
 ${mv} ${h} ${tmp}
-printf '#ifndef n_CONFIG_H\n# define n_CONFIG_H 1\n' > ${h}
+printf '#ifndef n_MK_CONFIG_H\n# define n_MK_CONFIG_H 1\n' > ${h}
 ${cat} ${tmp} >> ${h}
 ${rm} -f ${tmp}
 printf '\n' >> ${h}
 
-# Create the real mk.mk
+# Create the real mk-config.mk
 # Note we cannout use explicit ./ filename prefix for source and object
 # pathnames because of a bug in bmake(1)
 ${rm} -rf ${tmp0}.* ${tmp0}*
@@ -2670,12 +2670,12 @@ done
 #${awk} -v opts="${OPTIONS_DETECT} ${OPTIONS} ${OPTIONS_XTRA}" \
 #   -v xopts="${XOPTIONS_DETECT} ${XOPTIONS} ${XOPTIONS_XTRA}" \
 #
-printf '"\n#endif /* n_CONFIG_H */\n' >> ${h}
+printf '"\n#endif /* n_MK_CONFIG_H */\n' >> ${h}
 
 echo "LIBS = `${cat} ${lib}`" >> ${mk}
 echo "INCS = `${cat} ${inc}`" >> ${mk}
 echo >> ${mk}
-${cat} ./mk-mk.in >> ${mk}
+${cat} ./make-config.in >> ${mk}
 
 ## Finished!
 

.gitignore
@@ -1,5 +1,4 @@
-config.*
-*.mk
+mk-*
 gen-mime-types.h
 
 uman.1

INSTALL
@@ -110,6 +110,7 @@ and library paths by passing prefilled $INCS and $LIBS variables:
 
   $ make INCS=-I/mypath/lib LIBS=-l/mypath/iconv tangerine
 
+Experts may find it valuable to adjust some settings in config.h.
 Note there are also some predefined configuration sets available, ment
 to be used instead of doing manual adjustments (in make.rc or) on the
 command line (names are case-insensitive):
@@ -158,29 +159,29 @@ command line (names are case-insensitive):
 1.1 What if configuration fails?
 --------------------------------
 
-The configuration process creates some files named "config":
+The configuration process creates some files named "mk-config.*":
 
-- config.log  output generated by the configuration compile tests.
-- config.lst  configuration (chosen option, programs, paths).
-              (Removing this file reenables configuration even after
-              a screwed up configuration, e.g., due to power failure.)
-- config.h    C program header produced according to config.lst.
-- config.ev   A set of sh(1) variable for reproducible compile runs.
-- config.inc  List of C header include paths, as compiler directives.
-- config.lib  List of used library information, as compiler directives.
+- mk-config.log  output generated by the configuration compile tests.
+- mk-config.lst  configuration (chosen option, programs, paths).
+                 (Removing this file reenables configuration even after
+                 a screwed up configuration, e.g., due to power failure.)
+- mk-config.h    C program header produced according to mk-config.lst.
+- mk-config.ev   A set of sh(1) variable for reproducible compile runs.
+- mk-config.inc  List of C header include paths, as compiler directives.
+- mk-config.lib  List of used library information, as compiler directives.
 
-Of special interest is config.log since the error usually manifests here
-in textual output.  Maybe that makes it obvious what can be done (header
-files could not be found because of missing entries in $C_INCLUDE_PATH,
-libraries could not be linked because of incomplete $LD_LIBRARY_PATH,
-etc.).
+Of special interest is mk-config.log since the error usually manifests
+here in textual output.  Maybe that makes it obvious what can be done
+(header files could not be found because of missing entries in
+$C_INCLUDE_PATH, libraries could not be linked because of incomplete
+$LD_LIBRARY_PATH, etc.).
 
 Otherwise it is getting complicated, and it would be appreciated if you
 would contact the mailing-list!
 
 If you hit an actual bug in the configuration system, and "make
 distclean" will not help you out, please "reset" the state by doing
-a simple "$ rm config.*".
+a simple "$ rm mk-*".
 
 1.2 What if building fails?
 ---------------------------

accmacvar.c
@@ -2,7 +2,7 @@
  *@ Account, macro and variable handling.
  *@ HOWTO add a new non-dynamic boolean or value option:
  *@ - add an entry to nail.h:enum okeys
- *@ - run mk-okey-map.pl
+ *@ - run make-okey-map.pl
  *@ - update the manual!
  *@ TODO . should be recursive environment based.
  *@ TODO   Otherwise, the `localopts' should be an attribute of the go.c
@@ -72,7 +72,7 @@ enum a_amv_mac_flags{
    a_AMV_MF__MAX = 0xFF
 };
 
-/* mk-okey-map.pl ensures that _VIRT implies _RDONLY and _NODEL, and that
+/* make-okey-map.pl ensures that _VIRT implies _RDONLY and _NODEL, and that
  * _IMPORT implies _ENV; it doesn't verify anything... */
 enum a_amv_var_flags{
    a_AMV_VF_NONE = 0,
@@ -212,7 +212,7 @@ struct a_amv_var_carrier{
    ui16_t avc_special_prop;
 };
 
-/* Include the constant mk-okey-map.pl output, and the generated version data */
+/* Include constant make-okey-map.pl output, and the generated version data */
 #include "gen-version.h"
 #include "gen-okeys.h"
 

auxlily.c
@@ -119,10 +119,10 @@ static ui64_t const a_aux_idec_cutlimit[35] = {
 };
 #undef a_X
 
-/* Include the constant mk-errors.sh output */
+/* Include the constant make-errors.sh output */
 #include "gen-errors.h"
 
-/* And these things come from config.h (config-time mk-errors.sh output) */
+/* And these things come from mk-config.h (config-time make-errors.sh output) */
 static n__ERR_NUMBER_TYPE const a_aux_err_no2mapoff[][2] = {
 #undef a_X
 #define a_X(N,I) {N,I},

cmd-message.c
@@ -364,7 +364,7 @@ a_cmsg_top(void *vp, struct n_ignore const *itp){
                   if(!asciichar(c))
                      break;
                   if(!blankspacechar(c)){
-                     if(!ISQUOTE(c))
+                     if(!n_QUOTE_IS_A(c))
                         break;
                      c = '\0';
                      break;

config.h
@@ -1,0 +1,144 @@
+/*@ S-nail - a mail user agent derived from Berkeley Mail.
+ *@ Some constants etc. for which adjustments may be desired.
+ *@ This is included after all the (system) headers.
+ *
+ * Copyright (c) 2000-2004 Gunnar Ritter, Freiburg i. Br., Germany.
+ * Copyright (c) 2012 - 2017 Steffen (Daode) Nurpmeso <steffen@sdaoden.eu>.
+ */
+/*
+ * Copyright (c) 1980, 1993
+ *      The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#ifndef n_CONFIG_H
+# define n_CONFIG_H
+
+#define APPEND                   /* New mail goes to end of mailbox */
+#define CBAD            (-15555)
+#define DOTLOCK_TRIES   5        /* Number of open(2) calls for dotlock */
+#define FILE_LOCK_TRIES 10       /* Maximum tries before n_file_lock() fails */
+#define FILE_LOCK_MILLIS 200     /* If UIZ_MAX, fall back to that */
+#define n_ERROR "ERROR"          /* Is-error?  Also as n_error[] */
+#define ERRORS_MAX      1000     /* Maximum error ring entries TODO configable*/
+#define n_ESCAPE        '~'      /* Default escape for sending */
+#define HSHSIZE         23       /* Hash prime TODO make dynamic, obsolete */
+#define n_MAXARGC       512      /* Maximum list of raw strings */
+#define MAXEXP          25       /* Maximum expansion of aliases */
+#define REFERENCES_MAX  20       /* Maximum entries in References: */
+#define n_UNIREPL "\xEF\xBF\xBD" /* 0xFFFD in UTF-8 */
+#define FTMP_OPEN_TRIES 10       /* Maximum number of Ftmp() open(2) tries */
+#define n_VEXPR_REGEX_MAX 10     /* Maximum address. `vexpr' regex(7) matches */
+
+/* Max readable line width TODO simply use BUFSIZ? */
+#if BUFSIZ + 0 > 2560
+# define LINESIZE BUFSIZ
+#else
+# define LINESIZE 2560
+#endif
+#define BUFFER_SIZE (BUFSIZ >= (1u << 13) ? BUFSIZ : (1u << 14))
+
+/* *indentprefix* default as of POSIX */
+#define INDENT_DEFAULT  "\t"
+
+/* Auto-reclaimed memory storage: size of the buffers.  Maximum auto-reclaimed
+ * storage is that value /2, which is n_CTA()ed to be > 1024 */
+#define n_MEMORY_AUTOREC_SIZE 0x2000u
+/* Ugly, but avoid dynamic allocation for the management structure! */
+#define n_MEMORY_POOL_TYPE_SIZEOF (7 * sizeof(void*))
+
+/* Default *encoding* as enum mime_enc */
+#define MIME_DEFAULT_ENCODING MIMEE_B64
+
+/* Maximum allowed line length in a mail before QP folding is necessary), and
+ * the real limit we go for */
+#define MIME_LINELEN_MAX   998   /* Plus CRLF */
+#define MIME_LINELEN_LIMIT (MIME_LINELEN_MAX - 48)
+
+/* Ditto, SHOULD */
+#define MIME_LINELEN    78    /* Plus CRLF */
+
+/* And in headers which contain an encoded word according to RFC 2047 there is
+ * yet another limit; also RFC 2045: 6.7, (5). */
+#define MIME_LINELEN_RFC2047 76
+
+/* Locations of mime.types(5) */
+#define MIME_TYPES_USR  "~/.mime.types"
+#define MIME_TYPES_SYS  "/etc/mime.types"
+
+/* Fallback MIME charsets, if *charset-7bit* and *charset-8bit* or not set.
+ * Note: must be lowercase!  Changes affect enum okeys!
+ * (Keep in SYNC: ./nail.1:"Character sets", ./nail.h:CHARSET_*!) */
+#define CHARSET_7BIT "us-ascii"
+#ifdef HAVE_ICONV
+# define CHARSET_8BIT "utf-8"
+# define CHARSET_8BIT_OKEY charset_8bit
+#else
+# define CHARSET_8BIT "iso-8859-1"
+# define CHARSET_8BIT_OKEY ttycharset
+#endif
+
+/* Some environment variables for pipe hooks etc. */
+#define n_PIPEENV_FILENAME "MAILX_FILENAME"
+#define n_PIPEENV_FILENAME_GENERATED "MAILX_FILENAME_GENERATED"
+#define n_PIPEENV_FILENAME_TEMPORARY "MAILX_FILENAME_TEMPORARY"
+#define n_PIPEENV_CONTENT "MAILX_CONTENT"
+#define n_PIPEENV_CONTENT_EVIDENCE "MAILX_CONTENT_EVIDENCE"
+
+/* Is *W* a quoting (ASCII only) character? */
+#define n_QUOTE_IS_A(W) \
+   ((W) == n_WC_C('>') || (W) == n_WC_C('|') ||\
+    (W) == n_WC_C('}') || (W) == n_WC_C(':'))
+
+/* Maximum number of quote characters (not bytes!) that'll be used on
+ * follow lines when compressing leading quote characters */
+#define n_QUOTE_MAX 42u
+
+/* How much spaces should a <tab> count when *quote-fold*ing? (power-of-two!) */
+#define n_QUOTE_TAB_SPACES 8
+
+/* For long iterative output, like `list', tabulator-completion, etc.,
+ * determine the screen width that should be used */
+#define n_SCRNWIDTH_FOR_LISTS ((size_t)n_scrnwidth - ((size_t)n_scrnwidth >> 3))
+
+/* Smells fishy after, or asks for shell expansion, dependent on context */
+#define n_SHEXP_MAGIC_PATH_CHARS "|&;<>{}()[]*?$`'\"\\"
+
+/* Maximum size of a message that is passed through to the spam system */
+#define SPAM_MAXSIZE    420000
+
+/* Number of Not-Yet-Dead calls that are remembered */
+#if defined HAVE_DEBUG || defined HAVE_DEVEL || defined HAVE_NYD2
+# ifdef HAVE_NYD2
+#  define NYD_CALLS_MAX (25 * 84)
+# elif defined HAVE_DEVEL
+#  define NYD_CALLS_MAX (25 * 42)
+# else
+#  define NYD_CALLS_MAX (25 * 10)
+# endif
+#endif
+
+#endif /* n_CONFIG_H */
+/* s-it-mode */

filter.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef HAVE_QUOTE_FOLD
-n_CTAV(QUOTE_MAX > 3);
+n_CTAV(n_QUOTE_MAX > 3);
 
 enum qf_state {
    _QF_CLEAN,
@@ -100,7 +100,7 @@ _qf_add_data(struct quoteflt *self, wchar_t wc)
    /* Unroll <tab> to spaces */
    if (wc == L'\t') {
       save_l = self->qf_datw;
-      save_w = (save_l + QUOTE_TAB_SPACES) & ~(QUOTE_TAB_SPACES - 1);
+      save_w = (save_l + n_QUOTE_TAB_SPACES) & ~(n_QUOTE_TAB_SPACES - 1);
       save_w -= save_l;
       while (save_w-- > 0) {
          ssize_t j = _qf_add_data(self, L' ');
@@ -212,13 +212,13 @@ _qf_state_prefix(struct qf_vc *vc)
          ++self->qf_wscnt;
          continue;
       }
-      if (i == 1 && ISQUOTE(wc)) {
+      if (i == 1 && n_QUOTE_IS_A(wc)) {
          self->qf_wscnt = 0;
-         if (self->qf_currq.l >= QUOTE_MAX - 3) {
-            self->qf_currq.s[QUOTE_MAX - 3] = '.';
-            self->qf_currq.s[QUOTE_MAX - 2] = '.';
-            self->qf_currq.s[QUOTE_MAX - 1] = '.';
-            self->qf_currq.l = QUOTE_MAX;
+         if (self->qf_currq.l >= n_QUOTE_MAX - 3) {
+            self->qf_currq.s[n_QUOTE_MAX - 3] = '.';
+            self->qf_currq.s[n_QUOTE_MAX - 2] = '.';
+            self->qf_currq.s[n_QUOTE_MAX - 1] = '.';
+            self->qf_currq.l = n_QUOTE_MAX;
          } else
             self->qf_currq.s[self->qf_currq.l++] = buf[-1];
          continue;
@@ -230,7 +230,7 @@ jfin:
       /* Overtake WS to the current quote in order to preserve it for eventual
        * necessary follow lines, too */
       /* TODO we de-facto "normalize" to ASCII SP here which MESSES tabs!! */
-      while (self->qf_wscnt-- > 0 && self->qf_currq.l < QUOTE_MAX)
+      while (self->qf_wscnt-- > 0 && self->qf_currq.l < n_QUOTE_MAX)
          self->qf_currq.s[self->qf_currq.l++] = ' ';
       self->qf_datw = self->qf_pfix_len + self->qf_currq.l;
       self->qf_wscnt = 0;
@@ -317,7 +317,7 @@ quoteflt_init(struct quoteflt *self, char const *prefix)
       self->qf_pfix_len = (ui32_t)strlen(prefix);
 
    /* Check whether the user wants the more fancy quoting algorithm */
-   /* TODO *quote-fold*: QUOTE_MAX may excess it! */
+   /* TODO *quote-fold*: n_QUOTE_MAX may excess it! */
 #ifdef HAVE_QUOTE_FOLD
    if (self->qf_pfix_len > 0 && (cp = ok_vlook(quote_fold)) != NULL) {
       ui32_t qmin, qmax;
@@ -341,7 +341,7 @@ quoteflt_init(struct quoteflt *self, char const *prefix)
 
       /* Add pad for takeover copies, reverse solidus and newline */
       self->qf_dat.s = salloc((qmax + 3) * n_mb_cur_max);
-      self->qf_currq.s = salloc((QUOTE_MAX + 1) * n_mb_cur_max);
+      self->qf_currq.s = salloc((n_QUOTE_MAX + 1) * n_mb_cur_max);
    }
 #endif
    NYD_LEAVE;

commit a5d52a6f27d9ff9963f00729ac52162ed0dfe57e

make-config.sh: fix OPT_AMALGAMATION, broken since [7039d7f4]

make-config.sh
@@ -2658,40 +2658,7 @@ ${cat} ${tmp} >> ${h}
 ${rm} -f ${tmp}
 printf '\n' >> ${h}
 
-# Create the real mk-config.mk
-# Note we cannout use explicit ./ filename prefix for source and object
-# pathnames because of a bug in bmake(1)
-${rm} -rf ${tmp0}.* ${tmp0}*
-printf 'OBJ_SRC = ' >> ${mk}
-if feat_no AMALGAMATION; then
-   for i in `printf '%s\n' *.c | ${sort}`; do
-      if [ "${i}" = privsep.c ]; then
-         continue
-      fi
-      printf "${i} " >> ${mk}
-   done
-   printf '\nAMALGAM_TARGET =\nAMALGAM_DEP =\n' >> ${mk}
-else
-   printf 'main.c\nAMALGAM_TARGET = main.o\nAMALGAM_DEP = ' >> ${mk}
-
-   printf '\n/* HAVE_AMALGAMATION: include sources */\n' >> ${h}
-   printf '#elif _CONFIG_H + 0 == 1\n' >> ${h}
-   printf '# undef _CONFIG_H\n' >> ${h}
-   printf '# define _CONFIG_H 2\n' >> ${h}
-   for i in `printf '%s\n' *.c | ${sort}`; do
-      if [ "${i}" = "${j}" ] || [ "${i}" = main.c ] || \
-            [ "${i}" = privsep.c ]; then
-         continue
-      fi
-      printf "${i} " >> ${mk}
-      printf "# include \"${i}\"\n" >> ${h}
-   done
-   echo >> ${mk}
-   # tcc(1) fails on 2015-11-13 unless this #else clause existed
-   echo '#else' >> ${h}
-fi
-
-# Finally, create the string that is used by *features* and `version'.
+# Create the string that is used by *features* and `version'.
 # Take this nice opportunity and generate a visual listing of included and
 # non-included features for the person who runs the configuration
 msg '\nThe following features are included (+) or not (-):'
@@ -2714,8 +2681,40 @@ done
 #exec 5>&1 >>${h}
 #${awk} -v opts="${OPTIONS_DETECT} ${OPTIONS} ${OPTIONS_XTRA}" \
 #   -v xopts="${XOPTIONS_DETECT} ${XOPTIONS} ${XOPTIONS_XTRA}" \
-#
-printf '"\n#endif /* n_MK_CONFIG_H */\n' >> ${h}
+printf '"\n' >> ${h}
+
+# Create the real mk-config.mk
+# Note we cannout use explicit ./ filename prefix for source and object
+# pathnames because of a bug in bmake(1)
+${rm} -rf ${tmp0}.* ${tmp0}*
+printf 'OBJ_SRC = ' >> ${mk}
+if feat_no AMALGAMATION; then
+   for i in `printf '%s\n' *.c | ${sort}`; do
+      if [ "${i}" = privsep.c ]; then
+         continue
+      fi
+      printf "${i} " >> ${mk}
+   done
+   printf '\nAMALGAM_TARGET =\nAMALGAM_DEP =\n' >> ${mk}
+else
+   printf 'main.c\nAMALGAM_TARGET = main.o\nAMALGAM_DEP = ' >> ${mk}
+
+   printf '\n/* HAVE_AMALGAMATION: include sources */\n' >> ${h}
+   printf '#elif n_MK_CONFIG_H + 0 == 1\n' >> ${h}
+   printf '# undef n_MK_CONFIG_H\n' >> ${h}
+   printf '# define n_MK_CONFIG_H 2\n' >> ${h}
+   for i in `printf '%s\n' *.c | ${sort}`; do
+      if [ "${i}" = "${j}" ] || [ "${i}" = main.c ] || \
+            [ "${i}" = privsep.c ]; then
+         continue
+      fi
+      printf "${i} " >> ${mk}
+      printf "# include \"${i}\"\n" >> ${h}
+   done
+   echo >> ${mk}
+fi
+
+printf '#endif /* n_MK_CONFIG_H */\n' >> ${h}
 
 echo "LIBS = `${cat} ${lib}`" >> ${mk}
 echo "INCS = `${cat} ${inc}`" >> ${mk}