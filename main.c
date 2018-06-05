sdaoden/s-mailx

commit 94bafbc6b433d593007f0e2736d51caa9cd05b37

Temporarily freeze variables set via -S..
This was long in TODO, so it must be said that i have doubts since
any update or change is silently rejected.  This is so to not
interfere with *errexit* or *posix*: in $POSIXLY_CORRECT mode any
normal startup with -S becomes a problem otherwise.

On the other hand it really is a huge problem unless we have
a functional `local' command modifier so that 'local set i' and
-Si=100 cannot clash!

But we need it now to avoid initialization order issues for
*ssl-config-file*: the command line should succeed anyhow!

main.c
@@ -645,8 +645,8 @@ main(int argc, char *argv[]){
       a_RF_USER = 1<<2,
       a_RF_ALL = a_RF_SYSTEM | a_RF_USER
    } resfiles;
-   size_t oargs_size, oargs_cnt, Xargs_size, Xargs_cnt, smopts_size;
-   char const *Aarg, *emsg, *folder, *Larg, *okey, **oargs, *qf,
+   size_t Xargs_size, Xargs_cnt, smopts_size;
+   char const *Aarg, *emsg, *folder, *Larg, *okey, *qf,
       *subject, *uarg, **Xargs;
    struct attachment *attach;
    struct name *to, *cc, *bcc;
@@ -658,8 +658,8 @@ main(int argc, char *argv[]){
    to = cc = bcc = NULL;
    attach = NULL;
    Aarg = emsg = folder = Larg = okey = qf = subject = uarg = NULL;
-   oargs = Xargs = NULL;
-   oargs_size = oargs_cnt = Xargs_size = Xargs_cnt = smopts_size = 0;
+   Xargs = NULL;
+   Xargs_size = Xargs_cnt = smopts_size = 0;
    resfiles = a_RF_ALL;
 
    /*
@@ -706,12 +706,12 @@ main(int argc, char *argv[]){
          cc = cat(cc, lextract(a_main_oarg, GCC | GFULL));
          break;
       case 'd':
-         a_main_oarg = "debug";
-         goto jsetvar;
+         ok_bset(debug);
+         break;
       case 'E':
          n_OBSOLETE(_("-E will be removed, please use \"-Sskipemptybody\""));
-         a_main_oarg = "skipemptybody";
-         goto jsetvar;
+         ok_bset(skipemptybody);
+         break;
       case 'e':
          n_poption |= n_PO_EXISTONLY;
          break;
@@ -733,8 +733,8 @@ main(int argc, char *argv[]){
          goto j_leave;
       case 'i':
          /* Ignore interrupts */
-         a_main_oarg = "ignore";
-         goto jsetvar;
+         ok_bset(ignore);
+         break;
       case 'L':
          Larg = a_main_oarg;
          n_poption |= n_PO_HEADERLIST;
@@ -765,8 +765,8 @@ main(int argc, char *argv[]){
          break;
       case 'N':
          /* Avoid initial header printing */
-         a_main_oarg = "noheader";
-         goto jsetvar;
+         ok_bclear(header);
+         break;
       case 'n':
          /* Don't source "unspecified system start-up file" */
          if(resfiles & a_RF_SET){
@@ -820,7 +820,8 @@ jeMmq:
          }
          break;
       case 'S':
-jsetvar: /* Set variable (TODO twice [only if successful]) */
+         n_poption |= n_PO_S_FLAG_TEMPORARY;
+jsetvar: /* Set variable */
          {  char const *a[2];
             bool_t b;
 
@@ -828,15 +829,13 @@ jsetvar: /* Set variable (TODO twice [only if successful]) */
             a[1] = NULL;
             n_pstate |= n_PS_ROBOT;
             b = (c_set(a) == 0);
-            n_pstate &= ~n_PS_ROBOT;
+            n_pstate &= ~(n_PS_ROOT | n_PS_ROBOT);
+            n_poption &= ~n_PO_S_FLAG_TEMPORARY;
             if(!b && (ok_blook(errexit) || ok_blook(posix))){
                emsg = N_("-S failed to adjust variable");
                goto jusage;
             }
          }
-         if(oargs_cnt == oargs_size)
-            oargs_size = a_main_grow_cpp(&oargs, oargs_size + 8, oargs_cnt);
-         oargs[oargs_cnt++] = okey;
          break;
       case 's':
          /* Subject:; take care for Debian #419840 and strip any \r and \n */
@@ -862,8 +861,8 @@ jsetvar: /* Set variable (TODO twice [only if successful]) */
          goto j_leave;
       case 'v':
          /* Be verbose */
-         a_main_oarg = "verbose";
-         goto jsetvar;
+         ok_bset(verbose);
+         break;
       case 'X':
          /* Add to list of commands to exec before entering normal operation */
          if(Xargs_cnt == Xargs_size)
@@ -897,19 +896,17 @@ jsetvar: /* Set variable (TODO twice [only if successful]) */
             setvbuf(n_stdin, NULL, _IOLBF, 0);
          n_poption |= n_PO_TILDE_FLAG | n_PO_BATCH_FLAG;
          folder = n_path_devnull;
-         if(oargs_cnt + 10 >= oargs_size)
-            oargs_size = a_main_grow_cpp(&oargs, oargs_size + 11, oargs_cnt);
          n_pstate |= n_PS_ROBOT; /* (be silent unsetting undefined variables) */
-         ok_vset(MAIL, folder), oargs[oargs_cnt++] = "MAIL=" n_PATH_DEVNULL;
-         ok_vset(MBOX, folder), oargs[oargs_cnt++] = "MBOX=" n_PATH_DEVNULL;
-         ok_bset(emptystart), oargs[oargs_cnt++] = "emptystart";
-         ok_bclear(errexit), oargs[oargs_cnt++] = "noerrexit";
-         ok_bclear(header), oargs[oargs_cnt++] = "noheader";
-         ok_vset(inbox, folder), oargs[oargs_cnt++] = "inbox=" n_PATH_DEVNULL;
-         ok_bclear(posix), oargs[oargs_cnt++] = "noposix";
-         ok_bset(quiet), oargs[oargs_cnt++] = "quiet";
-         ok_bset(sendwait), oargs[oargs_cnt++] = "sendwait";
-         ok_bset(typescript_mode), oargs[oargs_cnt++] = "typescript-mode";
+         ok_vset(MAIL, folder);
+         ok_vset(MBOX, folder);
+         ok_bset(emptystart);
+         ok_bclear(errexit);
+         ok_bclear(header);
+         ok_vset(inbox, folder);
+         ok_bclear(posix);
+         ok_bset(quiet);
+         ok_bset(sendwait);
+         ok_bset(typescript_mode);
          n_pstate &= ~n_PS_ROBOT;
          break;
       case '.':
@@ -1056,19 +1053,6 @@ jgetopt_done:
       if((cp != NULL || (cp = ok_vlook(mailx_extra_rc)) != NULL) &&
             !n_go_load(fexpand(cp, FEXP_LOCAL | FEXP_NOPROTO)))
          goto j_leave;
-   }
-
-   /* Ensure the -S and other command line options take precedence over
-    * anything that may have been placed in resource files.
-    * Our "ternary binary" option *verbose* needs special treament */
-   if((n_poption & (n_PO_VERB | n_PO_VERBVERB)) == n_PO_VERB)
-      n_poption &= ~n_PO_VERB;
-   /* ..and be silent when unsetting undefined variables again */
-   if(oargs_cnt > 0){
-      n_pstate |= n_PS_ROBOT;
-      oargs[oargs_cnt] = NULL;
-      c_set(oargs);
-      n_pstate &= ~n_PS_ROBOT;
    }
 
    /* Cause possible umask(2) to be applied, now that any setting is

auxlily.c
@@ -343,11 +343,9 @@ n_locale_init(void){
    /* C99 */{
       char const *cp;
 
-      /* TODO *ttycharset* may be set several times during startup unless
-       * TODO we gain a mechanism that -S fixates a setting during startup,
-       * TODO effectively turning later adjustments (during startup) in noop */
       if((cp = nl_langinfo(CODESET)) != NULL)
-         ok_vset(ttycharset, cp);
+         /* Avoid logging if user set that via -S! */
+         n_PS_ROOT_BLOCK(ok_vset(ttycharset, cp));
    }
 # endif
 

cc-test.sh
@@ -355,6 +355,7 @@ fi
 t_behave() {
    t_behave_X_opt_input_command_stack
    t_behave_X_errexit
+   t_behave_S_freeze
    t_behave_wysh
    t_behave_input_inject_semicolon_seq
    t_behave_commandalias
@@ -546,6 +547,106 @@ t_behave_X_errexit() {
       > "${BODY}" 2>&1
    check behave:x_errexit-11 0 "${BODY}" '916157812 53'
 
+   t_epilog
+}
+
+t_behave_S_freeze() {
+   t_prolog t_behave_S_freeze
+   oterm=$TERM
+   unset TERM
+
+   # Test basic assumption
+   </dev/null MAILRC="${BODY}" ${MAILX} ${ARGS} \
+      -X'echo asksub<$asksub> dietcurd<$dietcurd>' \
+      -Xx > "${MBOX}" 2>&1
+   check behave:s_freeze-1 0 "${MBOX}" '270686329 21'
+
+   #
+   ${cat} <<- '__EOT' > "${BODY}"
+	echo asksub<$asksub>
+	set asksub
+	echo asksub<$asksub>
+	__EOT
+   </dev/null MAILRC="${BODY}" ${MAILX} ${ARGS} -:u \
+      -Snoasksub -Sasksub -Snoasksub \
+      -X'echo asksub<$asksub>' -X'set asksub' -X'echo asksub<$asksub>' \
+      -Xx > "${MBOX}" 2>&1
+   check behave:s_freeze-2 0 "${MBOX}" '3182942628 37'
+
+   ${cat} <<- '__EOT' > "${BODY}"
+	echo asksub<$asksub>
+	unset asksub
+	echo asksub<$asksub>
+	__EOT
+   </dev/null MAILRC="${BODY}" ${MAILX} ${ARGS} -:u \
+      -Snoasksub -Sasksub \
+      -X'echo asksub<$asksub>' -X'unset asksub' -X'echo asksub<$asksub>' \
+      -Xx > "${MBOX}" 2>&1
+   check behave:s_freeze-3 0 "${MBOX}" '2006554293 39'
+
+   #
+   ${cat} <<- '__EOT' > "${BODY}"
+	echo dietcurd<$dietcurd>
+	set dietcurd=cherry
+	echo dietcurd<$dietcurd>
+	__EOT
+   </dev/null MAILRC="${BODY}" ${MAILX} ${ARGS} -:u \
+      -Sdietcurd=strawberry -Snodietcurd -Sdietcurd=vanilla \
+      -X'echo dietcurd<$dietcurd>' -X'unset dietcurd' \
+         -X'echo dietcurd<$dietcurd>' \
+      -Xx > "${MBOX}" 2>&1
+   check behave:s_freeze-4 0 "${MBOX}" '1985768109 65'
+
+   ${cat} <<- '__EOT' > "${BODY}"
+	echo dietcurd<$dietcurd>
+	unset dietcurd
+	echo dietcurd<$dietcurd>
+	__EOT
+   </dev/null MAILRC="${BODY}" ${MAILX} ${ARGS} -:u \
+      -Sdietcurd=strawberry -Snodietcurd \
+      -X'echo dietcurd<$dietcurd>' -X'set dietcurd=vanilla' \
+         -X'echo dietcurd<$dietcurd>' \
+      -Xx > "${MBOX}" 2>&1
+   check behave:s_freeze-5 0 "${MBOX}" '151574279 51'
+
+   # TODO once we have a detached one with env=1..
+   if [ -n "`</dev/null ${MAILX} ${ARGS} -:/ -X'!echo $TERM' -Xx`" ]; then
+      echo 'behave:s_freeze-{5,6}: shell sets $TERM, skipped'
+   else
+      ${cat} <<- '__EOT' > "${BODY}"
+		!echo "shell says TERM<$TERM>"
+	echo TERM<$TERM>
+		!echo "shell says TERM<$TERM>"
+	set TERM=cherry
+		!echo "shell says TERM<$TERM>"
+	echo TERM<$TERM>
+		!echo "shell says TERM<$TERM>"
+		__EOT
+      </dev/null MAILRC="${BODY}" ${MAILX} ${ARGS} -:u \
+         -STERM=strawberry -SnoTERM -STERM=vanilla \
+         -X'echo mail<$TERM>' -X'unset TERM' \
+         -X'!echo "shell says TERM<$TERM>"' -X'echo TERM<$TERM>' \
+         -Xx > "${MBOX}" 2>&1
+   check behave:s_freeze-6 0 "${MBOX}" '1211476036 167'
+
+      ${cat} <<- '__EOT' > "${BODY}"
+		!echo "shell says TERM<$TERM>"
+	echo TERM<$TERM>
+		!echo "shell says TERM<$TERM>"
+	set TERM=cherry
+		!echo "shell says TERM<$TERM>"
+	echo TERM<$TERM>
+		!echo "shell says TERM<$TERM>"
+	__EOT
+      </dev/null MAILRC="${BODY}" ${MAILX} ${ARGS} -:u \
+         -STERM=strawberry -SnoTERM \
+         -X'echo TERM<$TERM>' -X'set TERM=vanilla' \
+         -X'!echo "shell says TERM<$TERM>"' -X'echo TERM<$TERM>' \
+         -Xx > "${MBOX}" 2>&1
+      check behave:s_freeze-7 0 "${MBOX}" '3365080441 132'
+   fi
+
+   TERM=$oterm
    t_epilog
 }
 

cmd-misc.c
@@ -281,7 +281,7 @@ c_shell(void *v)
          n_pstate_err_no = n_ERR_NOTSUP;
          rv = -1;
       }
-   }else if(rv >= 0){
+   }else if(rv >= 0 && (n_psonce & n_PSO_INTERACTIVE)){
       fprintf(n_stdout, "!\n");
       /* Line buffered fflush(n_stdout); */
    }

nail.1
@@ -132,7 +132,7 @@
 .
 .Bd -filled -compact -offset indent
 .Sy Compatibility note:
-S-nail (\*(UA) will wrap up into \%S-mailx in v15.0 (circa 2019).
+S-nail (\*(UA) will wrap up into \%S-mailx in v15.0 (circa 2020).
 Backward incompatibility has to be expected \(en
 .Sx COMMANDS
 will use
@@ -529,27 +529,29 @@ accordingly or the user is member of a group with special privileges.
 .Mx
 .It Fl S Ar var Ns Op = Ns value
 .Ic set
-the internal
+(or, with a prefix string
+.Ql no ,
+as documented in
+.Sx "INTERNAL VARIABLES" ,
+.Ic unset )
 .Ar var Ns
-iable and, in case of a non-boolean variable which has a value, assign
-.Ar value
-to it; in case the operation fails the program will exit if the variables
+iable and optionally assign
+.Ar value ,
+if supported.
+If the operation fails the program will exit if any of
 .Va errexit
 or
 .Va posix
 are set.
-Even though
-.Sx "INTERNAL VARIABLES"
-.Ic \&\&set
-via
+Settings established via
 .Fl \&\&S
-may be overwritten from within resource files,
-the command line setting will be reestablished after all resource files
-have been loaded.
-\*(ID In the future such a setting may instead become
-.Dq frozen
-until the startup is complete, silently ignoring any tries to set or
-unset the variable.
+cannot be changed from within
+.Sx "Resource files"
+or an account switch initiated by
+.Fl A .
+They will become mutable again before commands registered via
+.Fl X
+are executed.
 .
 .Mx
 .It Fl s Ar subject
@@ -668,7 +670,7 @@ set of (interactive) commands is available, processing of
 is enabled in compose mode, and diverse
 .Sx "INTERNAL VARIABLES"
 are adjusted for batch necessities, exactly as if done via
-.Fl S :
+.Ic set :
 .Va emptystart ,
 .Pf no Va errexit ,
 .Pf no Va header ,

nail.h
@@ -1366,9 +1366,10 @@ enum n_program_option{
    n_PO_Mm_FLAG = 1u<<8,      /* -M or -m (plus n_poption_arg_Mm) */
    n_PO_R_FLAG = 1u<<9,       /* -R */
    n_PO_r_FLAG = 1u<<10,      /* -r (plus n_poption_arg_r) */
-   n_PO_t_FLAG = 1u<<11,      /* -t */
-   n_PO_TILDE_FLAG = 1u<<12,  /* -~ */
-   n_PO_BATCH_FLAG = 1u<<13,  /* -# */
+   n_PO_S_FLAG_TEMPORARY = 1u<<11, /* -S about to set a variable */
+   n_PO_t_FLAG = 1u<<12,      /* -t */
+   n_PO_TILDE_FLAG = 1u<<13,  /* -~ */
+   n_PO_BATCH_FLAG = 1u<<14,  /* -# */
 
    /*  */
    n_PO_MEMDEBUG = 1<<24,     /* *memdebug* */

commit 122169885673a5fa8d9cc90382ca711b0f4f2dfb

Fix: allow -r to set from after -Sfrom= (Michael Dressel)..
Michael Dressel (dressel at mail dot desy dot de) reported a bug
introduced in [94bafbc6b4] (Temporarily freeze variables set via
-S.., 2017-09-03): if *from* would have been set via -S somehow,
-r would have not been allowed to overwrite that freeze.
(Funnily Desy seems to be great science and operates super
lasers which also heat water to 100000 degree Celsius in 75
microseconds, and even more.)

Despite this the situation is pretty frustrating and TODO contains
since v14.9.8 i think this

- -r should be the Sender:, which should automatically propagate to
  From: if possible and/or necessary.  It should be possible to suppress
  -r stuff from From: and Sender:, but fallback to special -r arg as
  appropriate.

Sigh.

main.c
@@ -862,8 +862,8 @@ jeMmq:
          }
          break;
       case 'S':
-         n_poption |= n_PO_S_FLAG_TEMPORARY;
 jsetvar: /* Set variable TODO optimize v15-compat case */
+         n_poption |= n_PO_S_FLAG_TEMPORARY;
          {  struct str sin;
             struct n_string s, *sp;
             char const *a[2];