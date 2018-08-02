sdaoden/s-mailx

commit 330c77ce556dced7203ae7e5ede0357c4f263b12

url_parse(): fix (im)possible off-by-x when giving a:port

urlcrecry.c
@@ -1069,19 +1069,18 @@ jurlp_err:
 #endif /* HAVE_IDNA */
 
    /* .url_h_p: HOST:PORT */
-   {  size_t i;
+   {  size_t upl, i;
       struct str *s = &urlp->url_h_p;
 
-      s->s = salloc(urlp->url_host.l + 1 + sizeof("65536")-1 +1);
+      upl = (urlp->url_port == NULL) ? 0 : 1u + strlen(urlp->url_port);
+      s->s = n_autorec_alloc(urlp->url_host.l + upl +1);
       memcpy(s->s, urlp->url_host.s, i = urlp->url_host.l);
-      if (urlp->url_port != NULL) {
-         size_t j = strlen(urlp->url_port);
+      if(upl > 0){
          s->s[i++] = ':';
-         memcpy(s->s + i, urlp->url_port, j);
-         i += j;
+         memcpy(&s->s[i], urlp->url_port, upl);
+         i += upl;
       }
-      s->s[i] = '\0';
-      s->l = i;
+      s->s[s->l = i] = '\0';
    }
 
    /* User, II

commit 46e2ed748098f50e7ab731698723a61a4f648eea

url_parse(): FIX off-by-one introduced in [330c77ce] onto [master]

urlcrecry.c
@@ -1077,7 +1077,7 @@ jurlp_err:
       memcpy(s->s, urlp->url_host.s, i = urlp->url_host.l);
       if(upl > 0){
          s->s[i++] = ':';
-         memcpy(&s->s[i], urlp->url_port, upl);
+         memcpy(&s->s[i], urlp->url_port, --upl);
          i += upl;
       }
       s->s[s->l = i] = '\0';