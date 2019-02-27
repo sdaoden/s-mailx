sdaoden/s-mailx

commit fba091c1015c20e091556333b1ebddae2a4d70a7

Various fixes to actually integrate IFS for at least some for real..
Using the new (still incomplete and only supporting flat table)
option parser to be able to truly support IFS has had the bad
side effect of requiring that we use RAWDAT argument type in the
normal command table, and that made actually the `xcall' test
fail: of course, because the macro data environment which holds
the local arguments which we want to expand when we say

  xcall BLA "$@"

are already gone!  That is the essence of the `xcall'
optimization, and did not hurt with WYSH parsing because there the
arguments became expanded before c_xcall() has been invoked.

To overcome this, we have to incorporate the new option parser
into the normal command table and add a new command argument type
for it, so that the evaluation takes place at the same time as it
does for WYSH.  Sigh, i explicitly avoided doing this work once
i have implemented `bind' and `unbind', the first users of the new
parser, but now it happened.

It made thus sense to reorganize the code a little bit more,
outsource the command table into its own file (again), and merge
cmd-arg.c with it.  At the cost of needing to expose some formerly
static functions, of course.  I thought that was worth it.

TODO We are far from being there, the command table should be
implemented as a multi-dimensional array where the first level is
indexed by first letter of command MODULO size, and all that
should be sorted alphabetically excluding POSIX standardized
abbreviations ordering first.  All that should be extracted
automatically from especially crafted comments in the sources, in
or at the function definitions.

Also, unrolling `commandalias' code in go.c was fine but besides
these are normal key/value tuples and thus belong into nam-a-grp.c
where all the other hashtable stuff is located.

nail.1
@@ -2774,7 +2774,8 @@ the newline character: it is discarded and the next line of input is
 used as a follow-up line, with all leading whitespace removed;
 once the entire command line is completed, the processing that is
 documented in the following, after removal of the whitespace characters
-space, tabulator and newline as well as those defined by the variable
+.Cm space , tabulator , newline
+as well as those defined by the variable
 .Va ifs
 from the beginning and end of the line, begins.
 .
@@ -2962,7 +2963,9 @@ ampersand
 .Cm & ,
 semicolon
 .Cm \&; ,
-as well as
+as well as all characters from the variable
+.Va ifs ,
+and / or
 .Cm space , tabulator , newline .
 The additional metacharacters left and right parenthesis
 .Cm \&( , \&)
@@ -2971,8 +2974,27 @@ and less-than and greater-than signs
 that the
 .Xr sh 1
 supports are not used, and are treated as ordinary characters: for one
-these characters are a vivid part of email addresses, and it also seems
+these characters are a vivid part of email addresses, and it seems
 highly unlikely that their function will become meaningful to \*(UA.
+.
+.Bd -filled -offset indent
+.Sy Compatibility note:
+\*(ID Please note that many even new-style commands do not yet honour
+.Va ifs
+to parse their arguments: whereas the
+.Xr sh 1 Ns
+ell is a language with syntactic elements of clearly defined semantics,
+\*(UA parses entire input lines and decides on a per-command base what
+to do with the rest of the line.
+Often it even depends on subcommands how the rest of the line should be
+treated, and until v15 we are not capable to perform this deep
+inspection of arguments.
+Nonetheless, at least the following commands which work with positional
+parameters fully support
+.Va ifs
+for an almost shell-compatible field splitting:
+.Ic call , call_if , read , vpospar , xcall .
+.Ed
 .
 .Pp
 Any unquoted number sign
@@ -4013,8 +4035,8 @@ will remove all established mappings.
 \*(NQ Define or list, and remove, respectively, command aliases.
 An (command)alias can be used everywhere a normal command can be used,
 but always takes precedence: any arguments that are given to the command
-alias are joined onto the alias content, and the resulting string forms
-the command line that is, in effect, executed.
+alias are joined onto the alias expansion, and the resulting string
+forms the command line that is, in effect, executed.
 The latter command removes all given aliases, the special name
 .Ql *
 will remove all existing aliases.
@@ -4027,9 +4049,9 @@ be accessible, the content of which can be just about anything.
 An alias may itself expand to another alias, but to avoid expansion loops
 further expansion will be prevented if an alias refers to itself or if
 an expansion depth limit is reached.
-Explicit expansion prevention is available via reverse solidus
+Explicit expansion prevention via reverse solidus
 .Cm \e ,
-see
+one of the
 .Sx "Command modifiers" .
 .Bd -literal -offset indent
 ? commandalias xx
@@ -6204,8 +6226,17 @@ error will occur.
 .Pp
 If the first argument is
 .Ql quote ,
-a round-trip capable representation of the stack contents is created:
-this mode supports
+a round-trip capable representation of the stack contents is created,
+with each quoted parameter separated from each other with the first
+character of
+.Va ifs ,
+and followed by the first character of
+.Va if-ws ,
+if that is not empyty and not identical to the first.
+If that results in no separation at all a
+.Cm space
+character is used.
+This mode supports
 .Cm vput
 (see
 .Sx "Command modifiers" ) .

tty.c
@@ -1981,7 +1981,7 @@ a_tty_kht(struct a_tty_line *tlp){
 
             exp = sub;
             shs = n_shexp_parse_token((n_SHEXP_PARSE_DRYRUN |
-                  n_SHEXP_PARSE_TRIMSPACE | n_SHEXP_PARSE_IGNORE_EMPTY |
+                  n_SHEXP_PARSE_TRIM_SPACE | n_SHEXP_PARSE_IGNORE_EMPTY |
                   n_SHEXP_PARSE_QUOTE_AUTO_CLOSE), NULL, &sub, NULL);
             if(sub.l != 0){
                size_t x;
@@ -1997,7 +1997,7 @@ a_tty_kht(struct a_tty_line *tlp){
                   (int)exp.l, exp.s);
                goto jnope;
             }
-            n_shexp_parse_token((n_SHEXP_PARSE_TRIMSPACE |
+            n_shexp_parse_token((n_SHEXP_PARSE_TRIM_SPACE |
                   n_SHEXP_PARSE_IGNORE_EMPTY | n_SHEXP_PARSE_QUOTE_AUTO_CLOSE),
                   shoup, &exp, NULL);
             break;
@@ -3121,9 +3121,9 @@ a_tty_bind_parse(bool_t isbindcmd, struct a_tty_bind_parse_ctx *tbpcp){
       enum n_shexp_state shs;
 
       shin_save = shin;
-      shs = n_shexp_parse_token((n_SHEXP_PARSE_TRUNC | n_SHEXP_PARSE_TRIMSPACE |
-            n_SHEXP_PARSE_IGNORE_EMPTY | n_SHEXP_PARSE_IFS_IS_COMMA),
-            shoup, &shin, NULL);
+      shs = n_shexp_parse_token((n_SHEXP_PARSE_TRUNC |
+            n_SHEXP_PARSE_TRIM_SPACE | n_SHEXP_PARSE_IGNORE_EMPTY |
+            n_SHEXP_PARSE_IFS_IS_COMMA), shoup, &shin, NULL);
       if(shs & n_SHEXP_STATE_ERR_UNICODE){
          f |= a_TTY_BIND_DEFUNCT;
          if(isbindcmd && (n_poption & n_PO_D_V))
@@ -4201,35 +4201,20 @@ jentry:{
 # ifdef HAVE_KEY_BINDINGS
 FL int
 c_bind(void *v){
-   n_CMD_ARG_DESC_SUBCLASS_DEF(bind, 3, a_tty_bind_cad) { /* TODO cmd-tab.h */
-      {n_CMD_ARG_DESC_STRING, 0},
-      {n_CMD_ARG_DESC_WYSH | n_CMD_ARG_DESC_OPTION |
-            n_CMD_ARG_DESC_HONOUR_STOP,
-         n_SHEXP_PARSE_DRYRUN | n_SHEXP_PARSE_LOG},
-      {n_CMD_ARG_DESC_WYSH | n_CMD_ARG_DESC_OPTION | n_CMD_ARG_DESC_GREEDY |
-            n_CMD_ARG_DESC_HONOUR_STOP,
-         n_SHEXP_PARSE_IGNORE_EMPTY}
-   } n_CMD_ARG_DESC_SUBCLASS_DEF_END;
-   struct n_cmd_arg_ctx cac;
    struct a_tty_bind_ctx *tbcp;
    enum n_go_input_flags gif;
    bool_t aster, show;
    union {char const *cp; char *p; char c;} c;
+   struct n_cmd_arg_ctx *cacp;
    NYD_ENTER;
 
-   cac.cac_desc = n_CMD_ARG_DESC_SUBCLASS_CAST(&a_tty_bind_cad);
-   cac.cac_indat = v;
-   cac.cac_inlen = UIZ_MAX;
-   if(!n_cmd_arg_parse(&cac)){
-      v = NULL;
-      goto jleave;
-   }
+   cacp = v;
 
-   c.cp = cac.cac_arg->ca_arg.ca_str.s;
-   if(cac.cac_no == 1)
+   c.cp = cacp->cac_arg->ca_arg.ca_str.s;
+   if(cacp->cac_no == 1)
       show = TRU1;
    else
-      show = !asccasecmp(cac.cac_arg->ca_next->ca_arg.ca_str.s, "show");
+      show = !asccasecmp(cacp->cac_arg->ca_next->ca_arg.ca_str.s, "show");
    aster = FAL0;
 
    if((gif = a_tty_bind_ctx_find(c.cp)) == (enum n_go_input_flags)-1){
@@ -4328,15 +4313,15 @@ c_bind(void *v){
       struct n_string store;
 
       memset(&tbpc, 0, sizeof tbpc);
-      tbpc.tbpc_cmd = a_tty_bind_cad.cad_name;
-      tbpc.tbpc_in_seq = cac.cac_arg->ca_next->ca_arg.ca_str.s;
-      tbpc.tbpc_exp.s = n_string_cp(n_cmd_arg_join_greedy(&cac,
+      tbpc.tbpc_cmd = cacp->cac_desc->cad_name;
+      tbpc.tbpc_in_seq = cacp->cac_arg->ca_next->ca_arg.ca_str.s;
+      tbpc.tbpc_exp.s = n_string_cp(n_cmd_arg_join_greedy(cacp,
             n_string_creat_auto(&store)));
       tbpc.tbpc_exp.l = store.s_len;
       tbpc.tbpc_flags = gif;
       if(!a_tty_bind_create(&tbpc, TRU1))
          v = NULL;
-      n_string_gut(&store);
+      /*n_string_gut(&store);*/
    }
 jleave:
    NYD_LEAVE;
@@ -4345,28 +4330,16 @@ jleave:
 
 FL int
 c_unbind(void *v){
-   n_CMD_ARG_DESC_SUBCLASS_DEF(unbind, 2, a_tty_unbind_cad) {/* TODO cmd-tab.h*/
-      {n_CMD_ARG_DESC_STRING, 0},
-      {n_CMD_ARG_DESC_WYSH | n_CMD_ARG_DESC_HONOUR_STOP,
-         n_SHEXP_PARSE_DRYRUN | n_SHEXP_PARSE_LOG}
-   } n_CMD_ARG_DESC_SUBCLASS_DEF_END;
    struct a_tty_bind_parse_ctx tbpc;
-   struct n_cmd_arg_ctx cac;
    struct a_tty_bind_ctx *tbcp;
    enum n_go_input_flags gif;
    bool_t aster;
    union {char const *cp; char *p;} c;
+   struct n_cmd_arg_ctx *cacp;
    NYD_ENTER;
 
-   cac.cac_desc = n_CMD_ARG_DESC_SUBCLASS_CAST(&a_tty_unbind_cad);
-   cac.cac_indat = v;
-   cac.cac_inlen = UIZ_MAX;
-   if(!n_cmd_arg_parse(&cac)){
-      v = NULL;
-      goto jleave;
-   }
-
-   c.cp = cac.cac_arg->ca_arg.ca_str.s;
+   cacp = v;
+   c.cp = cacp->cac_arg->ca_arg.ca_str.s;
    aster = FAL0;
 
    if((gif = a_tty_bind_ctx_find(c.cp)) == (enum n_go_input_flags)-1){
@@ -4378,7 +4351,7 @@ c_unbind(void *v){
       gif = 0;
    }
 
-   c.cp = cac.cac_arg->ca_next->ca_arg.ca_str.s;
+   c.cp = cacp->cac_arg->ca_next->ca_arg.ca_str.s;
 jredo:
    if(n_is_all_or_aster(c.cp)){
       while((tbcp = a_tty.tg_bind[gif]) != NULL){
@@ -4389,7 +4362,7 @@ jredo:
       }
    }else{
       memset(&tbpc, 0, sizeof tbpc);
-      tbpc.tbpc_cmd = a_tty_unbind_cad.cad_name;
+      tbpc.tbpc_cmd = cacp->cac_desc->cad_name;
       tbpc.tbpc_in_seq = c.cp;
       tbpc.tbpc_flags = gif;
 

accmacvar.c
@@ -257,7 +257,7 @@ static struct a_amv_var *a_amv_compose_lopts;
 static struct a_amv_mac *a_amv_mac_lookup(char const *name,
                            struct a_amv_mac *newamp, enum a_amv_mac_flags amf);
 
-/* `call', `call_if' */
+/* `call', `call_if' (and `xcall' via go.c -> c_call()) */
 static int a_amv_mac_call(void *v, bool_t silent_nexist);
 
 /* Execute a macro; amcap must reside in LOFI memory */
@@ -394,47 +394,59 @@ jleave:
 
 static int
 a_amv_mac_call(void *v, bool_t silent_nexist){
-   int rv;
    struct a_amv_mac *amp;
+   int rv;
    char const *name;
+   struct n_cmd_arg_ctx *cacp;
    NYD_ENTER;
 
-   name = *(char const**)v;
+   cacp = v;
 
-   if((amp = a_amv_mac_lookup(name, NULL, a_AMV_MF_NONE)) != NULL){
+   if(cacp->cac_no == 0){
+      n_err(_("Synopsis: call(_if)?: name [:<arguments>:]\n"));
+      n_pstate_err_no = n_ERR_INVAL;
+      rv = 1;
+      goto jleave;
+   }
+
+   name = cacp->cac_arg->ca_arg.ca_str.s;
+
+   if(n_UNLIKELY(cacp->cac_no > a_AMV_POSPAR_MAX)){
+      n_err(_("Too many arguments to macro `call': %s\n"), name);
+      n_pstate_err_no = n_ERR_OVERFLOW;
+      rv = 1;
+   }else if(n_UNLIKELY((amp = a_amv_mac_lookup(name, NULL, a_AMV_MF_NONE)
+         ) == NULL)){
+      if(!silent_nexist)
+         n_err(_("Undefined macro called: %s\n"), n_shexp_quote_cp(name, FAL0));
+      n_pstate_err_no = n_ERR_NOENT;
+      rv = 1;
+   }else{
+      char const **argv;
       struct a_amv_mac_call_args *amcap;
+      size_t argc;
 
-      amcap = n_lofi_alloc(sizeof *amcap);
+      argc = cacp->cac_no + 1;
+      amcap = n_lofi_alloc(sizeof *amcap + (argc * sizeof *argv));
+      argv = (void*)&amcap[1];
+
+      for(argc = 0; (cacp->cac_arg = cacp->cac_arg->ca_next) != NULL; ++argc)
+         argv[argc] = cacp->cac_arg->ca_arg.ca_str.s;
+
+      argv[argc] = NULL;
+
       memset(amcap, 0, sizeof *amcap);
       amcap->amca_name = name;
       amcap->amca_amp = amp;
-      /* C99 */{
-         char const **argv;
-         size_t argc;
 
-         for(argc = 0, argv = v; *++argv != NULL; ++argc)
-            ;
-         if(argc > 0){
-            if(argc > a_AMV_POSPAR_MAX){
-               n_err(_("Too many arguments to macro `call': %s\n"), name);
-               n_pstate_err_no = n_ERR_OVERFLOW;
-               rv = 1;
-               goto jleave;
-            }
-            amcap->amca_pospar.app_count = (ui16_t)argc;
-            amcap->amca_pospar.app_not_heap = TRU1;
-            amcap->amca_pospar.app_dat = &(argv = v)[1];
-         }
+      if(argc > 0){
+         amcap->amca_pospar.app_count = (ui16_t)argc;
+         amcap->amca_pospar.app_not_heap = TRU1;
+         amcap->amca_pospar.app_dat = argv;
       }
 
-      rv = a_amv_mac_exec(amcap);
+      (void)a_amv_mac_exec(amcap);
       rv = n_pstate_ex_no;
-   }else{
-      if((rv = (silent_nexist == FAL0)))
-         n_err(_("Undefined macro `call'ed: %s\n"),
-            n_shexp_quote_cp(name, FAL0));
-      n_pstate_err_no = n_ERR_NOENT;
-      rv = 1;
    }
 jleave:
    NYD_LEAVE;
@@ -1959,21 +1971,21 @@ c_undefine(void *v){
 }
 
 FL int
-c_call(void *v){
+c_call(void *vp){
    int rv;
    NYD_ENTER;
 
-   rv = a_amv_mac_call(v, FAL0);
+   rv = a_amv_mac_call(vp, FAL0);
    NYD_LEAVE;
    return rv;
 }
 
 FL int
-c_call_if(void *v){
+c_call_if(void *vp){
    int rv;
    NYD_ENTER;
 
-   rv = a_amv_mac_call(v, TRU1);
+   rv = a_amv_mac_call(vp, TRU1);
    NYD_LEAVE;
    return rv;
 }
@@ -3279,6 +3291,7 @@ jestr:
 
 FL int
 c_vpospar(void *v){
+   struct n_cmd_arg *cap;
    size_t i;
    struct a_amv_pospar *appp;
    enum{
@@ -3288,35 +3301,45 @@ c_vpospar(void *v){
       a_CLEAR = 1u<<2,
       a_QUOTE = 1u<<3
    } f;
-   char const **argv, *varname, *varres, *cp;
+   char const *varres;
+   struct n_cmd_arg_ctx *cacp;
    NYD_ENTER;
 
    n_pstate_err_no = n_ERR_NONE;
-   argv = v;
-   varname = (n_pstate & n_PS_ARGMOD_VPUT) ? *argv++ : NULL;
    n_UNINIT(varres, n_empty);
+   cacp = v;
+   cap = cacp->cac_arg;
 
-   if(is_asccaseprefix(argv[0], "set"))
+   if(is_asccaseprefix(cap->ca_arg.ca_str.s, "set"))
       f = a_SET;
-   else if(is_asccaseprefix(argv[0], "clear"))
+   else if(is_asccaseprefix(cap->ca_arg.ca_str.s, "clear"))
       f = a_CLEAR;
-   else if(is_asccaseprefix(argv[0], "quote"))
+   else if(is_asccaseprefix(cap->ca_arg.ca_str.s, "quote"))
       f = a_QUOTE;
    else{
       n_err(_("`vpospar': invalid subcommand: %s\n"),
-         n_shexp_quote_cp(argv[0], FAL0));
+         n_shexp_quote_cp(cap->ca_arg.ca_str.s, FAL0));
       n_pstate_err_no = n_ERR_INVAL;
       f = a_ERR;
       goto jleave;
    }
-   ++argv;
+   --cacp->cac_no;
+
+   if((f & (a_CLEAR | a_QUOTE)) && cap->ca_next != NULL){
+      n_err(_("`vpospar': `%s': takes no argument\n"), cap->ca_arg.ca_str.s);
+      n_pstate_err_no = n_ERR_INVAL;
+      f = a_ERR;
+      goto jleave;
+   }
+
+   cap = cap->ca_next;
 
    /* If in a macro, we need to overwrite the local instead of global argv */
    appp = (a_amv_lopts != NULL) ? &a_amv_lopts->as_amcap->amca_pospar
          : &a_amv_pospar;
 
    if(f & (a_SET | a_CLEAR)){
-      if(varname != NULL && (n_poption & n_PO_D_V))
+      if(cacp->cac_vput != NULL && (n_poption & n_PO_D_V))
          n_err(_("`vpospar': `vput' only supported for `quote' subcommand\n"));
       if(!appp->app_not_heap && appp->app_maxcount > 0){
          for(i = appp->app_maxcount; i-- != 0;)
@@ -3326,9 +3349,7 @@ c_vpospar(void *v){
       memset(appp, 0, sizeof *appp);
 
       if(f & a_SET){
-         for(i = 0; argv[i] != NULL; ++i)
-            ;
-         if(i > a_AMV_POSPAR_MAX){
+         if((i = cacp->cac_no) > a_AMV_POSPAR_MAX){
             n_err(_("`vpospar': overflow: %" PRIuZ " arguments!\n"), i);
             n_pstate_err_no = n_ERR_OVERFLOW;
             f = a_ERR;
@@ -3343,12 +3364,10 @@ c_vpospar(void *v){
             i *= sizeof *appp->app_dat;
             appp->app_dat = n_alloc(i);
 
-            for(i = 0; (cp = argv[i]) != NULL; ++i){
-               size_t j;
-
-               j = strlen(cp) +1;
-               appp->app_dat[i] = n_alloc(j);
-               memcpy(n_UNCONST(appp->app_dat[i]), cp, j);
+            for(i = 0; cap != NULL; ++i, cap = cap->ca_next){
+               appp->app_dat[i] = n_alloc(cap->ca_arg.ca_str.l +1);
+               memcpy(n_UNCONST(appp->app_dat[i]), cap->ca_arg.ca_str.s,
+                  cap->ca_arg.ca_str.l +1);
             }
 
             appp->app_dat[i] = NULL;
@@ -3360,15 +3379,29 @@ c_vpospar(void *v){
       else{
          struct str in;
          struct n_string s, *sp;
+         char sep1, sep2;
 
          sp = n_string_creat_auto(&s);
 
+         sep1 = *ok_vlook(ifs);
+         sep2 = *ok_vlook(ifs_ws);
+         if(sep1 == sep2)
+            sep2 = '\0';
+         if(sep1 == '\0')
+            sep1 = ' ';
+
          for(i = 0; i < appp->app_count; ++i){
-            if(sp->s_len)
-               sp = n_string_push_c(sp, ' ');
+            if(sp->s_len){
+               if(!n_string_can_swallow(sp, 2))
+                  goto jeover;
+               sp = n_string_push_c(sp, sep1);
+               if(sep2 != '\0')
+                  sp = n_string_push_c(sp, sep2);
+            }
             in.l = strlen(in.s = n_UNCONST(appp->app_dat[i + appp->app_idx]));
 
             if(!n_string_can_swallow(sp, in.l)){
+jeover:
                n_err(_("`vpospar': overflow: string too long!\n"));
                n_pstate_err_no = n_ERR_OVERFLOW;
                f = a_ERR;
@@ -3380,12 +3413,12 @@ c_vpospar(void *v){
          varres = n_string_cp(sp);
       }
 
-      if(varname == NULL){
+      if(cacp->cac_vput == NULL){
          if(fprintf(n_stdout, "%s\n", varres) < 0){
             n_pstate_err_no = n_err_no;
             f |= a_ERR;
          }
-      }else if(!n_var_vset(varname, (uintptr_t)varres)){
+      }else if(!n_var_vset(cacp->cac_vput, (uintptr_t)varres)){
          n_pstate_err_no = n_ERR_NOTSUP;
          f |= a_ERR;
       }

attachment.c
@@ -343,9 +343,10 @@ n_attachment_append_list(struct attachment *aplist, char const *names){
       struct attachment *nap;
       enum n_shexp_state shs;
 
-      shs = n_shexp_parse_token((n_SHEXP_PARSE_TRUNC | n_SHEXP_PARSE_TRIMSPACE |
-            n_SHEXP_PARSE_LOG | n_SHEXP_PARSE_IFS_ADD_COMMA |
-            n_SHEXP_PARSE_IGNORE_EMPTY), shoup, &shin, NULL);
+      shs = n_shexp_parse_token((n_SHEXP_PARSE_TRUNC |
+            n_SHEXP_PARSE_TRIM_SPACE | n_SHEXP_PARSE_LOG |
+            n_SHEXP_PARSE_IFS_ADD_COMMA | n_SHEXP_PARSE_IGNORE_EMPTY),
+            shoup, &shin, NULL);
       if(shs & n_SHEXP_STATE_ERR_MASK)
          break;
 

commit e0edb15eb031687e39d763bcffa6c70cc2f7c0ec

MLE: FIX `bind' crash if expansion is empty..
Aouh-wou-wou!

This was not possible earlier (i am _absolutely_ sure i have
tested it once i have implemented `bind', has this not reached the
final changeset??!!!), but i would say that has been broken by
[fba091c1] (Various fixes to actually integrate IFS for at least
some for real.., 2017-05-16), which actually integrated the new
(still not widely used) option parser into the normal command
table.

We already tested for empty expansion, but only if we saw the
no-commit @ indicator.

nail.1
@@ -4326,6 +4326,7 @@ expansion shall instead be furtherly editable by the user, a commercial at
 (that will be removed) can be placed last in the expansion, from which
 leading and trailing whitespace will finally be removed.
 Reverse solidus cannot be used as the last character of expansion.
+An empty expansion will be rejected.
 .
 .Pp
 Contexts define when a binding applies, i.e., a binding will not be seen

tty.c
@@ -3746,10 +3746,12 @@ jeempty:
 
       i = tbpcp->tbpc_exp.l;
       if(i > 0 && exp[i - 1] == '@'){
-         while(--i > 0){
-            if(!blankspacechar(exp[i - 1]))
+         --i;
+#if 0 /* xxx no: allow trailing whitespace, as in 'echo du @' .. */
+         while(--i > 0)
+            if(!su_cs_is_space(exp[i - 1]))
                break;
-         }
+#endif
          if(i == 0)
             goto jeempty;
 
@@ -3765,6 +3767,9 @@ jeempty:
             tbpcp->tbpc_cmd, tbpcp->tbpc_in_seq);
          goto jleave;
       }
+
+      if(i == 0)
+         goto jeempty;
 
       /* It may map to an internal MLE command! */
       for(i = 0; i < n_NELEM(a_tty_bind_fun_names); ++i)