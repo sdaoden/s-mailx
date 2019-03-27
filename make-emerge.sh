c7ad63803a40d5ab648b9e3a826d2f085e073937
Steffen Nurpmeso <steffen@sdaoden.eu>
2019-03-27 01:32:51 +0100

    make-emerge.sh: use mk/su-find-command.sh

--- make-emerge.sh
+++ make-emerge.sh
@@ -16,76 +16,9 @@ fi
 [ -n "${SHELL}" ] || SHELL=/bin/sh
 export SHELL
 
-( set -o noglob ) >/dev/null 2>&1 && noglob_shell=1 || unset noglob_shell
 
 config_exit() {
    exit ${1}
-}
-
-msg() {
-   fmt=${1}
-   shift
-   printf >&2 -- "${fmt}\\n" "${@}"
-}
-
-# which(1) not standardized, command(1) -v may return non-executable: unroll!
-acmd_test() { __acmd "${1}" 1 0 0; }
-acmd_test_fail() { __acmd "${1}" 1 1 0; }
-acmd_set() { __acmd "${2}" 0 0 0 "${1}"; }
-acmd_set_fail() { __acmd "${2}" 0 1 0 "${1}"; }
-acmd_testandset() { __acmd "${2}" 1 0 0 "${1}"; }
-acmd_testandset_fail() { __acmd "${2}" 1 1 0 "${1}"; }
-thecmd_set() { __acmd "${2}" 0 0 1 "${1}"; }
-thecmd_set_fail() { __acmd "${2}" 0 1 1 "${1}"; }
-thecmd_testandset() { __acmd "${2}" 1 0 1 "${1}"; }
-thecmd_testandset_fail() { __acmd "${2}" 1 1 1 "${1}"; }
-__acmd() {
-   pname=${1} dotest=${2} dofail=${3} verbok=${4} varname=${5}
-
-   if [ "${dotest}" -ne 0 ]; then
-      eval dotest=\$${varname}
-      if [ -n "${dotest}" ]; then
-         [ -n "${VERBOSE}" ] && [ ${verbok} -ne 0 ] &&
-            msg ' . ${%s} ... %s' "${pname}" "${dotest}"
-         return 0
-      fi
-   fi
-
-   oifs=${IFS} IFS=:
-   [ -n "${noglob_shell}" ] && set -o noglob
-   set -- ${PATH}
-   [ -n "${noglob_shell}" ] && set +o noglob
-   IFS=${oifs}
-   for path
-   do
-      if [ -z "${path}" ] || [ "${path}" = . ]; then
-         if [ -d "${PWD}" ]; then
-            path=${PWD}
-         else
-            path=.
-         fi
-      fi
-      if [ -f "${path}/${pname}" ] && [ -x "${path}/${pname}" ]; then
-         [ -n "${VERBOSE}" ] && [ ${verbok} -ne 0 ] &&
-            msg ' . ${%s} ... %s' "${pname}" "${path}/${pname}"
-         [ -n "${varname}" ] && eval ${varname}="${path}/${pname}"
-         return 0
-      fi
-   done
-
-   # We may have no builtin string functions, we yet have no programs we can
-   # use, try to access once from the root, assuming it is an absolute path if
-   # that finds the executable
-   if ( cd && [ -f "${pname}" ] && [ -x "${pname}" ] ); then
-     [ -n "${VERBOSE}" ] && [ ${verbok} -ne 0 ] &&
-            msg ' . ${%s} ... %s' "${pname}" "${pname}"
-      [ -n "${varname}" ] && eval ${varname}="${pname}"
-      return 0
-   fi
-
-   [ ${dofail} -eq 0 ] && return 1
-   msg 'ERROR: no trace of utility '"${pname}"
-   exit 1
 }
 
 syno() {
@@ -110,6 +43,8 @@ oneslash() {
 }
 
 [ ${#} -eq 0 ] || syno
+
+SU_FIND_COMMAND_INCLUSION=1 . ${0%/*}/mk/su-find-command.sh
 
 # Rude simple, we should test for Solaris, but who runs this script?
 if [ -d /usr/xpg4 ]; then
