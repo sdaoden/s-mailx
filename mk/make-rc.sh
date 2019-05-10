084e25739854ef5e07603411867c5a0386e337ef
Steffen Nurpmeso <steffen@sdaoden.eu>
2019-05-10 15:47:51 +0200

    mk/make-rc.sh: added: split rc creation off make-config.in (Alexander Harm)

---
+++ mk/make-rc.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -
+#@ Adjust rc file according to configuration settings (sourced in environment).
+#
+# Public Domain
+
+LC_ALL=C
+
+< "$1" > "$2" exec ${awk} '
+   BEGIN {written = 0}
+   /^#--MKRC-START--/,/^#--MKRC-END--/{
+      if(written == 1)
+         next
+      written = 1
+      OFS = ""
+      ln = tolower(ENVIRON["VAL_UAGENT"])
+      cn = toupper(substr(ln, 1, 1)) substr(ln, 2)
+      print "#@ ", ENVIRON["VAL_SYSCONFDIR"], "/", ENVIRON["VAL_SYSCONFRC"]
+      print "#@ Configuration file for ", cn, " v", ENVIRON["VERSION"], "."
+      OFS = " "
+      next
+   }
+   {print}
+'
+
+# s-sh-mode
