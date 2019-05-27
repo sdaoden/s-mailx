4764f7a81ae1b4621349330f74353882a91eab56
Steffen Nurpmeso <steffen@sdaoden.eu>
2019-05-27 22:25:03 +0200

    mk/su-quote-roundtrip.sh: do not leak local vars (Steven Penny)

--- mk/su-quote-rndtrip.sh
+++ mk/su-quote-rndtrip.sh
@@ -10,20 +10,20 @@
 # 2017 - 2019 Steffen (Daode) Nurpmeso <steffen@sdaoden.eu>.
 # Public Domain
 
-# Though slower use a subshell version instead of properly restoring $IFS and
-# flags, as elder shells may not be able to properly restore flags via "set +o"
-# as later standardized in POSIX, and it seems overkill to handle all possible
-# forms of output that "set +o" may or may not actually generate
+# Though slower use a subshell version instead of properly restoring $IFS
+# and flags, as elder shells may not be able to properly restore flags via
+# "set +o" as later standardized in POSIX, and it seems overkill to handle
+# all possible forms of output "set +o" may or may not actually generate.
 quote__rndtrip() (
    case "$1" in
    *\'*) ;;
    *) printf "'%s'" "$1"; return 0;;
    esac
-   __A__="$1" __S__= __E__=
-   while case "$__A__" in
-      \'*)  __A__=${__A__#?}; __S__="${__S__}\\\\'";;
-      *\')  __A__=${__A__%?}; __E__="${__E__}\\\\'";;
-      '')   printf "${__S__}${__E__}"; exit 0;;
+   a="$1" s= e=
+   while case "$a" in
+      \'*)  a=${a#?}; s="${s}\\\\'";;
+      *\')  a=${a%?}; e="${e}\\\\'";;
+      '')   printf "${s}${e}"; exit 0;;
       *) false;;
       esac
    do
@@ -31,18 +31,18 @@ quote__rndtrip() (
    done
    IFS=\'
    set -f
-   set -- $__A__
-   _result_="${1}"
+   set -- $a
+   r="${1}"
         shift
-   for __A__
+   for a
    do
-      _result_="${_result_}'\\''${__A__}"
+      r="${r}'\\''${a}"
    done
-   printf "${__S__}'%s'${__E__}" "${_result_}"
+   printf "${s}'%s'${e}" "${r}"
    exit 0
 )
 
-quote_rndtrip() {
+quote_rndtrip() (
    j=
    for i
    do
@@ -50,9 +50,9 @@ quote_rndtrip() {
       j=' '
       quote__rndtrip "$i"
    done
-}
+)
 
-quote_string() {
+quote_string() (
    j=
    for i
    do
@@ -60,6 +60,6 @@ quote_string() {
       j=' '
       quote__rndtrip "$i"
    done
-}
+)
 
 # s-sh-mode
