b05f27a6666c0988e2584b535e8347baab7cdaa5
Steffen Nurpmeso <steffen@sdaoden.eu>
2019-08-26 23:32:45 +0200

    [net-test] Add OPT_NET_TEST, by default enabled, and src/net-test stub

---
+++ src/net-test/main.c
@@ -0,0 +1,38 @@
+/*@ S-nail - a mail user agent derived from Berkeley Mail.
+ *@ Simple optional pseudo server to test network I/O and protocols.
+ *
+ * Copyright (c) 2019 Steffen (Daode) Nurpmeso <steffen@sdaoden.eu>.
+ * SPDX-License-Identifier: ISC
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#undef su_FILE
+#define su_FILE net_test_main
+#define mx_SOURCE
+#define mx_SOURCE_NET_TEST_MAIN
+
+#include <sys/socket.h>
+
+#include <errno.h>
+#include <string.h>
+
+int
+main(int argc, char **argv){
+
+:
+
+
+   return 0;
+}
+
+/* s-it-mode */
