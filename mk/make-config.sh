a507b4699939813c5507abcf6799e53ed6189ce1
Yasuhiro KIMURA <yasu@utahime.org>
2019-09-04 19:38:33 +0200

    mk/make-{config,install}.sh: FIX strip(1)ing (Yasuhiro KIMURA)..
    __stripfile() strips ${_file} only when value of ${OPT_DEBUG} is zero
    and length of ${HAVE_STRIP} is nonzero. However, HAVE_STRIP is set in
    mk/make-config.sh but not written to mk-config.mk. Therefore
    __stripfile() never strips ${_file} in any cases. So change logic of
    __stripfile() so it strips ${_file} only when value of ${OPT_DEBUG} is
    zero and length of ${strip} is nonzero.

--- mk/make-config.sh
+++ mk/make-config.sh
@@ -1356,7 +1356,7 @@ PATH=${__PATH}
 thecmd_testandset_fail MAKE make
 make=${MAKE}
 export MAKE
-thecmd_testandset strip strip && HAVE_STRIP=1 || HAVE_STRIP=0
+thecmd_testandset strip strip
 
 # For ./mx-test.sh only
 thecmd_testandset_fail cksum cksum
