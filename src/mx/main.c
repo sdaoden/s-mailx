e821b217f903fcb467744ac2d474530e3f16057e
Steffen Nurpmeso <steffen@sdaoden.eu>
2019-12-22 21:30:31 +0100

    a_main_dump_doc(): add an UNUSED()

--- src/mx/main.c
+++ src/mx/main.c
@@ -646,6 +646,7 @@ a_main_dump_doc(up cookie, boole has_arg, char const *sopt, char const *lopt,
       char const *doc){
    char const *x1, *x2;
    NYD2_IN;
+   UNUSED(doc);
 
    if(has_arg)
       /* I18N: describing arguments to command line options */
@@ -655,6 +656,7 @@ a_main_dump_doc(up cookie, boole has_arg, char const *sopt, char const *lopt,
       x1 = (sopt[0] != '\0' ? _(", ") : sopt), x2 = su_empty;
    /* I18N: short option, "[ ARG], " separator, long option [=ARG], doc */
    fprintf(S(FILE*,cookie), _("%s%s%s%s: %s\n"), sopt, x1, lopt, x2, V_(doc));
+
    NYD2_OU;
    return TRU1;
 }
