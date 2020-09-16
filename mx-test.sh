f1287e3c9ddb5e5d8daec5ab923ef6d1dbf10827
Steffen Nurpmeso <steffen@sdaoden.eu>
2020-09-16 22:28:57 +0200

    FIXME [-] (BWDIC!) Rework Reply-To:, fix *-add-cc* <-> Reply-To:, etc. (Geoff Clare)..
    After a discussion on the POSIX list i realized that our Reply-To:
    handling is not forbidden but the most strange possible,
    effectively acting like the never standardized Mail-Followup-To:.
    Not only because i stumbled about the consequences myself multiple
    times i think it is for the much better if this behaviour is
    simply thrown overboard.
    
    The email-standard desired behaviour requires two new pseudo
    headers so that hooks etc. know about edited origins (and
    sender), so invent Mailx-Edited-Sender: and Mailx-Edited-Origin:
    pseudo header fields.
    
    It also requires changes to our *forward-add-cc* and
    *quote-add-cc* functionality, which now have to take into account
    a possibly set Reply-To:.
    
    Alongside all this try to organize the code a little bit more
    sensible.  At least until all that gets hidden under an object
    interface, shall we ever reach this state.

--- mx-test.sh
+++ mx-test.sh
@@ -14380,6 +14380,8 @@ define t_oce {
 		digmsg - hea show mailx-orig-tO;\
 		digmsg - hea show mailx-orig-Cc;\
 		digmsg - hea show mailx-oriG-bcc;\
+		digmsg - hea show maIlx-editeD-SENDER;\
+		digmsg - hea show maIlx-editeD-origiN;\
 		digmsg remove -;ec "$?/$!/$^ERRNAME"
 	digmsg create -;ec "$?/$!/$^ERRNAME";\
 		digmsg - hea list;readall x;echon "$x";\
@@ -14408,6 +14410,8 @@ define t_ocl {
 		digmsg - hea show mailx-orig-tO;\
 		digmsg - hea show mailx-orig-Cc;\
 		digmsg - hea show mailx-oriG-bcc;\
+		digmsg - hea show maIlx-editeD-SENDER;\
+		digmsg - hea show maIlx-editeD-origiN;\
 		digmsg remove -;ec "$?/$!/$^ERRNAME"
 	digmsg create -;ec "$?/$!/$^ERRNAME";\
 		digmsg - hea list;readall x;echon "$x";\
@@ -14432,6 +14436,8 @@ define t_occ {
 		digmsg - hea show mailx-orig-tO;\
 		digmsg - hea show mailx-orig-Cc;\
 		digmsg - hea show mailx-oriG-bcc;\
+		digmsg - hea show maIlx-editeD-SENDER;\
+		digmsg - hea show maIlx-editeD-origiN;\
 		digmsg remove -;ec "$?/$!/$^ERRNAME"
 	# ditto digmsg create -;ec "$?/$!/$^ERRNAME";\
 		digmsg - hea list;readall x;echon "$x";\
