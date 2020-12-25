28765c784bc3a04ff312c9ad62b9f96bf17975f4
Steffen Nurpmeso <steffen@sdaoden.eu>
2020-12-25 01:01:21 +0100

    [-] Add ?* mimetype type-marker, work around standard restriction (Olaf Hering)..
    Because MIME media type handlers, like those defined in "The
    Mailcap files", match media types, non-standardized fictional
    types like "x-tar-gz" are used in the wild as MIME environments
    become configured.
    
    That is, IANA MIME registry standards do not know about "extension
    chains": "tar.gz" is thus a gzip(1) compressed file, and the sane
    solution of recursively unpacking until no more MIME media type
    unpacking is possible is not available.
    
    Therefore introduce this type-marker, which says that the given
    MIME media type shall only be matched when looking for handlers,
    but not when classifying content to create messages.

--- mk/make-mime-types.sh
+++ mk/make-mime-types.sh
@@ -20,30 +20,39 @@ LC_ALL=C
          e_a[mt] = e_a[mt] e
       }
    }
+
    /^[ 	]*#/{next}
    /^[ 	]*$/{ltype = ""; next}
    /^[ 	]/{
       if(!ltype)
-         print "FAULTY CONTINUATION: " $0 >> "/dev/stderr"
+         print "! FAULTY MIME TYPE CONTINUATION: " $0 >> "/dev/stderr"
       add(ltype, $0)
       next
    }
-   /^(\?([thHq])? )?[a-zA-Z]/{
-      if($1 ~ /^\?([thHq])?$/){
+   /^(\?([*thHq]*) )?[a-zA-Z]/{
+      # type-marker?
+      if($1 ~ /^\?([*thHq]*)$/){
          pa = $1
          $1 = $2
          $2 = ""
       }else
          pa = ""
+
       if($1 !~ /^([0-9a-zA-Z]+)\/([0-9a-zA-Z_+-]+)$/)
-         print "FAULTY MIME TYPE: <" $1 ">" >> "/dev/stderr"
+         print "! FAULTY MIME TYPE: <" $1 ">" >> "/dev/stderr"
       ltype = $1; $1 = ""
-      if(pa)
-         p_a[ltype] = pa
-      if(!nt_a[ltype])
+
+      if(!nt_a[ltype]){
+         if(pa)
+            p_a[ltype] = pa
          no_a[++no_ai] = nt_a[ltype] = ltype
+      }else if(pa && p_a[ltype])
+         print "! TYPE-MARKER IGNORED WHEN EXTENDING MIME TYPE: <" \
+            ltype ">: " $0 >> "/dev/stderr"
+
       add(ltype, $0)
    }
+
    END{
       for(z = 1; z <= no_ai; ++z){
          t = no_a[z]
@@ -51,19 +60,47 @@ LC_ALL=C
          mt = toupper(substr(t, 1, j - 1))
          j = substr(t, j + 1)
          l = length(j)
+
+         # type-marker?
          if(!p_a[t])
             mt = "a_MT_" mt
          else{
             tm = p_a[t]
-            if(tm ~ /^\?t?$/)
-               mt = "a_MT_" mt " | a_MT_TM_PLAIN"
-            else if(tm ~ /^\?h$/)
-               mt = "a_MT_" mt " | a_MT_TM_SOUP_h"
-            else if(tm ~ /^\?H$/)
-               mt = "a_MT_" mt " | a_MT_TM_SOUP_H"
-            else if(tm ~ /^\?q$/)
-               mt = "a_MT_" mt " | a_MT_TM_QUIET"
+            tmx = ""
+            aster = 0
+
+            while(length(tm) > 0){
+               x = substr(tm, 1, 1)
+               tm = substr(tm, 2)
+               if(x == "*"){
+                  if(aster++ != 0){
+                     print "! DUPLICATE * MIME TYPE-MARKER: <" no_a[z] ">" \
+                        " <" p_a[t] ">" >> "/dev/stderr"
+                  }
+               }else{
+                  if(tmx)
+                     print "! INVALID MIME TYPE-MARKER: <" no_a[z] ">" \
+                        " <" p_a[t] ">" >> "/dev/stderr"
+                  if(x == "t")
+                     tmx = "a_MT_TM_PLAIN"
+                  else if(x == "h")
+                     tmx = "a_MT_TM_SOUP_h"
+                  else if(x == "H")
+                     tmx = "a_MT_TM_SOUP_H"
+                  else if(x == "q")
+                     tmx = "a_MT_TM_QUIET"
+               }
+            }
+
+            mt = "a_MT_" mt
+            if(!tmx && !aster)
+               tmx = "a_MT_TM_PLAIN"
+            if(aster)
+               mt = mt " | a_MT_TM_HDL_ONLY"
+            if(tmx)
+               mt = mt " | " tmx
          }
+
          print "   {" mt ", " l ", \"" j e_a[t] "\"},"
       }
    }
