941dfb1ace788b62cff015cbb2a9d33282de1927
Steffen Nurpmeso <steffen@sdaoden.eu>
2021-02-24 02:38:16 +0100

    Bump S-nail v14.9.22.ar ("Feathery sound of close tit flypast"), 2021-02-24

--- include/su/config.h
+++ include/su/config.h
@@ -21,14 +21,11 @@
 #endif
 #ifndef su_CONFIG_H
 #define su_CONFIG_H
-
 /*#define su_HAVE_NSPC*/
-
 /* For now thought of _MX, _ROFF; _SU: standalone library */
 #ifndef su_USECASE_SU
 # define su_USECASE_MX
 #endif
-
 #ifdef su_USECASE_MX
    /* In this case we get our config, error maps etc., all from here.
     * We must take care not to break OPT_AMALGAMATION though */
@@ -38,40 +35,30 @@
 #else
 # include <su/gen-config.h>
 #endif
-
 /* Internal configurables: values */
-
 /* Number of Not-Yet-Dead calls that are remembered */
 #define su_NYD_ENTRIES (25 * 84)
-
 /* Global configurables (code.h:CONFIG): features */
-
 #ifdef mx_HAVE_DEBUG
 # define su_HAVE_DEBUG
 #endif
 #ifdef mx_HAVE_DEVEL
 # define su_HAVE_DEVEL
 #endif
-
 #ifdef mx_HAVE_DOCSTRINGS
 # define su_HAVE_DOCSTRINGS
 #endif
-
 #define su_HAVE_MEM_BAG_AUTO
 #define su_HAVE_MEM_BAG_LOFI
 #ifdef mx_HAVE_NOMEMDBG
 # define su_HAVE_MEM_CANARIES_DISABLE
 #endif
-
 #undef su_HAVE_MT
 #undef su_HAVE_SMP
-
 /* Global configurables (code.h:CONFIG): values */
-
 /* Hardware page size (xxx additional dynamic lookup support) */
 #ifndef su_PAGE_SIZE
 # error Need su_PAGE_SIZE configuration
 #endif
-
 #endif /* !su_CONFIG_H */
 /* s-it-mode */
