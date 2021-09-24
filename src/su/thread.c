7a92770eca2cbb6edb0699c4a19e95f8ed77e0c7
Steffen Nurpmeso <steffen@sdaoden.eu>
2021-09-24 20:37:05 +0200

    [SU.2] SU: add some dummy atomic.h, mutex.h, spinlock.h, thread.h

---
+++ src/su/thread.c
@@ -0,0 +1,46 @@
+/*@ Implementation of thread.h.
+ *
+ * Copyright (c) 2001 - 2021 Steffen (Daode) Nurpmeso <steffen@sdaoden.eu>.
+ * SPDX-License-Identifier: ISC
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#define su_FILE su_thread
+#define su_SOURCE
+#define su_SOURCE_THREAD
+
+#include "su/code.h"
+
+su_USECASE_MX_DISABLED
+su_EMPTY_FILE()
+#ifdef su_HAVE_MT
+
+#include "su/thread.h"
+#include "su/y-thread.h" /* $(SU_SRCDIR) */
+/*#define NYDPROF_ENABLE*/
+/*#define NYD_ENABLE*/
+/*#define NYD2_ENABLE*/
+#include "su/code-in.h"
+
+/* struct su_thread su__thread_main; -> in core-code.c! */
+
+#include <su/y-thread.h> /* 2. */
+
+#include <su/y-thread.h> /* 3. */
+
+#include "su/code-ou.h"
+#endif /* su_HAVE_MT */
+#undef su_FILE
+#undef su_SOURCE
+#undef su_SOURCE_THREAD
+/* s-it-mode */
