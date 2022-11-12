769819bc80955d9fff012608794f3c56a893485a
Steffen Nurpmeso <steffen@sdaoden.eu>
2022-11-12 23:14:41 +0100

    mk: sync scripts

--- mk/make-mime-types.sh
+++ mk/make-mime-types.sh
@@ -6,104 +6,100 @@
 LC_ALL=C
 
 < "$1" > "$2" exec ${awk} '
-   function add(mt, ln){
-      gsub(/[ 	]]+/, " ", ln)
-      i = split(ln, i_a)
-      e = ""
-      for(j = 1; j <= i; ++j){
-         k = i_a[j]
-         e = (e ? e " " : "") k
-      }
-      if(e){
-         if(e_a[mt])
-            e_a[mt] = e_a[mt] " "
-         e_a[mt] = e_a[mt] e
-      }
-   }
+	function add(mt, ln){
+		gsub(/[	 ]]+/, " ", ln)
+		i = split(ln, i_a)
+		e = ""
+		for(j = 1; j <= i; ++j){
+			k = i_a[j]
+			e = (e ? e " " : "") k
+		}
+		if(e){
+			if(e_a[mt])
+				e_a[mt] = e_a[mt] " "
+			e_a[mt] = e_a[mt] e
+		}
+	}
 
-   /^[ 	]*#/{next}
-   /^[ 	]*$/{ltype = ""; next}
-   /^[ 	]/{
-      if(!ltype)
-         print "! FAULTY MIME TYPE CONTINUATION: " $0 >> "/dev/stderr"
-      add(ltype, $0)
-      next
-   }
-   /^(\?([*thHq]*) )?[a-zA-Z]/{
-      # type-marker?
-      if($1 ~ /^\?([*thHq]*)$/){
-         pa = $1
-         $1 = $2
-         $2 = ""
-      }else
-         pa = ""
+	/^[	 ]*#/{next}
+	/^[	 ]*$/{ltype = ""; next}
+	/^[	 ]/{
+		if(!ltype)
+			print "! FAULTY MIME TYPE CONTINUATION: " $0 >> "/dev/stderr"
+		add(ltype, $0)
+		next
+	}
+	/^(\?([*thHq]*) )?[a-zA-Z]/{
+		# type-marker?
+		if($1 ~ /^\?([*thHq]*)$/){
+			pa = $1
+			$1 = $2
+			$2 = ""
+		}else
+			pa = ""
 
-      if($1 !~ /^([0-9a-zA-Z]+)\/([0-9a-zA-Z_+-]+)$/)
-         print "! FAULTY MIME TYPE: <" $1 ">" >> "/dev/stderr"
-      ltype = $1; $1 = ""
+		if($1 !~ /^([0-9a-zA-Z]+)\/([0-9a-zA-Z_+-]+)$/)
+			print "! FAULTY MIME TYPE: <" $1 ">" >> "/dev/stderr"
+		ltype = $1; $1 = ""
 
-      if(!nt_a[ltype]){
-         if(pa)
-            p_a[ltype] = pa
-         no_a[++no_ai] = nt_a[ltype] = ltype
-      }else if(pa && p_a[ltype])
-         print "! TYPE-MARKER IGNORED WHEN EXTENDING MIME TYPE: <" \
-            ltype ">: " $0 >> "/dev/stderr"
+		if(!nt_a[ltype]){
+			if(pa)
+				p_a[ltype] = pa
+			no_a[++no_ai] = nt_a[ltype] = ltype
+		}else if(pa && p_a[ltype])
+			print "! TYPE-MARKER IGNORED WHEN EXTENDING MIME TYPE: <" ltype ">: " $0 >> "/dev/stderr"
 
-      add(ltype, $0)
-   }
+		add(ltype, $0)
+	}
 
-   END{
-      for(z = 1; z <= no_ai; ++z){
-         t = no_a[z]
-         j = index(t, "/")
-         mt = toupper(substr(t, 1, j - 1))
-         j = substr(t, j + 1)
-         l = length(j)
+	END{
+		for(z = 1; z <= no_ai; ++z){
+			t = no_a[z]
+			j = index(t, "/")
+			mt = toupper(substr(t, 1, j - 1))
+			j = substr(t, j + 1)
+			l = length(j)
 
-         # type-marker?
-         if(!p_a[t])
-            mt = "a_MT_" mt
-         else{
-            tm = p_a[t]
-            tmx = ""
-            aster = 0
+			# type-marker?
+			if(!p_a[t])
+				mt = "a_MT_" mt
+			else{
+				tm = p_a[t]
+				tmx = ""
+				aster = 0
 
-            while(length(tm) > 0){
-               x = substr(tm, 1, 1)
-               tm = substr(tm, 2)
-               if(x == "*"){
-                  if(aster++ != 0){
-                     print "! DUPLICATE * MIME TYPE-MARKER: <" no_a[z] ">" \
-                        " <" p_a[t] ">" >> "/dev/stderr"
-                  }
-               }else{
-                  if(tmx)
-                     print "! INVALID MIME TYPE-MARKER: <" no_a[z] ">" \
-                        " <" p_a[t] ">" >> "/dev/stderr"
-                  if(x == "t")
-                     tmx = "a_MT_TM_PLAIN"
-                  else if(x == "h")
-                     tmx = "a_MT_TM_SOUP_h"
-                  else if(x == "H")
-                     tmx = "a_MT_TM_SOUP_H"
-                  else if(x == "q")
-                     tmx = "a_MT_TM_QUIET"
-               }
-            }
+				while(length(tm) > 0){
+					x = substr(tm, 1, 1)
+					tm = substr(tm, 2)
+					if(x == "*"){
+						if(aster++ != 0)
+							print "! DUPLICATE * TYPE-MARKER: <" no_a[z] "> <" p_a[t] ">" >> "/dev/stderr"
+					}else{
+						if(tmx)
+							print "! INVALID TYPE-MARKER: <" no_a[z] "> <" p_a[t] ">" >> "/dev/stderr"
+						if(x == "t")
+							tmx = "a_MT_TM_PLAIN"
+						else if(x == "h")
+							tmx = "a_MT_TM_SOUP_h"
+						else if(x == "H")
+							tmx = "a_MT_TM_SOUP_H"
+						else if(x == "q")
+							tmx = "a_MT_TM_QUIET"
+					}
+				}
 
-            mt = "a_MT_" mt
-            if(!tmx && !aster)
-               tmx = "a_MT_TM_PLAIN"
-            if(aster)
-               mt = mt " | a_MT_TM_HDL_ONLY"
-            if(tmx)
-               mt = mt " | " tmx
-         }
+				mt = "a_MT_" mt
+				if(!tmx && !aster)
+					tmx = "a_MT_TM_PLAIN"
+				if(aster)
+					mt = mt " | a_MT_TM_HDL_ONLY"
+				if(tmx)
+					mt = mt " | " tmx
+			}
 
-         print "   {" mt ", " l ", \"" j e_a[t] "\"},"
-      }
-   }
+			print "	{" mt ", " l ", \"" j e_a[t] "\"},"
+		}
+	}
 '
 
-# s-sh-mode
+# s-sht-mode
--- mk/pcb-cc.sh
+++ mk/pcb-cc.sh
@@ -5,36 +5,36 @@
 
 # Do we have to go via a real C compiler in preprocessor mode?
 dowemayhave() {
-   while [ $# -gt 0 ]; do
-      [ "$1" = -E ] && return 0
-      shift
-   done
-   return 1
+	while [ $# -gt 0 ]; do
+		[ "$1" = -E ] && return 0
+		shift
+	done
+	return 1
 }
 
 if dowemayhave "$@"; then
-   SU_FIND_COMMAND_INCLUSION=1 . "${TOPDIR}"mk/su-find-command.sh
+	SU_FIND_COMMAND_INCLUSION=1 . "${TOPDIR}"mk/su-find-command.sh
 
-   if acmd_set CC clang || acmd_set CC gcc ||
-         acmd_set CC tcc || acmd_set CC pcc ||
-         acmd_set CC c89 || acmd_set CC c99; then
-      exec $CC "$@"
-   else
-      echo >&2 'boing booom tschak'
-      echo >&2 'ERROR: I cannot find a compiler!'
-      echo >&2 'No clang(1), gcc(1), tcc(1), pcc(1), c89(1) or c99(1).'
-   fi
+	if acmd_set CC clang || acmd_set CC gcc ||
+			acmd_set CC tcc || acmd_set CC pcc ||
+			acmd_set CC c89 || acmd_set CC c99; then
+		exec $CC "$@"
+	else
+		echo >&2 'boing booom tschak'
+		echo >&2 'ERROR: I cannot find a compiler!'
+		echo >&2 'No clang(1), gcc(1), tcc(1), pcc(1), c89(1) or c99(1).'
+	fi
 else
-   [ $# -eq 0 ] && exit 1
-   while [ $# -gt 0 ]; do
-      if [ "$1" = -o ]; then
-         printf '\n' > "$2"
-         exit 0
-      fi
-      shift
-   done
-   exit 0
+	[ $# -eq 0 ] && exit 1
+	while [ $# -gt 0 ]; do
+		if [ "$1" = -o ]; then
+			printf '\n' > "$2"
+			exit 0
+		fi
+		shift
+	done
+	exit 0
 fi
 
 exit 1
-# s-sh-mode
+# s-sht-mode
