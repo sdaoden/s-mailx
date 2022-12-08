829f4951aca9a95f0833f541d138bfc48d85ae8d
Steffen Nurpmeso <steffen@sdaoden.eu>
2022-12-08 21:53:15 +0100

    [-] su/code.h: ISO C23 static_assert() is usable, use it for CTA(),LCTA()

--- include/su/code.h
+++ include/su/code.h
@@ -363,12 +363,11 @@
 #ifdef DOXYGEN
 # define su_CTA(T,M) /*!< \_ */
 # define su_LCTA(T,M) /*!< \remarks{Introduces a block scope.} */
-#elif !su_C_LANG && __cplusplus +0 >= 201103l
+  /* ISO C variant unusable pre-C23! */
+#elif (!su_C_LANG && __cplusplus +0 >= 201103l) || \
+	(su_C_LANG && defined __STDC_VERSION__ && __STDC_VERSION__ +0 >= 202311l)
 # define su_CTA(T,M) static_assert(T, M)
 # define su_LCTA(T,M) static_assert(T, M)
-#elif 0 /* unusable! */ && defined __STDC_VERSION__ && __STDC_VERSION__ +0 >= 201112l
-# define su_CTA(T,M) _Static_assert(T, M)
-# define su_LCTA(T,M) _Static_assert(T, M)
 #else
 # define su_CTA(T,M) su__CTA_1(T, su_FILE, __LINE__)
 # define su_LCTA(T,M) su__LCTA_1(T, su_FILE, __LINE__)
