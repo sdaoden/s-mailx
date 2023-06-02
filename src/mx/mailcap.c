ff95958ebadbfaf540d662c5657981f655069c8d
Steffen Nurpmeso <steffen@sdaoden.eu>
2023-06-03 00:13:29 +0200

    [SU.3] cs_dict: add data_space() / CS_DICT_DATA_SPACE_RAW "logic"..
    Very often i found the need to deal with allocations etc for
    simple datatypes stored in a dictionary, yet only a few bytes,
    and the dict has to do all the dance for its node anyway.
    
    So, even though it makes the dictionary logic even more
    complicated and horrifying, let us add some more of it so that it
    can reserve some data_space() bytes for non-DICT_OWNS instances.

--- src/mx/mailcap.c
+++ src/mx/mailcap.c
@@ -53,7 +53,7 @@ su_EMPTY_FILE()
 
 /* Dictionary stores a_mailcap_hdl* list, not owned */
 #define a_MAILCAP_CSD_FLAGS (su_CS_DICT_CASE | su_CS_DICT_HEAD_RESORT | su_CS_DICT_ERR_PASS)
-#define a_MAILCAP_CSD_THRESHOLD_SHIFT 3
+#define a_MAILCAP_CSD_THRESHOLD 3
 
 /* Must be alphabetical */
 enum a_mailcap_sfields{
@@ -160,8 +160,8 @@ a_mailcap_create(void){
 	char *cp_base, *cp;
 	NYD_IN;
 
-	a_mailcap_dp = su_cs_dict_set_threshold_shift(su_cs_dict_create(&a_mailcap__d, a_MAILCAP_CSD_FLAGS, NIL),
-			a_MAILCAP_CSD_THRESHOLD_SHIFT);
+	a_mailcap_dp = su_cs_dict_set_threshold(su_cs_dict_create(&a_mailcap__d, a_MAILCAP_CSD_FLAGS, NIL),
+			a_MAILCAP_CSD_THRESHOLD);
 	DVL( su_state_on_gut_install(&a_mailcap__on_gut, FAL0, su_STATE_ERR_NOPASS); )
 
 	if(*(cp_base = UNCONST(char*,ok_vlook(MAILCAPS))) == '\0')
