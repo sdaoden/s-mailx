b9f17c059a05cee2375e8cd73e64b261d11c55e8
Steffen Nurpmeso <steffen@sdaoden.eu>
2023-12-16 00:44:15 +0100

    Add "send-text" `mimetype' type-marker flag to enforce text/plain..
    MUAs like mutt(1) enforce text/plain for MIME parts when the
    content appears like text for them.
    
    We instead used the detected MIME type, but added, until not too
    long ago (Rework mime-classification a bit[..]), the charset=
    parameter to such parts.  And did interpret it ourselfs, then.
    However, this is not portable (due to MIME standard deficencies,
    or, better, loose definitions, but, well).
    
    For example mutt(1) requires users to go around a corner to
    actually see those files in the pager, and does not apply
    character set conversion when saving such attachments.
    
    So what are we going to do?
    Invent another `mimetype' marker which users may use to enforce
    text/plain when sending such data (only).  Of course, if the
    content does not seem text, it will then end up as octet-stream.
    
    While here, rename the type-marker * introduced on this devel
    branch (Add ?* mimetype type-marker, work around standard
    restriction (Olaf Hering)..) to "only-handler".
    
    Make type-markers a comma-separated list.

--- mk/make-mime-types.sh
+++ mk/make-mime-types.sh
@@ -5,6 +5,8 @@
 
 LC_ALL=C
 
+: ${awk:=awk}
+
 < "$1" > "$2" exec ${awk} '
 	function add(mt, ln){
 		gsub(/[	 ]]+/, " ", ln)
@@ -29,10 +31,12 @@ LC_ALL=C
 		add(ltype, $0)
 		next
 	}
-	/^(\?([*thHq]*) )?[a-zA-Z]/{
+	/^(\?([-,*[:alnum:]]*) )?[a-zA-Z]/{
 		# type-marker?
-		if($1 ~ /^\?([*thHq]*)$/){
-			pa = $1
+		if($1 ~ /^\?([-,*[:alnum:]])*$/){
+			pa = substr($1, 2)
+			if(!pa)
+				pa = ","
 			$1 = $2
 			$2 = ""
 		}else
@@ -43,8 +47,7 @@ LC_ALL=C
 		ltype = $1; $1 = ""
 
 		if(!nt_a[ltype]){
-			if(pa)
-				p_a[ltype] = pa
+			p_a[ltype] = pa
 			no_a[++no_ai] = nt_a[ltype] = ltype
 		}else if(pa && p_a[ltype])
 			print "! TYPE-MARKER IGNORED WHEN EXTENDING MIME TYPE: <" ltype ">: " $0 >> "/dev/stderr"
@@ -66,17 +69,32 @@ LC_ALL=C
 			else{
 				tm = p_a[t]
 				tmx = ""
-				aster = 0
+				only_handler = send_text = 0
 
 				while(length(tm) > 0){
-					x = substr(tm, 1, 1)
-					tm = substr(tm, 2)
-					if(x == "*"){
-						if(aster++ != 0)
-							print "! DUPLICATE * TYPE-MARKER: <" no_a[z] "> <" p_a[t] ">" >> "/dev/stderr"
+					i = index(tm, ",")
+					if(i != 0){
+						x = substr(tm, 1, i - 1)
+						tm = substr(tm, i + 1)
+						if(!length(x))
+							continue
+					}else{
+						x = substr(tm, 1)
+						tm = ""
+					}
+
+					if(x == "o" || x == "only-handler"){
+						if(only_handler++ != 0)
+							print "! DUPLICATE only-handler TYPE-MARKER: <" no_a[z] \
+								"> <" p_a[t] ">" >> "/dev/stderr"
+					}else if(x == "s" || x == "send-text"){
+						if(send_text++ != 0)
+							print "! DUPLICATE send-text TYPE-MARKER: <" no_a[z] \
+								"> <" p_a[t] ">" >> "/dev/stderr"
 					}else{
 						if(tmx)
-							print "! INVALID TYPE-MARKER: <" no_a[z] "> <" p_a[t] ">" >> "/dev/stderr"
+							print "! INVALID TYPE-MARKER: <" no_a[z] "> <" p_a[t] ">" \
+								>> "/dev/stderr"
 						if(x == "t")
 							tmx = "a_MT_TM_PLAIN"
 						else if(x == "h")
@@ -85,14 +103,24 @@ LC_ALL=C
 							tmx = "a_MT_TM_SOUP_H"
 						else if(x == "q")
 							tmx = "a_MT_TM_QUIET"
+						else
+							print "! INVALID TYPE-MARKER: <" no_a[z] "> <" p_a[t] ">: " x \
+								>> "/dev/stderr"
 					}
 				}
 
+				if(only_handler == send_text && only_handler)
+					print "! INVALID TYPE-MARKER: mutual exclusive: only_handler and send_text: ?" \
+						p_a[t] " " no_a[z] >> "/dev/stderr"
+
 				mt = "a_MT_" mt
-				if(!tmx && !aster)
+				if(!tmx && !only_handler && !send_text)
 					tmx = "a_MT_TM_PLAIN"
-				if(aster)
-					mt = mt " | a_MT_TM_HDL_ONLY"
+				if(only_handler)
+					mt = mt " | a_MT_TM_ONLY_HANDLER"
+				# xxx else: is mutual
+				if(send_text)
+					mt = mt " | a_MT_TM_SEND_TEXT"
 				if(tmx)
 					mt = mt " | " tmx
 			}
