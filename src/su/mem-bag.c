6c4988f59fc3a8c35a64f4753e538ec28d3dc448
Steffen Nurpmeso <steffen@sdaoden.eu>
2024-01-19 02:40:46 +0100

    SU mem_bag: no AUTO with SU; sprinkle more (UN)?LIKELY()

--- src/su/mem-bag.c
+++ src/su/mem-bag.c
@@ -137,11 +137,11 @@ a_membag_lofi_free_top(struct su_mem_bag *self){
 
 	mblcp = self->mb_lofi_top;
 	p.mblc = mblcp->mblc_last;
-	if((isheap = (p.u & 0x1)))
+	if(UNLIKELY((isheap = (p.u & 0x1))))
 		p.u ^= 0x1;
 	self->mb_lofi_top = p.mblc;
 
-	if(isheap){
+	if(UNLIKELY(isheap)){
 		p.v = &mblcp->mblc_buf[0];
 		su_FREE(p.vp[0]);
 	}else
@@ -149,7 +149,7 @@ a_membag_lofi_free_top(struct su_mem_bag *self){
 
 	/* We remove free pools but the last (== the first) one */
 	mblpp = self->mb_lofi_pool;
-	if((mblpp->mblp_caster = R(char*,mblcp)) == mblpp->mblp_buf && (p.mblp = mblpp->mblp_last) != NIL){
+	if(UNLIKELY((mblpp->mblp_caster = R(char*,mblcp)) == mblpp->mblp_buf && (p.mblp = mblpp->mblp_last) != NIL)){
 		self->mb_lofi_pool = p.mblp;
 		su_FREE(mblpp);
 	}
@@ -590,7 +590,7 @@ su_mem_bag_auto_allocate(struct su_mem_bag *self, uz size, uz no, BITENUM(u32,su
 
 			/* Have a pool, chunk in rv (and: cp==.mbab_caster == rv+chunksz) */
 jhave_pool:;
-#if a_MEMBAG_HULL
+# if a_MEMBAG_HULL
 			/* C99 */{
 				union {void *p; void **pp;} v;
 
@@ -604,7 +604,7 @@ jhave_pool:;
 					goto jleave;
 				}
 			}
-#endif
+# endif
 		}else{
 			struct su__mem_bag_auto_huge *mbahp;
 
@@ -618,9 +618,9 @@ jhave_pool:;
 					su_DVL_LOC_ARGS_FILE, su_DVL_LOC_ARGS_LINE));
 			if(UNLIKELY(mbahp == NIL))
 				goto jleave;
-#if !a_MEMBAG_HULL
+# if !a_MEMBAG_HULL
 			rv = mbahp->mbah_buf;
-#else
+# else
 			rv = su_ALLOCATE_LOC(size, 1, (mbaf | su_MEM_ALLOC_MARK_AUTO_HUGE),
 					su_DVL_LOC_ARGS_FILE, su_DVL_LOC_ARGS_LINE);
 			if(rv != NIL){
@@ -632,7 +632,7 @@ jhave_pool:;
 				su_FREE(mbahp);
 				goto jleave;
 			}
-#endif
+# endif
 			mbahp->mbah_last = self->mb_auto_huge;
 			self->mb_auto_huge = mbahp;
 		}
@@ -678,13 +678,13 @@ su_mem_bag_lofi_snap_gut(struct su_mem_bag *self, void *cookie){
 		self = oself->mb_top;
 
 	while((mblcp = self->mb_lofi_top) != cookie){
-#ifdef su_HAVE_DEBUG
+# ifdef su_HAVE_DEBUG
 		if(mblcp == NIL){
 			su_log_write(su_LOG_DEBUG,
 				"su_mem_bag_lofi_snap_gut(%p): no such snap exists: non-debug crash!", oself);
 			break;
 		}
-#endif
+# endif
 		self = a_membag_lofi_free_top(self);
 	}
 
@@ -747,7 +747,7 @@ su_mem_bag_lofi_allocate(struct su_mem_bag *self, uz size, uz no, BITENUM(u32,su
 		/* Need a pool */
 		mblpp = S(struct su__mem_bag_lofi_pool*,su_ALLOCATE_LOC((self->mb_bsz + a_MEMBAG_BSZ_BASE), 1,
 				(mbaf | su_MEM_ALLOC_MARK_LOFI), su_DVL_LOC_ARGS_FILE, su_DVL_LOC_ARGS_LINE));
-		if(mblpp == NIL)
+		if(UNLIKELY(mblpp == NIL))
 			goto jleave;
 		rv = cp = mblpp->mblp_buf;
 		mblpp->mblp_caster = cp = &cp[chunksz];
@@ -758,7 +758,7 @@ su_mem_bag_lofi_allocate(struct su_mem_bag *self, uz size, uz no, BITENUM(u32,su
 jhave_pool:
 		mblcp = S(struct su__mem_bag_lofi_chunk*,rv);
 		mblcp->mblc_last = R(struct su__mem_bag_lofi_chunk*, R(up,self->mb_lofi_top) | S(uz,isheap));
-		if(!isheap)
+		if(LIKELY(!isheap))
 			rv = mblcp->mblc_buf;
 		else{
 			union {void *p; void **pp;} v;
@@ -776,7 +776,7 @@ jhave_pool:
 		}
 		self->mb_lofi_top = mblcp;
 
-		if(mbaf & su_MEM_BAG_ALLOC_ZERO)
+		if(UNLIKELY(mbaf & su_MEM_BAG_ALLOC_ZERO))
 			su_mem_set(rv, 0, size);
 		DVLDBG( else su_mem_set(rv, su__mem_filler, size); )
 	}else
@@ -796,7 +796,7 @@ su_mem_bag_lofi_free(struct su_mem_bag *self, void *ovp  su_DVL_LOC_ARGS_DECL){
 	if(self->mb_top != NIL)
 		self = self->mb_top;
 
-#ifdef su_HAVE_DEBUG
+# ifdef su_HAVE_DEBUG
 	/* C99 */{
 		struct su__mem_bag_lofi_chunk *mblcp;
 
@@ -825,7 +825,7 @@ jeinval:
 			goto NYD_OU_LABEL;
 		}
 	}
-#endif /* su_HAVE_DEBUG */
+# endif /* su_HAVE_DEBUG */
 
 	self = a_membag_lofi_free_top(self);
 
