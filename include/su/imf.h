58eed3211a836187ed4b97e3b3f4ec46b156ba6a
Steffen Nurpmeso <steffen@sdaoden.eu>
2024-03-24 02:25:30 +0100

    IMF: add su_imf_parse_struct_header()

--- include/su/imf.h
+++ include/su/imf.h
@@ -34,8 +34,13 @@
 C_DECL_BEGIN
 
 struct su_imf_addr;
+struct su_imf_shtok;
 
-/* su_imf_addr {{{ */
+#ifndef su_HAVE_MEM_BAG
+# error IMF only with su_HAVE_MEM_BAG
+#endif
+
+/* su_imf* {{{ */
 /*!
  * \defgroup IMF Internet Message Format
  * \ingroup NET
@@ -62,7 +67,7 @@ struct su_imf_addr;
  * Comments in display names of address groups are discarded (after being parsed correctly),
  * just like comments "in the void" (like \c{..., (comment), ...}).
  * }\li{
- * ASCII NUL aka bytes with value 0 are never supported in bodies.
+ * ASCII \NUL aka bytes with value 0 are never supported in bodies.
  * }}
  *
  * \remarks{Only available if \r{su_HAVE_IMF} is defined.}
@@ -72,55 +77,95 @@ struct su_imf_addr;
 /*! Shares bit range with \r{su_imf_state} and \r{su_imf_err}. */
 enum su_imf_mode{
 	su_IMF_MODE_NONE, /*!< Nothing (this is 0). */
-	su_IMF_MODE_RELAX = 1u<<0, /*!< Ignore some errors which would otherwise cause hard failure. */
-	/*! Ok \c{.} in display-name, support \c{Dr. X &lt;y&#40;z&gt;} user input. */
-	su_IMF_MODE_OK_DISPLAY_NAME_DOT = 1u<<1,
+
+	/*! Ok \c{.} in display-name, support \c{Dr. X &lt;y&#40;z&gt;} user input.
+	 * For address headers. */
+	su_IMF_MODE_OK_DISPLAY_NAME_DOT = 1u<<0,
 	/*! Ok plain user name in angle-bracket address, that is \c{&lt;USERNAME&gt;}, without domain name.
-	 * The validity of \c{USERNAME} and its expansion is up to the caller. */
-	su_IMF_MODE_OK_ADDR_SPEC_NO_DOMAIN = 1u<<2,
-	/*! Stop parsing whenever the first address has been successfully parsed.
-	 * \remarks{Empty groups (\c{Undisclosed recipients:;}) do not count as addresses.} */
+	 * The validity of \c{USERNAME} and its expansion is up to the caller.
+	 * For address headers. */
+	su_IMF_MODE_OK_ADDR_SPEC_NO_DOMAIN = 1u<<1,
+
+	/*! Parse RFC 5322 \c{dot-atom-text} instead of \c{atext} for \c{phrase}.
+	 * For structured header( token)s, extend the meaning of \c{phrase} to allow unquoted period. */
+	su_IMF_MODE_OK_DOT_ATEXT = 1u<<2,
+	/*! Generate result tokens for comments.
+	 * For structured header( token)s. */
+	su_IMF_MODE_TOK_COMMENT = 1u<<3,
+	/*! Treat (unquoted) semicolon as a terminator of the current token, instead of an error.
+	 * Multiple tokens may still be generated (in between semicolons).
+	 * For structured header( token)s. */
+	su_IMF_MODE_TOK_SEMICOLON = 1u<<4,
+	/*! Allow empty tokens in conjunction with \r{su_IMF_MODE_TOK_SEMICOLON}.
+	 * As whitespace is skipped over, and empty (or ignored) comments and quoted-strings do not generate output,
+	 * a semantically meaningful semicolon (for example in the \c{Authentication-Results} header) will not generate
+	 * a token until this flag is set.
+	 * For structured header( token)s. */
+	su_IMF_MODE_TOK_EMPTY = 1u<<5,
+
+	su_IMF_MODE_RELAX = 1u<<6, /*!< Ignore some errors which would otherwise cause hard failure. */
+	/*! Stop parsing whenever the first address or token has been successfully parsed.
+	 * \remarks{Empty groups (\c{Undisclosed recipients:;}) do not count as addresses, therefore the result list
+	 * can consist of multiple nodes despite that flag being set.} */
 	su_IMF_MODE_STOP_EARLY = 1u<<7,
+
+	su__IMF_MODE_ADDR_MASK = su_IMF_MODE_OK_DISPLAY_NAME_DOT | su_IMF_MODE_OK_ADDR_SPEC_NO_DOMAIN |
+			su_IMF_MODE_RELAX | su_IMF_MODE_STOP_EARLY,
+	su__IMF_MODE_STRUCT_MASK = su_IMF_MODE_OK_DOT_ATEXT | su_IMF_MODE_TOK_COMMENT |
+			su_IMF_MODE_TOK_SEMICOLON | su_IMF_MODE_TOK_EMPTY |
+			su_IMF_MODE_RELAX | su_IMF_MODE_STOP_EARLY,
 	su__IMF_MODE_MASK = 0xFF
 };
 
 /*! Shares bit range with \r{su_imf_mode} and \r{su_imf_err}. */
 enum su_imf_state{
+	/*!< \r{su_IMF_MODE_OK_DISPLAY_NAME_DOT}, and an unquoted . was seen; result is correctly quoted. */
+	su_IMF_STATE_DISPLAY_NAME_DOT = 1u<<8,
+	/*! \r{su_IMF_MODE_OK_ADDR_SPEC_NO_DOMAIN}, \c{&lt;USERNAME&gt;} was seen. */
+	su_IMF_STATE_ADDR_SPEC_NO_DOMAIN = 1u<<9,
+	/*! \r{su_imf_addr::imfa_domain} is a (possibly empty) literal; surrounding brackets are not stripped. */
+	su_IMF_STATE_DOMAIN_LITERAL = 1u<<10,
+	su_IMF_STATE_GROUP = 1u<<11, /*!< Belongs to an address group (that maybe starts and/or ends). */
+	su_IMF_STATE_GROUP_START = 1u<<12, /*!< Group start. */
+	su_IMF_STATE_GROUP_END = 1u<<13, /*!< Group end. */
+	su_IMF_STATE_GROUP_EMPTY = 1u<<14, /*!< Group without address, for example \c{Undisclosed recipients:;}. */
+
+	su_IMF_STATE_SEMICOLON = 1u<<15, /*!< With \r{su_IMF_MODE_TOK_SEMICOLON}, a semicolon was seen. */
+	su_IMF_STATE_COMMENT = 1u<<16, /*!< With \r{su_IMF_MODE_TOK_COMMENT}, result represents a parsed comment. */
+
 	/*! Errors were ignored due to \r{su_IMF_MODE_RELAX}.
 	 * \r{su_imf_err} bits (but \r{su_IMF_ERR_RELAX}) are set.
-	 * Be aware that with this the plain empty string \c{:} is a "valid" empty group. */
-	su_IMF_STATE_RELAX = 1u<<8,
-	/*!< \r{su_IMF_MODE_OK_DISPLAY_NAME_DOT}, and an unquoted . was seen; result is correctly quoted. */
-	su_IMF_STATE_DISPLAY_NAME_DOT = 1u<<9,
-	/*! \r{su_IMF_MODE_OK_ADDR_SPEC_NO_DOMAIN}, \c{&lt;USERNAME&gt;} was seen. */
-	su_IMF_STATE_ADDR_SPEC_NO_DOMAIN = 1u<<10,
-	/*! \r{su_imf_addr::imfa_domain} is a (possibly empty) literal; surrounding brackets are not stripped. */
-	su_IMF_STATE_DOMAIN_LITERAL = 1u<<11,
-	su_IMF_STATE_GROUP = 1u<<12, /*!< Belongs to a group (that maybe starts and/or ends). */
-	su_IMF_STATE_GROUP_START = 1u<<13, /*!< Group start. */
-	su_IMF_STATE_GROUP_END = 1u<<14, /*!< Group end. */
-	su_IMF_STATE_GROUP_EMPTY = 1u<<15, /*!< Group without address, for example \c{Undisclosed recipients:;}. */
+	 * Be aware that with this flag a colon \c{:} is a "valid" empty address group. */
+	su_IMF_STATE_RELAX = 1u<<17,
+
 	/*! A mask of all states except \r{su_IMF_STATE_RELAX}. */
 	su_IMF_STATE_MASK = su_IMF_STATE_DISPLAY_NAME_DOT | su_IMF_STATE_ADDR_SPEC_NO_DOMAIN |
-			su_IMF_STATE_DOMAIN_LITERAL | su_IMF_STATE_GROUP | su_IMF_STATE_GROUP_START |
-			su_IMF_STATE_GROUP_END | su_IMF_STATE_GROUP_EMPTY
+				su_IMF_STATE_DOMAIN_LITERAL | su_IMF_STATE_GROUP | su_IMF_STATE_GROUP_START |
+				su_IMF_STATE_GROUP_END | su_IMF_STATE_GROUP_EMPTY |
+			su_IMF_STATE_SEMICOLON | su_IMF_STATE_COMMENT
 };
 
 /*! Shares bit range with \r{su_imf_mode} and \r{su_imf_state}. */
 enum su_imf_err{
-	su_IMF_ERR_RELAX = 1u<<20, /*!< Errors could have been or were ignored with or due \r{su_IMF_MODE_RELAX}. */
-	su_IMF_ERR_GROUP_DISPLAY_NAME_EMPTY = 1u<<21, /*!< Group display-name must not be empty, but is. */
-	su_IMF_ERR_DISPLAY_NAME_DOT = 1u<<22, /*!< \c{.} in display-name, no \r{su_IMF_MODE_OK_DISPLAY_NAME_DOT}. */
-	su_IMF_ERR_ADDR_SPEC = 1u<<23, /*!< Invalid or empty address content; also: invalid route; also: fallback. */
-	su_IMF_ERR_COMMENT = 1u<<24, /*!< Comment content invalid, or comment not closed. */
-	su_IMF_ERR_DQUOTE = 1u<<25, /*!< Quoted-string \c{".."} content invalid, or quote not closed. */
-	su_IMF_ERR_GROUP_OPEN = 1u<<26, /*!< A group was not closed. */
+	su_IMF_ERR_GROUP_DISPLAY_NAME_EMPTY = 1u<<24, /*!< Address group display-name must not be empty, but is. */
+	su_IMF_ERR_DISPLAY_NAME_DOT = 1u<<25, /*!< \c{.} in display-name, no \r{su_IMF_MODE_OK_DISPLAY_NAME_DOT}. */
+	su_IMF_ERR_DQUOTE = 1u<<26, /*!< Quoted-string \c{".."} content invalid, or quote not closed. */
+	su_IMF_ERR_GROUP_OPEN = 1u<<27, /*!< A group was not closed. */
+
+	su_IMF_ERR_COMMENT = 1u<<28, /*!< Comment content invalid, or comment not closed. */
+
+	su_IMF_ERR_CONTENT = 1u<<29, /*!< Invalid or (unexpectedly) empty content; also: invalid route, fallback. */
+
+	su_IMF_ERR_RELAX = 1u<<30, /*!< Errors could have been or were ignored with or due \r{su_IMF_MODE_RELAX}. */
+
 	/*! A mask of all errors except \r{su_IMF_ERR_RELAX}. */
-	su_IMF_ERR_MASK = su_IMF_ERR_GROUP_DISPLAY_NAME_EMPTY | su_IMF_ERR_DISPLAY_NAME_DOT | su_IMF_ERR_ADDR_SPEC |
-			su_IMF_ERR_COMMENT | su_IMF_ERR_DQUOTE | su_IMF_ERR_GROUP_OPEN
+	su_IMF_ERR_MASK = su_IMF_ERR_GROUP_DISPLAY_NAME_EMPTY | su_IMF_ERR_DISPLAY_NAME_DOT |
+				su_IMF_ERR_DQUOTE | su_IMF_ERR_GROUP_OPEN |
+			su_IMF_ERR_COMMENT |
+			su_IMF_ERR_CONTENT
 };
 
-/*! \_ */
+/*! Parsed address structure; all buffers are accessible and \NUL terminated. */
 struct su_imf_addr{
 	struct su_imf_addr *imfa_next; /*!< In case of address lists and/or groups. */
 	char *imfa_group_display_name; /*!< Only with \r{su_IMF_STATE_GROUP_START}. */
@@ -135,6 +180,14 @@ struct su_imf_addr{
 	u32 imfa_comm_len; /*!< \_ */
 	u32 imfa_mse; /*!< Bitmix of \r{su_imf_mode}, \r{su_imf_state} and \r{su_imf_err}. */
 	char imfa_dat[VFIELD_SIZE(0)]; /* Storage for any text (single chunk struct) */
+};
+
+/*! Parsed structured header field token; the buffer is accessible and \NUL terminated. */
+struct su_imf_shtok{
+	struct su_imf_shtok *imfsht_next; /*!< Next token, if any. */
+	u32 imfsht_mse; /*!< Bitmix of \r{su_imf_mode}, \r{su_imf_state} and \r{su_imf_err}. */
+	u32 imfsht_len; /*!< Length of \c{imfsht_dat}. */
+	char imfsht_dat[VFIELD_SIZE(0)];
 };
 
 /*! Create a snap for the bag type \r{IMF} uses; see \r{su_imf_parse_addr_header()}. */
@@ -161,6 +214,7 @@ INLINE void su_imf_snap_gut(struct su_mem_bag *membp, void *snap){
 
 /*! Parse an (possibly multiline) Internet Message Format address(-list / mailbox(-list)) header field body.
  * Stores a result list in \a{*app}, or \NIL if nothing can be parsed.
+ * A result without any data is not produced.
  * Results may contain \c{IMF_ERR_} entries with \r{su_IMF_MODE_RELAX}, or according to \c{IMF_MODE_OK_*}.
  * If \a{endptr_or_nil} is set it will point to where parsing stopped (points to \NUL but in error cases).
  * Returns \ERR{NONE} on success, -\ERR{NODATA} on empty (or only whitespace) input, -\ERR{OVERFLOW} if input is too
@@ -173,6 +227,28 @@ INLINE void su_imf_snap_gut(struct su_mem_bag *membp, void *snap){
  * \r{su_imf_snap_create()} and \r{su_imf_snap_gut()} can be used for this. */
 EXPORT s32 su_imf_parse_addr_header(struct su_imf_addr **app, char const *header, BITENUM(u32,su_imf_mode) mode,
 		struct su_mem_bag *membp, char const **endptr_or_nil);
+
+/*! Parse a (possibly multiline) structured header field body.
+ * This is interpreted as a mix of \c{CFWS} and \c{phrase} tokens.
+ * If \c{quoted-string} is parsed within \c{phrase}, the entire result token will be (re-)quoted as such.
+ * With \r{su_IMF_MODE_OK_DOT_ATEXT} RFC 5322 \c{phrase} is assumed to contain \c{dot-atom-text}, not \c{atext}.
+ * With \r{su_IMF_MODE_TOK_COMMENT} comments create result tokens instead of being discarded:
+ * adjacent comments are joined, and results do not contain the parenthesis, for example \c{(a (b)) (c)}
+ * creates the result \c{a b c}.
+ * With \r{su_IMF_MODE_TOK_SEMICOLON} a(n unquoted) semicolon does not result in error but terminates the current
+ * token: it is not stored, but announced via \r{su_IMF_STATE_SEMICOLON} in \r{su_imf_shtok::imfsht_mse};
+ * then \r{su_IMF_MODE_TOK_EMPTY} can also be used: an input \¢{"";();;} would then create three empty tokens
+ * with only \r{su_IMF_STATE_SEMICOLON} set.
+ *
+ * Stores a result list in \a{*shtpp}, or \NIL if nothing can be parsed.
+ * \r{su_IMF_MODE_RELAX} may be used, and \r{su_IMF_MODE_STOP_EARLY}, too.
+ * If \a{endptr_or_nil} is set it will point to where parsing stopped (points to \NUL but in error cases).
+ * Returns \ERR{NONE} on success, -\ERR{NODATA} on empty (or only whitespace) input, -\ERR{OVERFLOW} if input is too
+ * long, or -\ERR{NOMEM} if an allocation failed.
+ * A positive return represents a \r{su_imf_state} and \r{su_imf_err} (logical subset) bitmix of the parse error.
+ * The same result allocation scheme as for \r{su_imf_parse_addr_header()} is used. */
+EXPORT s32 su_imf_parse_struct_header(struct su_imf_shtok **shtpp, char const *header, BITENUM(u32,su_imf_mode) mode,
+		struct su_mem_bag *membp, char const **endptr_or_nil);
 /*! @} *//* }}} */
 
 C_DECL_END
@@ -184,6 +260,7 @@ NSPC_BEGIN(su)
 
 class imf;
 //class imf::addr;
+//class imf::shtok;
 
 /* imf {{{ */
 /*!
@@ -195,6 +272,7 @@ class imf{
 	su_CLASS_NO_COPY(imf);
 public:
 	class addr;
+	class shtok;
 
 	/* addr {{{ */
 	/*! \copydoc{su_imf_addr} */
@@ -211,58 +289,88 @@ public:
 		/*! \r{su_imf_addr::imfa_mse} */
 		u32 mse(void) const {return imfa_mse;}
 
-		/*! \r{su_imf_addr::imfa_group_display_name} */
-		char const *group_display_name(void) const {return imfa_group_display_name;}
-
 		/*! \r{su_imf_addr::imfa_group_display_name_len} */
 		u32 group_display_name_len(void) const {return imfa_group_display_name_len;}
 
-		/*! \r{su_imf_addr::imfa_display_name} */
-		char const *display_name(void) const {return imfa_display_name;}
+		/*! \r{su_imf_addr::imfa_group_display_name} */
+		char const *group_display_name(void) const {return imfa_group_display_name;}
 
 		/*! \r{su_imf_addr::imfa_display_name_len} */
 		u32 display_name_len(void) const {return imfa_display_name_len;}
 
-		/*! \r{su_imf_addr::imfa_locpar} */
-		char const *locpar(void) const {return imfa_locpar;}
+		/*! \r{su_imf_addr::imfa_display_name} */
+		char const *display_name(void) const {return imfa_display_name;}
 
 		/*! \r{su_imf_addr::imfa_locpar_len} */
 		u32 locpar_len(void) const {return imfa_locpar_len;}
 
-		/*! \r{su_imf_addr::imfa_domain} */
-		char const *domain(void) const {return imfa_domain;}
+		/*! \r{su_imf_addr::imfa_locpar} */
+		char const *locpar(void) const {return imfa_locpar;}
 
 		/*! \r{su_imf_addr::imfa_domain_len} */
 		u32 domain_len(void) const {return imfa_domain_len;}
 
-		/*! \r{su_imf_addr::imfa_comm} */
-		char const *comm(void) const {return imfa_comm;}
+		/*! \r{su_imf_addr::imfa_domain} */
+		char const *domain(void) const {return imfa_domain;}
 
 		/*! \r{su_imf_addr::imfa_comm_len} */
 		u32 comm_len(void) const {return imfa_comm_len;}
+
+		/*! \r{su_imf_addr::imfa_comm} */
+		char const *comm(void) const {return imfa_comm;}
+	};
+	/* }}} */
+
+	/* shtok {{{ */
+	/*! \copydoc{su_imf_shtok} */
+	class shtok : private su_imf_shtok{
+		friend class imf;
+	protected:
+		shtok(void) {}
+	public:
+		~shtok(void) {}
+
+		/*! \r{su_imf_shtok::imfsht_next} */
+		shtok *next(void) const {return S(shtok*,imfsht_next);}
+
+		/*! \r{su_imf_shtokr::imfsht_mse} */
+		u32 mse(void) const {return imfsht_mse;}
+
+		/*! \r{su_imf_shtok::imfsht_len} */
+		u32 len(void) const {return imfsht_len;}
+
+		/*! \r{su_imf_shtok::imfsht_dat} */
+		char const *dat(void) const {return imfsht_dat;}
 	};
 	/* }}} */
 
 private:
 #ifndef DOXYGEN
 	su_CXXCAST(addr, struct su_imf_addr);
+	su_CXXCAST(shtok, struct su_imf_shtok);
 #endif
 public:
 
 	/*! \copydoc{su_imf_mode} */
 	enum mode{
 		mode_none = su_IMF_MODE_NONE, /*!< \copydoc{su_IMF_MODE_NONE} */
-		mode_relax = su_IMF_MODE_RELAX, /*!< \copydoc{su_IMF_MODE_RELAX} */
+
 		/*! \copydoc{su_IMF_MODE_OK_DISPLAY_NAME_DOT} */
 		mode_ok_display_name_dot = su_IMF_MODE_OK_DISPLAY_NAME_DOT,
 		/*! \copydoc{su_IMF_MODE_OK_ADDR_SPEC_NO_DOMAIN} */
 		mode_ok_addr_spec_no_domain = su_IMF_MODE_OK_ADDR_SPEC_NO_DOMAIN,
+
+		mode_ok_dot_atext = su_IMF_MODE_OK_DOT_ATEXT, /*!< \copydoc{su_IMF_MODE_OK_DOT_ATEXT} */
+		mode_tok_comment = su_IMF_MODE_TOK_COMMENT, /*! \copydoc{su_IMF_MODE_TOK_COMMENT} */
+		mode_tok_semicolon = su_IMF_MODE_TOK_SEMICOLON, /*!< \copydoc{su_IMF_MODE_TOK_SEMICOLON} */
+		mode_tok_empty = su_IMF_MODE_TOK_EMPTY, /*! \copydoc{su_IMF_MODE_TOK_EMPTY} */
+
+		mode_relax = su_IMF_MODE_RELAX, /*!< \copydoc{su_IMF_MODE_RELAX} */
 		mode_stop_early = su_IMF_MODE_STOP_EARLY /*!< \copydoc{su_IMF_MODE_STOP_EARLY} */
 	};
 
 	/*! \copydoc{su_imf_state} */
 	enum state{
-		state_relax = su_IMF_STATE_RELAX, /*!< \copydoc{su_IMF_STATE_RELAX} */
 		state_display_name_dot = su_IMF_STATE_DISPLAY_NAME_DOT, /*!< \copydoc{su_IMF_STATE_DISPLAY_NAME_DOT} */
 		/*! \copydoc{su_IMF_STATE_ADDR_SPEC_NO_DOMAIN} */
 		state_addr_spec_no_domain = su_IMF_STATE_ADDR_SPEC_NO_DOMAIN,
@@ -271,19 +379,29 @@ public:
 		state_group_start = su_IMF_STATE_GROUP_START, /*!< \copydoc{su_IMF_STATE_GROUP_START} */
 		state_group_end = su_IMF_STATE_GROUP_END, /*!< \copydoc{su_IMF_STATE_GROUP_END} */
 		state_group_empty = su_IMF_STATE_GROUP_EMPTY, /*!< \copydoc{su_IMF_STATE_GROUP_EMPTY} */
+
+		state_semicolon = su_IMF_STATE_SEMICOLON, /*! \copydoc{su_IMF_STATE_SEMICOLON} */
+		state_comment = su_IMF_STATE_COMMENT, /*! \copydoc{su_IMF_STATE_COMMENT} */
+
+		state_relax = su_IMF_STATE_RELAX, /*!< \copydoc{su_IMF_STATE_RELAX} */
+
 		state_mask = su_IMF_STATE_MASK /*!< \copydoc{su_IMF_STATE_MASK} */
 	};
 
 	/*! \copydoc{su_imf_err} */
 	enum err{
-		err_relax = su_IMF_ERR_RELAX, /*!< \copydoc{su_IMF_ERR_RELAX} */
 		/*! \copydoc{su_IMF_ERR_GROUP_DISPLAY_NAME_EMPTY} */
 		err_group_display_name_empty = su_IMF_ERR_GROUP_DISPLAY_NAME_EMPTY,
 		err_display_name_dot = su_IMF_ERR_DISPLAY_NAME_DOT, /*!< \copydoc{su_IMF_ERR_DISPLAY_NAME_DOT} */
-		err_addr_spec = su_IMF_ERR_ADDR_SPEC, /*!< \copydoc{su_IMF_ERR_ADDR_SPEC} */
-		err_comment = su_IMF_ERR_COMMENT, /*!< \copydoc{su_IMF_ERR_COMMENT} */
 		err_dquote = su_IMF_ERR_DQUOTE, /*!< \copydoc{su_IMF_ERR_DQUOTE} */
 		err_group_open = su_IMF_ERR_GROUP_OPEN, /*!< \copydoc{su_IMF_ERR_GROUP_OPEN} */
+
+		err_comment = su_IMF_ERR_COMMENT, /*!< \copydoc{su_IMF_ERR_COMMENT} */
+
+		err_content = su_IMF_ERR_CONTENT, /*!< \copydoc{su_IMF_ERR_CONTENT} */
+
+		err_relax = su_IMF_ERR_RELAX, /*!< \copydoc{su_IMF_ERR_RELAX} */
+
 		err_mask = su_IMF_ERR_MASK /*!< \copydoc{su_IMF_ERR_MASK} */
 	};
 
@@ -297,6 +415,13 @@ public:
 	static s32 parse_addr_header(addr *&app, char const *header, BITENUM(u32,mode) mode, mem_bag &membp,
 			char const **endptr_or_nil){
 		return su_imf_parse_addr_header(R(su_imf_addr**,&app), header, mode, S(struct su_mem_bag*,&membp),
+			endptr_or_nil);
+	}
+
+	/*! \r{su_imf_parse_struct_header()} */
+	static s32 parse_struct_header(shtok *&shtpp, char const *header, BITENUM(u32,mode) mode, mem_bag &membp,
+			char const **endptr_or_nil){
+		return su_imf_parse_struct_header(R(su_imf_shtok**,&shtpp), header, mode, S(struct su_mem_bag*,&membp),
 			endptr_or_nil);
 	}
 };
--- src/su/imf.c
+++ src/su/imf.c
@@ -132,6 +132,7 @@ NSPC_USE(su)
  * obs-qp          = "\" (%d0 / obs-NO-WS-CTL / LF / CR); NOTE this implementation does not support NUL
  * obs-route       = obs-domain-list ":"
  * obs-unstruct    = *((*LF *CR *(obs-utext *LF *CR)) / FWS)
+ * obs-utext       = %d0 / obs-NO-WS-CTL / VCHAR
  *
  * . 4.4.  Obsolete Addressing
  *   - When interpreting addresses, the route portion SHOULD be ignored
@@ -140,6 +141,7 @@ NSPC_USE(su)
  *   - In order to accommodate that "3.2.2. Folding White Space and Comments" allows
  *     CFWS rather freely, Postel'ize that and allow it practically everywhere.
  *   - obs-qp is supported WITHOUT NUL.
+ *   - obs-utext only WITHOUT NUL.
  * }}} */
 
 #if defined a_IMF_TABLE_DUMP || defined a_IMF_TABLE_USE
@@ -653,7 +655,7 @@ su_imf_parse_addr_header(struct su_imf_addr **app, char const *header, BITENUM(u
 	s32 rv;
 	struct su_imf_addr **app_base;
 	NYD_IN;
-	ASSERT_EXEC((mode & ~su__IMF_MODE_MASK) == 0, mode &= su__IMF_MODE_MASK);
+	ASSERT_EXEC((mode & ~su__IMF_MODE_ADDR_MASK) == 0, mode &= su__IMF_MODE_ADDR_MASK);
 
 	*(app_base = app) = NIL;
 
@@ -668,8 +670,8 @@ su_imf_parse_addr_header(struct su_imf_addr **app, char const *header, BITENUM(u
 			goto j_leave;
 		}
 
-		/* S32_MAX is mem-bag stuff, 6 is 5 strings of maximum size plus room for the structure as such.
-		 * We may re-quote display-names and locpar, so reserve two byte for quotes plus one for NUL */
+		/* S32_MAX is mem-bag stuff, 3 is 2 strings of maximum size plus room for the structure as such.
+		 * We may re-quote dat, so reserve two bytes for quotes plus one for NUL */
 		if(i >= (S32_MAX - 6/*xxx too much*/*3) / 6){
 			rv = -su_ERR_OVERFLOW;
 			goto j_leave;
@@ -687,7 +689,7 @@ su_imf_parse_addr_header(struct su_imf_addr **app, char const *header, BITENUM(u
 		acp->ac_.mse = mode;
 		acp->ac_group_display_name = acp->ac_dat;
 		acp->ac_display_name = &acp->ac_group_display_name[i];
-		++acp->ac_group_display_name;
+		++acp->ac_group_display_name; /* (room for double-quote) */
 		acp->ac_locpar = &acp->ac_display_name[i];
 		++acp->ac_display_name;
 		acp->ac_domain = &acp->ac_locpar[i];
@@ -705,7 +707,7 @@ jlist_next:
 	STRUCT_ZERO_FROM(struct a_imf_x, &acp->ac_, group_display_name);
 	acp->ac_.mse &= su__IMF_MODE_MASK | ((acp->ac_.mse & su_IMF_STATE_GROUP_END) ? 0 : su_IMF_STATE_GROUP);
 
-	for(rv = su_IMF_ERR_ADDR_SPEC;;){
+	for(rv = su_IMF_ERR_CONTENT;;){
 		char c;
 		char const *xcp;
 		u32 i;
@@ -1058,6 +1060,190 @@ jlocpar_copy:
 	}
 
 jlist_done:
+	rv = su_ERR_NONE;
+jleave:
+	header = acp->ac_.hd;
+j_leave:
+	if(endptr_or_nil != NIL)
+		*endptr_or_nil = header;
+
+	NYD_OU;
+	return rv;
+} /* }}} */
+
+s32
+su_imf_parse_struct_header(struct su_imf_shtok **shtpp, char const *header, BITENUM(u32,su_imf_mode) mode, /* {{{ */
+		struct su_mem_bag *membp, char const **endptr_or_nil){
+	s32 rv;
+	struct a_imf_actx *acp;
+	uz i;
+	struct su_imf_shtok **shtpp_base, *shtp;
+	NYD_IN;
+	ASSERT_EXEC((mode & ~su__IMF_MODE_STRUCT_MASK) == 0, mode &= su__IMF_MODE_STRUCT_MASK);
+	ASSERT_EXEC((mode & su_IMF_MODE_TOK_SEMICOLON) || !(mode & su_IMF_MODE_TOK_EMPTY),
+		mode &= ~su_IMF_MODE_TOK_EMPTY);
+
+	*(shtpp_base = shtpp) = NIL;
+
+	while(a_imf_c_ANY_WSP(*header))
+		++header;
+
+	i = su_cs_len(header);
+	if(i == 0){
+		rv = -su_ERR_NODATA;
+		goto j_leave;
+	}
+
+	/* S32_MAX is mem-bag stuff, 2 is 1 string of maximum size plus room for the structure as such.
+	 * We may re-quote, so reserve two bytes for quotes plus one for NUL */
+	if(i >= (S32_MAX - 2/*xxx too much*/*3) / 2){
+		rv = -su_ERR_OVERFLOW;
+		goto j_leave;
+	}else if(i < VSTRUCT_SIZEOF(struct a_imf_actx,ac_dat))
+		i = VSTRUCT_SIZEOF(struct a_imf_actx,ac_dat);
+	i += 3;
+	i <<= 1;
+
+	acp = a_IMF_ALLOC(membp, i);
+	if(acp == NIL){
+		rv = -su_ERR_NOMEM;
+		goto j_leave;
+	}
+
+	acp->ac_.hd = header;
+	acp->ac_.mse = mode;
+	/* Yes.  This looks hacky.  But dig it */
+	acp->ac_group_display_name = acp->ac_display_name = acp->ac_domain = acp->ac_locpar = NIL;
+	acp->ac_comm = acp->ac_dat;
+
+	/* Simply: split tokens after CFWS (or, optionally, semicolon) */
+	shtp = NIL;
+jtoken_next:
+	rv = su_ERR_NONE;
+
+	STRUCT_ZERO_FROM(struct a_imf_x, &acp->ac_, group_display_name);
+	acp->ac_.mse &= su__IMF_MODE_STRUCT_MASK;
+
+	/* In order to correlate semicolon to h1 in "(c1) h1 (c2);" without TOK_COMMENT we need to delay STOP_EARLY */
+	for(rv = su_IMF_ERR_CONTENT; *acp->ac_.hd != '\0';){
+		char const *start;
+		boole any;
+
+		any = FAL0;
+		start = acp->ac_.hd;
+
+		if(!a_imf_s_CFWS(acp)){
+			rv = (acp->ac_.mse & (su_IMF_ERR_RELAX | su_IMF_ERR_MASK));
+			goto jleave;
+		}
+		if(acp->ac_.comm > 0){
+			if(mode & su_IMF_MODE_TOK_COMMENT){
+				if((mode & su_IMF_MODE_STOP_EARLY) && shtp != NIL)
+					goto jdone;
+				acp->ac_.mse |= su_IMF_STATE_COMMENT;
+				goto jtoken_create;
+			}
+			acp->ac_.comm = 0;
+		}
+
+		any = (start != acp->ac_.hd);
+
+		for(i = 0;;){
+			char c;
+
+			start = acp->ac_.hd;
+
+			if(a_imf_c_DQUOTE(*acp->ac_.hd)){
+				/* Remove quotes, they are "not [a] semantical[ly] part" */
+				char *cp;
+
+				cp = a_imf_s_quoted_string(acp, &acp->ac_comm[i]);
+				if(cp == NIL){
+					rv = (acp->ac_.mse & su_IMF_ERR_MASK);
+					goto jleave;
+				}
+				i += P2UZ(cp - &acp->ac_comm[i]);
+				if(i > 0)
+					acp->ac_.mse |= su_IMF_STATE_GROUP; /* gross hack: misuse bit for "quotes" */
+			}
+
+			while(a_imf_c_atext((c = *acp->ac_.hd))){
+				acp->ac_comm[i++] = c;
+				++acp->ac_.hd;
+			}
+
+			if(mode & su_IMF_MODE_OK_DOT_ATEXT){
+				while(*acp->ac_.hd == '.'){
+					++acp->ac_.hd;
+					acp->ac_comm[i++] = '.';
+				}
+			}
+
+			if(start == acp->ac_.hd){
+				if(i == 0)
+					break;
+				if((mode & su_IMF_MODE_STOP_EARLY) && shtp != NIL)
+					goto jdone;
+				acp->ac_.comm = S(u32,i);
+				goto jtoken_create;
+			}
+		}
+
+		if(*acp->ac_.hd == ';'){
+			if(!(mode & su_IMF_MODE_TOK_SEMICOLON))
+				goto jleave;
+			++acp->ac_.hd;
+			if(shtp != NIL){
+				boole yet;
+
+				yet = ((shtp->imfsht_mse & su_IMF_STATE_SEMICOLON) != 0);
+				if(!yet)
+					shtp->imfsht_mse |= su_IMF_STATE_SEMICOLON;
+				if(mode & su_IMF_MODE_STOP_EARLY)
+					goto jdone;
+				if(!yet)
+					continue;
+			}else
+				ASSERT(acp->ac_.comm == 0);
+
+			if(mode & su_IMF_MODE_TOK_EMPTY){
+				acp->ac_.mse |= su_IMF_STATE_SEMICOLON;
+				goto jtoken_create;
+			}
+		}else if(!any)
+			goto jleave;
+		continue;
+
+jtoken_create:
+		i = acp->ac_.comm;
+		i = VSTRUCT_SIZEOF(struct su_imf_shtok,imfsht_dat) + i + 2 +1;
+
+		shtp = a_IMF_ALLOC(membp, i);
+		*shtpp = shtp;
+		shtpp = &shtp->imfsht_next;
+
+		shtp->imfsht_next = NIL;
+		shtp->imfsht_mse = (acp->ac_.mse & ~(su__IMF_MODE_STRUCT_MASK | su_IMF_STATE_GROUP));
+
+		/* C99 */{
+			char *cp;
+
+			cp = shtp->imfsht_dat;
+			if(acp->ac_.mse & su_IMF_STATE_GROUP)
+				*cp++ = '"';
+			su_mem_copy(cp, acp->ac_comm, acp->ac_.comm);
+			cp += acp->ac_.comm;
+			if(acp->ac_.mse & su_IMF_STATE_GROUP)
+				*cp++ = '"';
+			*cp = '\0';
+
+			shtp->imfsht_len = S(u32,P2UZ(cp - shtp->imfsht_dat));
+		}
+		goto jtoken_next;
+	}
+	ASSERT(*acp->ac_.hd == '\0');
+
+jdone:
 	rv = su_ERR_NONE;
 jleave:
 	header = acp->ac_.hd;
