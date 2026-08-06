2f0e92d3c58e72f2fcbaca1ac954b26ab0479b39
Steffen Nurpmeso <steffen@sdaoden.eu>
2026-08-06 21:49:10 +0200

    mx_time_tzdiff(): FIX calc when year wraps (Paul Eggert)

--- src/mx/time.c
+++ src/mx/time.c
@@ -145,8 +145,11 @@ mx_time_tzdiff(s64 secsepoch, struct tm const *utcp_or_nil, struct tm const *loc
 			(localp_or_nil->tm_min - utcp_or_nil->tm_min)) * 60) +
 			(localp_or_nil->tm_sec - utcp_or_nil->tm_sec);
 
-	if((t = (localp_or_nil->tm_yday - utcp_or_nil->tm_yday)) != 0)
+	if((t = (localp_or_nil->tm_yday - utcp_or_nil->tm_yday)) != 0){
+		if(t < 0 && localp_or_nil->tm_yday == 0)
+			t = 1;
 		rv += (t == 1) ? S(s32,su_TIME_DAY_SECS) : -S(s32,su_TIME_DAY_SECS);
+	}
 #endif
 
 jleave:
